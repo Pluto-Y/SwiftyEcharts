bbb9e57aae99472335555a469d61521d03962d35
Longhua Zhang <kuaileainits@163.com>
2017-01-10 11:17:46 +0800

    Re-struct `SECItemStyle.swift`
    1. Make `SECItemStyleContent` not conforms to CustomStringConvertible
    and delete all parameter named ‘description’
    2. Add `init()` constructor
    3. Make `SECCommonItemStyleContent ` and `SECItemStyle ` conforms to
    `SECMappable`

--- SwiftyEcharts/Models/Type/SECItemStyle.swift
+++ SwiftyEcharts/Models/Type/SECItemStyle.swift
@@ -6,11 +6,11 @@
 //  Copyright © 2017 com.pluto-y. All rights reserved.
 //
 
-public protocol SECItemStyleContent: SECColorful, SECBorderable, SECShadowable, SECOpacitable, CustomStringConvertible {
+public protocol SECItemStyleContent : SECColorful, SECBorderable, SECShadowable, SECOpacitable {
     var borderType: SECLineType? { get set }
 }
 
-public struct SECCommonItemStyleContent: SECItemStyleContent {
+public struct SECCommonItemStyleContent : SECItemStyleContent {
     
     public var color: SECColor?
     public var borderColor: SECColor?
@@ -26,18 +26,35 @@ public struct SECCommonItemStyleContent: SECItemStyleContent {
         }
     }
     
-    public var description: String {
-        return ""
+    public init() { }
+}
+
+extension SECCommonItemStyleContent : SECMappable {
+    public func mapping(map: SECMap) {
+        map["color"] = color
+        map["borderColor"] = borderColor
+        map["borderWidth"] = borderWidth
+        map["borderType"] = borderType
+        map["shadowBlur"] = shadowBlur
+        map["shadowColor"] = shadowColor
+        map["shadowOffsetX"] = shadowOffsetX
+        map["shadowOffsetY"] = shadowOffsetY
+        map["opacity"] = opacity
     }
 }
 
-public struct SECItemStyle: SECEmphasisable {
+public struct SECItemStyle : SECEmphasisable {
     public typealias Style = SECCommonItemStyleContent
     
     public var normal: Style?
     public var emphasis: Style?
     
-    public var description: String {
-        return ""
+    public init() { }
+}
+
+extension SECItemStyle : SECMappable {
+    public func mapping(map: SECMap) {
+        map["normal"] = normal
+        map["emphasis"] = emphasis
     }
 }

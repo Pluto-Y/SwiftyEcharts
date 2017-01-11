8d64919d31b090c8029a1c2983b93722731a7baa
Longhua Zhang <kuaileainits@163.com>
2017-01-11 12:53:17 +0800

    Add log prefix and warning log method

--- SwiftyEcharts/Tools/SECUtilities.swift
+++ SwiftyEcharts/Tools/SECUtilities.swift
@@ -17,5 +17,10 @@ func printInfo(object: Any) {
 
 // 自定义错误输出的方法，保证不管是Debug还是Release版本都会输出信息
 func printError(object: Any) {
-    Swift.print(object)
+    Swift.print("Error: \(object)")
+}
+
+// 自定义警告输出的方法，保证不管是Debug还是Release版本都会输出信息
+func printWarning(object: Any) {
+    Swift.print("Warning: \(object)")
 }

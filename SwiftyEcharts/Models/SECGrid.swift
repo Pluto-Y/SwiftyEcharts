a17f78ab72ebce88ad6f33224d8a788e529e7c91
Longhua Zhang <kuaileainits@163.com>
2017-01-11 14:25:14 +0800

    Update SECGrid

--- SwiftyEcharts/Models/SECGrid.swift
+++ SwiftyEcharts/Models/SECGrid.swift
@@ -14,46 +14,46 @@ public struct SECGrid : SECBorderable, SECDisplayable, SECShadowable {
     /// 所有图形的 zlevel 值。
     /// zlevel用于 Canvas 分层，不同zlevel值的图形会放置在不同的 Canvas 中，Canvas 分层是一种常见的优化手段。我们可以把一些图形变化频繁（例如有动画）的组件设置成一个单独的zlevel。需要注意的是过多的 Canvas 会引起内存开销的增大，在手机端上需要谨慎使用以防崩溃。
     /// zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面。
-    public var zlevel = 0.0
+    public var zlevel: Float?
     /// 组件的所有图形的z值。控制图形的前后顺序。z值小的图形会被z值大的图形覆盖。
     /// z相比zlevel优先级更低，而且不会创建新的 Canvas。
-    public var z = 2.0
+    public var z: Float?
     /// grid 组件离容器左侧的距离。
-    public var left = SECPosition.auto
+    public var left: SECPosition?
     /// grid 组件离容器上侧的距离。
-    public var top = SECPosition.auto
+    public var top: SECPosition?
     /// grid 组件离容器右侧的距离。
-    public var right = SECPosition.auto
+    public var right: SECPosition?
     /// grid 组件离容器下侧的距离。
-    public var bottom = SECPosition.auto
+    public var bottom: SECPosition?
     /// grid 组件的宽度。默认自适应。
     public var width: Float?
     /// grid 组件的高度。默认自适应。
     public var height: Float?
     /// grid 区域是否包含坐标轴的刻度标签，在无法确定坐标轴标签的宽度，容器又比较小无法预留较多空间的时候，可以设为 true 防止标签溢出容器。
-    public var containLabel = false
+    public var containLabel: Bool?
     /// 网格背景色，默认透明。
     /// - Note: 注意：此配置项生效的前提是，设置了 show: true。
-    public var background = SECColor.transparent
+    public var background: SECColor?
     /// 网格的边框颜色。支持的颜色格式同 backgroundColor。 
     /// - Note: 注意：此配置项生效的前提是，设置了 show: true。
-    public var borderColor: SECColor? = SECColor.hexColor("#ccc")
+    public var borderColor: SECColor?
     /// 网格的边框线宽。
     /// - Note:  注意：此配置项生效的前提是，设置了 show: true。
     public var borderWidth: Float?
     
     /// 图形阴影的模糊大小。该属性配合 shadowColor,shadowOffsetX, shadowOffsetY 一起设置图形的阴影效果
     /// 注意：此配置项生效的前提是，设置了 show: true 以及值不为 tranparent 的背景色 backgroundColor。
-    public var shadowBlur: Float? = 0.0
+    public var shadowBlur: Float?
     /// 阴影颜色
     /// 注意：此配置项生效的前提是，设置了 show: true。
     public var shadowColor: SECColor?
     /// 阴影水平方向上的偏移距离。
     /// 注意：此配置项生效的前提是，设置了 show: true。
-    public var shadowOffsetX: Float? = 0.0
+    public var shadowOffsetX: Float?
     /// 阴影水平方向上的偏移距离。
     /// 注意：此配置项生效的前提是，设置了 show: true。
-    public var shadowOffsetY: Float? = 0.0
+    public var shadowOffsetY: Float?
 
     public init() { }
 }

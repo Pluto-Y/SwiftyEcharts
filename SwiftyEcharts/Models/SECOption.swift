bad02754a322d2183b9327784930d3ce6cd1fc26
Longhua Zhang <kuaileainits@163.com>
2017-01-16 15:05:28 +0800

    Make `SECOption` conform to `SECAnimatable`

--- SwiftyEcharts/Models/SECOption.swift
+++ SwiftyEcharts/Models/SECOption.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 com.pluto-y. All rights reserved.
 //
 
-public struct SECOption {
+public struct SECOption : SECAnimatable {
     public var title: SECTitle?
     public var legend: SECLegend?
     public var grid: SECGrid?
@@ -15,13 +15,53 @@ public struct SECOption {
     public var tooltip: SECTooltip?
     public var series: [SECSeries]?
     
+    /// 是否开启动画。
+    public var animation: Bool?
+    /// 是否开启动画的阈值，当单个系列显示的图形数量大于这个阈值时会关闭动画。
+    public var animationThreshold: Float?
+    /// 初始动画的时长，支持回调函数，可以通过每个数据返回不同的 delay 时间实现更戏剧的初始动画效果：
+    ///
+    ///     animationDuration: function (idx) {
+    ///         // 越往后的数据延迟越大
+    ///         return idx * 100;
+    ///     }
+    public var animationDuration: SECTime?
+    /// 初始动画的缓动效果。不同的缓动效果可以参考
+    public var animationEasing: SECAnimation?
+    /// 初始动画的延迟，支持回调函数，可以通过每个数据返回不同的 delay 时间实现更戏剧的初始动画效果。
+    ///
+    /// 如下示例：
+    ///
+    ///     animationDuration: function (idx) {
+    ///         // 越往后的数据延迟越大
+    ///         return idx * 100;
+    ///     }
+    public var animationDelay: SECTime?
+    /// 数据更新动画的时长。
+    /// 支持回调函数，可以通过每个数据返回不同的 delay 时间实现更戏剧的更新动画效果：
+    ///     animationDurationUpdate: function (idx) {
+    ///         // 越往后的数据延迟越大
+    ///         return idx * 100;
+    ///     }
+    public var animationDurationUpdate: SECTime?
+    /// 数据更新动画的缓动效果。
+    public var animationEasingUpdate: SECAnimation?
+    /// 数据更新动画的延迟，支持回调函数，可以通过每个数据返回不同的 delay 时间实现更戏剧的更新动画效果。
+    /// 如下示例：
+    ///
+    ///     animationDelayUpdate: function (idx) {
+    ///         // 越往后的数据延迟越大
+    ///         return idx * 100;
+    ///     }
+    public var animationDelayUpdate: SECTime?
+    
     public init() { }
 }
 
 extension SECOption : SECEnumable {
     
     public enum Enums {
-        case title(SECTitle), legend(SECLegend), grid(SECGrid), xAxis([SECAxis]), yAxis([SECAxis]), tooltip(SECTooltip), series([SECSeries])
+        case title(SECTitle), legend(SECLegend), grid(SECGrid), xAxis([SECAxis]), yAxis([SECAxis]), tooltip(SECTooltip), series([SECSeries]), animation(Bool), animationThreshold(Float), animationDuration(SECTime), animationEasing(SECAnimation), animationDelay(SECTime), animationDurationUpdate(SECTime), animationEasingUpdate(SECAnimation), animationDelayUpdate(SECTime)
     }
     
     public typealias ContentEnum = Enums
@@ -43,6 +83,22 @@ extension SECOption : SECEnumable {
                 self.tooltip = tooltip
             case let .series(series):
                 self.series = series
+            case let .animation(animation):
+                self.animation = animation
+            case let .animationThreshold(animationThreshold):
+                self.animationThreshold = animationThreshold
+            case let .animationDuration(animationDuration):
+                self.animationDuration = animationDuration
+            case let .animationEasing(animationEasing):
+                self.animationEasing = animationEasing
+            case let .animationDelay(animationDelay):
+                self.animationDelay = animationDelay
+            case let .animationDurationUpdate(animationDurationUpdate):
+                self.animationDurationUpdate = animationDurationUpdate
+            case let .animationEasingUpdate(animationEasingUpdate):
+                self.animationEasingUpdate = animationEasingUpdate
+            case let .animationDelayUpdate(animationDelayUpdate):
+                self.animationDelayUpdate = animationDelayUpdate
             }
         }
     }
@@ -57,5 +113,13 @@ extension SECOption : SECMappable {
         map["yAxis"] = yAxis
         map["tooltip"] = tooltip
         map["series"] = series
+        map["animation"] = animation
+        map["animationThreshold"] = animationThreshold
+        map["animationDuration"] = animationDuration
+        map["animationEasing"] = animationEasing
+        map["animationDelay"] = animationDelay
+        map["animationDurationUpdate"] = animationDurationUpdate
+        map["animationEasingUpdate"] = animationEasingUpdate
+        map["animationDelayUpdate"] = animationDelayUpdate
     }
 }

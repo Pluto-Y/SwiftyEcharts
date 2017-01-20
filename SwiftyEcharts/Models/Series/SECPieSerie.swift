154e7a6b8743f3bdc1fe14a710ab2d01278c93a0
Longhua Zhang <kuaileainits@163.com>
2017-01-20 16:21:12 +0800

    Make `SECPieSerie` and sub-modules conform to `SECEnummable` and `SECMappable`

--- SwiftyEcharts/Models/Series/SECPieSerie.swift
+++ SwiftyEcharts/Models/Series/SECPieSerie.swift
@@ -59,3 +59,173 @@ public struct SECPieSerie : SECSeries, SECAnimatable {
     public var animationDelayUpdate: SECTime?
 }
 
+extension SECPieSerie.LabelLineContent : SECEnumable {
+    public enum Enums {
+        case show(Bool), length(Float), length2(Float), smooth(Bool), lineStyle(SECCommonLineStyle)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: [Enums]) {
+        for ele in elements {
+            switch ele {
+            case let .show(show):
+                self.show = show
+            case let .length(length):
+                self.length = length
+            case let .length2(length2):
+                self.length2 = length2
+            case let .smooth(smooth):
+                self.smooth = smooth
+            case let .lineStyle(lineStyle):
+                self.lineStyle = lineStyle
+            }
+        }
+    }
+}
+
+extension SECPieSerie.LabelLineContent : SECMappable {
+    public func mapping(map: SECMap) {
+        map["show"] = show
+        map["length"] = length
+        map["length2"] = length2
+        map["smooth"] = smooth
+        map["lineStyle"] = lineStyle
+    }
+}
+
+extension SECPieSerie.LabelLine : SECEnumable {
+    public enum Enums {
+        case normal(Style), emphasis(Style)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: [Enums]) {
+        for ele in elements {
+            switch ele {
+            case let .normal(normal):
+                self.normal = normal
+            case let .emphasis(emphasis):
+                self.emphasis = emphasis
+            }
+        }
+    }
+}
+
+extension SECPieSerie.LabelLine : SECMappable {
+    public func mapping(map: SECMap) {
+        map["normal"] = normal
+        map["emphasis"] = emphasis
+    }
+}
+
+extension SECPieSerie : SECEnumable {
+    public enum Enums {
+        case name(String), legendHoverLink(Bool), hoverAnimation(Bool), selectedMode(SECSelectedMode), clockwise(Bool), startAngle(Float), minAngle(Float), roseType(Bool), avoidLabelOverlap(Bool), stillShowZeroSum(Bool), label(SECFormattedLabel), labelLine(LabelLine), itemStyle(SECItemStyle), zlevel(Float), z(Float), center([Float]), radius([Float]), data([Any]), markPoint(SECMarkPoint), markLine(SECMarkLine), markArea(SECMarkArea), silent(Bool), animation(Bool), animationThreshold(Float), animationDuration(SECTime), animationEasing(SECAnimation), animationDelay(SECTime), animationDurationUpdate(SECTime), animationEasingUpdate(SECAnimation), animationDelayUpdate(SECTime)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: [Enums]) {
+        for ele in elements {
+            switch ele {
+            case let .name(name):
+                self.name = name
+            case let .legendHoverLink(legendHoverLink):
+                self.legendHoverLink = legendHoverLink
+            case let .hoverAnimation(hoverAnimation):
+                self.hoverAnimation = hoverAnimation
+            case let .selectedMode(selectedMode):
+                self.selectedMode = selectedMode
+            case let .clockwise(clockwise):
+                self.clockwise = clockwise
+            case let .startAngle(startAngle):
+                self.startAngle = startAngle
+            case let .minAngle(minAngle):
+                self.minAngle = minAngle
+            case let .roseType(roseType):
+                self.roseType = roseType
+            case let .avoidLabelOverlap(avoidLabelOverlap):
+                self.avoidLabelOverlap = avoidLabelOverlap
+            case let .stillShowZeroSum(stillShowZeroSum):
+                self.stillShowZeroSum = stillShowZeroSum
+            case let .label(label):
+                self.label = label
+            case let .labelLine(labelLine):
+                self.labelLine = labelLine
+            case let .itemStyle(itemStyle):
+                self.itemStyle = itemStyle
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .z(z):
+                self.z = z
+            case let .center(center):
+                self.center = center
+            case let .radius(radius):
+                self.radius = radius
+            case let .data(data):
+                self.data = data
+            case let .markPoint(markPoint):
+                self.markPoint = markPoint
+            case let .markLine(markLine):
+                self.markLine = markLine
+            case let .markArea(markArea):
+                self.markArea = markArea
+            case let .silent(silent):
+                self.silent = silent
+            case let .animation(animation):
+                self.animation = animation
+            case let .animationThreshold(animationThreshold):
+                self.animationThreshold = animationThreshold
+            case let .animationDuration(animationDuration):
+                self.animationDuration = animationDuration
+            case let .animationEasing(animationEasing):
+                self.animationEasing = animationEasing
+            case let .animationDelay(animationDelay):
+                self.animationDelay = animationDelay
+            case let .animationDurationUpdate(animationDurationUpdate):
+                self.animationDurationUpdate = animationDurationUpdate
+            case let .animationEasingUpdate(animationEasingUpdate):
+                self.animationEasingUpdate = animationEasingUpdate
+            case let .animationDelayUpdate(animationDelayUpdate):
+                self.animationDelayUpdate = animationDelayUpdate
+            }
+        }
+    }
+}
+
+extension SECPieSerie : SECMappable {
+    public func mapping(map: SECMap) {
+        map["type"] = type
+        map["name"] = name
+        map["legendHoverLink"] = legendHoverLink
+        map["selectedMode"] = selectedMode
+        map["clockwise"] = clockwise
+        map["startAngle"] = startAngle
+        map["minAngle"] = minAngle
+        map["roseType"] = roseType
+        map["avoidLabelOverlap"] = avoidLabelOverlap
+        map["stillShowZeroSum"] = stillShowZeroSum
+        map["label"] = label
+        map["labelLine"] = labelLine
+        map["itemStyle"] = itemStyle
+        map["zlevel"] = zlevel
+        map["z"] = z
+        map["center"] = center
+        map["radius"] = radius
+        map["markPoint"] = markPoint
+        map["markLine"] = markLine
+        map["markArea"] = markArea
+        map["silent"] = silent
+        map["animation"] = animation
+        map["animationThreshold"] = animationThreshold
+        map["animationDuration"] = animationDuration
+        map["animationEasing"] = animationEasing
+        map["animationDelay"] = animationDelay
+        map["animationDurationUpdate"] = animationDurationUpdate
+        map["animationEasingUpdate"] = animationEasingUpdate
+        map["animationDelayUpdate"] = animationDelayUpdate
+    }
+}
+

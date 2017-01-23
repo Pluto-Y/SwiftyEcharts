79442a868685cff601a24fffd6769e1fb29f0a4d
Longhua Zhang <kuaileainits@163.com>
2017-01-23 23:39:34 +0800

    Fill most properties in `SECPiecewiseVisualMap`

--- SwiftyEcharts/Models/VisualMap/SECPiecewiseVisualMap.swift
+++ SwiftyEcharts/Models/VisualMap/SECPiecewiseVisualMap.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 com.pluto-y. All rights reserved.
 //
 
-public struct SECPiecewiseVisualMap {
+public struct SECPiecewiseVisualMap : SECVisualMap, SECDisplayable, SECBorderable, SECColorful, SECTextful, SECFormatted {
     public var type: String {
         return "piecewise"
     }
@@ -14,4 +14,40 @@ public struct SECPiecewiseVisualMap {
     public var splitNumber: Int?
     public var pieces: [SECJsonable]?
 //    public var categories: []
+    public var min: Float?
+    public var max: Float?
+    public var minOpen: Bool?
+    public var maxOpen: Bool?
+    public var selectedMode: SECSelectedMode?
+    public var inverse: Bool?
+    public var precision: UInt?
+    public var itemWidth: Float?
+    public var itemHeight: Float?
+    public var align: SECAlign?
+    public var text: [String]?
+    public var textGap: Float?
+    public var showLabel: Bool?
+    public var itemGap: Float?
+    public var itemSymbol: SECSymbol?
+    public var show: Bool?
+    public var dimension: Float?
+    public var seriesIndex: [UInt8]?
+    public var hoverLink: Bool?
+    public var inRange: [String: SECJsonable]?
+    public var outRange: [String: SECJsonable]?
+    public var controller: SECVMController?
+    public var zlevel: Float?
+    public var z: Float?
+    public var left: SECPosition?
+    public var top: SECPosition?
+    public var right: SECPosition?
+    public var bottom: SECPosition?
+    public var orient: SECOrient?
+    public var padding: SECPadding?
+    public var backgroundColor: SECColor?
+    public var borderColor: SECColor?
+    public var borderWidth: Float?
+    public var color: SECColor?
+    public var textStyle: SECTextStyle?
+    public var formatter: SECFormatter?
 }

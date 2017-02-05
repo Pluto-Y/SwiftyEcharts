95034b3aa4c3a3d633b6e70321a335d8aeec59e5
Longhua Zhang <kuaileainits@163.com>
2017-02-05 23:53:31 +0800

    Add `SECRadiusAxis` conform to `SECEnumable`

--- SwiftyEcharts/Models/SECRadiusAxis.swift
+++ SwiftyEcharts/Models/SECRadiusAxis.swift
@@ -55,14 +55,62 @@ extension SECRadiusAxis : SECEnumable {
         case polarIndex(UInt8), type(Type), name(String), nameLocation(SECPosition), nameTextStyle(SECTextStyle), nameGap(Float), nameRotate(Float), inverse(Bool), boundaryGap([Float]), min(Float), max(Float), scale(Bool), splitNumber(UInt8), minInterval(Float), interval(Float), logBase(Float), silent(Bool), triggerEvent(Bool), axisLine(SECAxisLine), axisTick(SECAxisTick), axisLabel(SECAxisLabel), splitLine(SECSplitLine), splitArea(SECSplitArea), zlevel(Float), z(Float)
     }
     
-    
     public typealias ContentEnum = Enums
     
     public init(_ elements: [Enums]) {
-        // for ele in elements {
-        //     switch ele {
-        //         
-        //     }
-        // }
+        for ele in elements {
+            switch ele {
+            case let .polarIndex(polarIndex):
+                self.polarIndex = polarIndex
+            case let .type(type):
+                self.type = type
+            case let .name(name):
+                self.name = name
+            case let .nameLocation(nameLocation):
+                self.nameLocation = nameLocation
+            case let .nameTextStyle(nameTextStyle):
+                self.nameTextStyle = nameTextStyle
+            case let .nameGap(nameGap):
+                self.nameGap = nameGap
+            case let .nameRotate(nameRotate):
+                self.nameRotate = nameRotate
+            case let .inverse(inverse):
+                self.inverse = inverse
+            case let .boundaryGap(boundaryGap):
+                self.boundaryGap = boundaryGap
+            case let .min(min):
+                self.min = min
+            case let .max(max):
+                self.max = max
+            case let .scale(scale):
+                self.scale = scale
+            case let .splitNumber(splitNumber):
+                self.splitNumber = splitNumber
+            case let .minInterval(minInterval):
+                self.minInterval = minInterval
+            case let .interval(interval):
+                self.interval = interval
+            case let .logBase(logBase):
+                self.logBase = logBase
+            case let .silent(silent):
+                self.silent = silent
+            case let .triggerEvent(triggerEvent):
+                self.triggerEvent = triggerEvent
+            case let .axisLine(axisLine):
+                self.axisLine = axisLine
+            case let .axisTick(axisTick):
+                self.axisTick = axisTick
+            case let .axisLabel(axisLabel):
+                self.axisLabel = axisLabel
+            case let .splitLine(splitLine):
+                self.splitLine = splitLine
+            case let .splitArea(splitArea):
+                self.splitArea = splitArea
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .z(z):
+                self.z = z
+            }
+        }
     }
 }

a831fa3e1329ed4f5c1524eaa0dbbd96c47f4cfc
Longhua Zhang <kuaileainits@163.com>
2017-02-06 22:32:46 +0800

    Move `SECSplitArea` into new swift file as a common module

--- SwiftyEcharts/Models/Axis/SECAxis.swift
+++ SwiftyEcharts/Models/Axis/SECAxis.swift
@@ -19,45 +19,6 @@ public enum SECBoundaryGap : SECJsonable {
     case notCategory(Float, Float)
 }
 
-/// 分割区域
-public struct SECSplitArea : SECDisplayable {
-    
-    public var show: Bool?
-    public var interval: UInt?
-    public var areaStyle: SECAreaStyle?
-    
-    public init() { }
-}
-
-extension SECSplitArea : SECEnumable {
-    public enum Enums {
-        case show(Bool), interval(UInt), areaStyle(SECAreaStyle)
-    }
-    
-    public typealias ContentEnum = Enums
-    
-    public init(_ elements: [Enums]) {
-        for ele in elements {
-            switch ele {
-            case let .show(show):
-                self.show = show
-            case let .interval(interval):
-                self.interval = interval
-            case let .areaStyle(areaStyle):
-                self.areaStyle = areaStyle
-            }
-        }
-    }
-}
-
-extension SECSplitArea : SECMappable {
-    public func mapping(map: SECMap) {
-        map["show"] = show
-        map["interval"] = interval
-        map["areaStyle"] = areaStyle
-    }
-}
-
 /// 坐标轴的定义
 public struct SECAxis {
 

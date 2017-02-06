23452bd878b13a5a412abda543c495d8aa7f9813
Longhua Zhang <kuaileainits@163.com>
2017-02-06 22:29:07 +0800

    Move `SECAxisLine` into new swift file as a common module

--- SwiftyEcharts/Models/Axis/SECAxis.swift
+++ SwiftyEcharts/Models/Axis/SECAxis.swift
@@ -19,45 +19,6 @@ public enum SECBoundaryGap : SECJsonable {
     case notCategory(Float, Float)
 }
 
-/// 坐标轴轴线相关设置。
-public struct SECAxisLine : SECDisplayable, SECLine {
-    public var show: Bool?
-    /// X 轴或者 Y 轴的轴线是否在另一个轴的 0 刻度上，只有在另一个轴为数值轴且包含 0 刻度时有效。
-    public var onZero: Bool?
-    public var lineStyle: SECLineStyle?
-    
-    public init() { }
-
-}
-
-extension SECAxisLine : SECEnumable {
-    public enum Enums {
-        case show(Bool), onZero(Bool), lineStyle(SECLineStyle)
-    }
-    
-    public typealias ContentEnum = Enums
-    
-    public init(_ elements: [Enums]) {
-        for ele in elements {
-            switch ele {
-            case let .show(show):
-                self.show = show
-            case let .onZero(onZero):
-                self.onZero = onZero
-            case let .lineStyle(lineStyle):
-                self.lineStyle = lineStyle
-            }
-        }
-    }
-}
-
-extension SECAxisLine : SECMappable {
-    public func mapping(map: SECMap) {
-        map["show"] = show
-        map["onZero"] = onZero
-        map["lineStyle"] = lineStyle
-    }
-}
 /// 坐标轴刻度相关设置。
 public struct SECAxisTick : SECLine, SECDisplayable {
     /// 是否显示坐标轴刻度。

6c89460bb25479b5c5a741feb70695c0bee35d91
Longhua Zhang <kuaileainits@163.com>
2017-02-06 22:34:36 +0800

    Move `SECSplitLine` into new swift file as a common module

---
+++ SwiftyEcharts/Models/Axis/SECSplitLine.swift
@@ -0,0 +1,48 @@
+//
+//  SECSplitLine.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 06/02/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+/// 分割线
+public struct SECSplitLine: SECDisplayable, SECLine {
+    /// 是否显示分隔线。
+    public var show: Bool?
+    /// 分隔线的显示间隔
+    public var interval: UInt?
+    /// 分割线的样式
+    public var lineStyle: SECLineStyle?
+    
+    public init() { }
+}
+
+extension SECSplitLine : SECEnumable {
+    public enum Enums {
+        case show(Bool), interval(UInt), lineStyle(SECLineStyle)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: [Enums]) {
+        for ele in elements {
+            switch ele {
+            case let .show(show):
+                self.show = show
+            case let .interval(interval):
+                self.interval = interval
+            case let .lineStyle(lineStyle):
+                self.lineStyle = lineStyle
+            }
+        }
+    }
+}
+
+extension SECSplitLine : SECMappable {
+    public func mapping(map: SECMap) {
+        map["show"] = show
+        map["interval"] = interval
+        map["lineStyle"] = lineStyle
+    }
+}

91e7126570e43f6115d5c5a192697553343d17b9
Longhua Zhang <kuaileainits@163.com>
2017-02-10 17:43:10 +0800

    Implement for `SECParallelAxis'

--- SwiftyEcharts/Models/Axis/SECParallelAxis.swift
+++ SwiftyEcharts/Models/Axis/SECParallelAxis.swift
@@ -7,5 +7,53 @@
 //
 
 public struct SECParallelAxis {
-    // TODO: 添加实现代码
+    
+    public struct AreaSelectStyle : SECColorful, SECOpacitable {
+        public var width: Float?
+        public var borderWidth: Float?
+        public var borderColor: SECColor?
+        public var color: SECColor?
+        public var opacity: Float?
+    }
+    
+    /// <#Description#>
+    ///
+    /// - start: <#start description#>
+    /// - middle: <#middle description#>
+    /// - end: <#end description#>
+    public enum NameLocation : String, SECJsonable {
+        case start = "start"
+        case middle = "middle"
+        case end = "end"
+        
+        public var jsonString: String {
+            return "\"\(self.rawValue)\""
+        }
+    }
+    
+    public var dim: Float?
+    public var parallelIndex: UInt8?
+    public var realtiem: Bool?
+    public var areaSelectedStyle: AreaSelectStyle?
+    public var type: SECAxisType?
+    public var name: String?
+    public var nameTextStyle: SECTextStyle?
+    public var nameLocation: NameLocation?
+    public var nameGap: Float?
+    public var nameRotate: Float?
+    public var inverse: Bool?
+    public var boudaryGap: SECBoundaryGap?
+    public var min: Float?
+    public var max: Float?
+    public var scale: Bool?
+    public var splitName: UInt8?
+    public var minInterval: UInt8?
+    public var interval: Int?
+    public var logBase: Float?
+    public var silent: Bool?
+    public var triggerEvent: Bool?
+    public var axisLine: SECAxisLine?
+    public var axisTick: SECAxisTick?
+    public var axisLabel: SECAxisLabel?
+    public var data: [Any]?
 }

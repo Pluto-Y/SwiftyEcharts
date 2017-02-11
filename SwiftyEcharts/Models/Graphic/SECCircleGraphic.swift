45943b1fb9a26229e26dbf25cc5d6ac8f22030ac
Longhua Zhang <kuaileainits@163.com>
2017-02-11 23:30:46 +0800

    Make `SECCircleGraphic` conform to `SECEnumable` and `SECMappable` , then re-structure code

--- SwiftyEcharts/Models/Graphic/SECCircleGraphic.swift
+++ SwiftyEcharts/Models/Graphic/SECCircleGraphic.swift
@@ -15,7 +15,7 @@ public struct SECCircleGraphic : SECGraphic {
         
         public init() {}
     }
-   
+    
     public var type: SECGraphicType {
         return .circle
     }
@@ -40,7 +40,99 @@ public struct SECCircleGraphic : SECGraphic {
     public init() {}
 }
 
+extension SECCircleGraphic : SECEnumable {
+    public enum Enums {
+        case id(String), action(SECGraphicAction), left(SECPosition), right(SECPosition), top(SECPosition), bottom(SECPosition), bounding(SECGraphicBounding), z(Float), zlevel(Float), silent(Bool), invisible(Bool), cursor(String), draggable(Bool), progressiv(Bool), shape(Shape), style(SECCommonGraphicStyle)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .id(id):
+                self.id = id
+            case let .action(action):
+                self.action = action
+            case let .left(left):
+                self.left = left
+            case let .right(right):
+                self.right = right
+            case let .top(top):
+                self.top = top
+            case let .bottom(bottom):
+                self.bottom = bottom
+            case let .bounding(bounding):
+                self.bounding = bounding
+            case let .z(z):
+                self.z = z
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .silent(silent):
+                self.silent = silent
+            case let .invisible(invisible):
+                self.invisible = invisible
+            case let .cursor(cursor):
+                self.cursor = cursor
+            case let .draggable(draggable):
+                self.draggable = draggable
+            case let .progressiv(progressiv):
+                self.progressiv = progressiv
+            case let .shape(shape):
+                self.shape = shape
+            case let .style(style):
+                self.style = style
+            }
+        }
+    }
+}
+
 extension SECCircleGraphic : SECMappable {
     public func mapping(map: SECMap) {
+        map["id"] = id
+        map["action"] = action
+        map["left"] = left
+        map["right"] = right
+        map["top"] = top
+        map["bottom"] = bottom
+        map["bounding"] = bounding
+        map["z"] = z
+        map["zlevel"] = zlevel
+        map["silent"] = silent
+        map["invisible"] = invisible
+        map["cursor"] = cursor
+        map["draggable"] = draggable
+        map["progressiv"] = progressiv
+        map["shape"] = shape
+        map["style"] = style
+    }
+}
+
+extension SECCircleGraphic.Shape : SECEnumable {
+    public enum Enums {
+        case cx(Float), cy(Float), r(Float)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .cx(cx):
+                self.cx = cx
+            case let .cy(cy):
+                self.cy = cy
+            case let .r(r):
+                self.r = r
+            }
+        }
+    }
+}
+
+extension SECCircleGraphic.Shape : SECMappable {
+    public func mapping(map: SECMap) {
+        map["cx"] = cx
+        map["cy"] = cy
+        map["r"] = r
     }
 }

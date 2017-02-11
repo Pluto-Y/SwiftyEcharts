4eec1204dc406a4a9691206efa79a3321e1713d1
Longhua Zhang <kuaileainits@163.com>
2017-02-11 20:56:22 +0800

    Re-structure code

--- SwiftyEcharts/Models/Graphic/SECImageGraphic.swift
+++ SwiftyEcharts/Models/Graphic/SECImageGraphic.swift
@@ -49,122 +49,122 @@ public struct SECImageGraphic : SECGraphic {
 }
 
 extension SECImageGraphic : SECEnumable {
-	public enum Enums {
-		case id(String), action(SECGraphicAction), left(SECPosition), right(SECPosition), top(SECPosition), bottom(SECPosition), bounding(SECGraphicBounding), z(Float), zlevel(Float), silent(Bool), invisible(Bool), cursor(String), draggable(Bool), progressiv(Bool), style(Style)
-	}
-
-	public typealias ContentEnum = Enums
-
-	public init(_ elements: Enums...) {
-		for ele in elements {
-			switch ele {
-				case let .id(id):
-					self.id = id
-				case let .action(action):
-					self.action = action
-				case let .left(left):
-					self.left = left
-				case let .right(right):
-					self.right = right
-				case let .top(top):
-					self.top = top
-				case let .bottom(bottom):
-					self.bottom = bottom
-				case let .bounding(bounding):
-					self.bounding = bounding
-				case let .z(z):
-					self.z = z
-				case let .zlevel(zlevel):
-					self.zlevel = zlevel
-				case let .silent(silent):
-					self.silent = silent
-				case let .invisible(invisible):
-					self.invisible = invisible
-				case let .cursor(cursor):
-					self.cursor = cursor
-				case let .draggable(draggable):
-					self.draggable = draggable
-				case let .progressiv(progressiv):
-					self.progressiv = progressiv
-				case let .style(style):
-					self.style = style
-			}
-		}
-	}
+    public enum Enums {
+        case id(String), action(SECGraphicAction), left(SECPosition), right(SECPosition), top(SECPosition), bottom(SECPosition), bounding(SECGraphicBounding), z(Float), zlevel(Float), silent(Bool), invisible(Bool), cursor(String), draggable(Bool), progressiv(Bool), style(Style)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .id(id):
+                self.id = id
+            case let .action(action):
+                self.action = action
+            case let .left(left):
+                self.left = left
+            case let .right(right):
+                self.right = right
+            case let .top(top):
+                self.top = top
+            case let .bottom(bottom):
+                self.bottom = bottom
+            case let .bounding(bounding):
+                self.bounding = bounding
+            case let .z(z):
+                self.z = z
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .silent(silent):
+                self.silent = silent
+            case let .invisible(invisible):
+                self.invisible = invisible
+            case let .cursor(cursor):
+                self.cursor = cursor
+            case let .draggable(draggable):
+                self.draggable = draggable
+            case let .progressiv(progressiv):
+                self.progressiv = progressiv
+            case let .style(style):
+                self.style = style
+            }
+        }
+    }
 }
 
 extension SECImageGraphic : SECMappable {
-	public func mapping(map: SECMap) {
-		map["id"] = id
-		map["action"] = action
-		map["left"] = left
-		map["right"] = right
-		map["top"] = top
-		map["bottom"] = bottom
-		map["bounding"] = bounding
-		map["z"] = z
-		map["zlevel"] = zlevel
-		map["silent"] = silent
-		map["invisible"] = invisible
-		map["cursor"] = cursor
-		map["draggable"] = draggable
-		map["progressiv"] = progressiv
-		map["style"] = style
-	}
+    public func mapping(map: SECMap) {
+        map["id"] = id
+        map["action"] = action
+        map["left"] = left
+        map["right"] = right
+        map["top"] = top
+        map["bottom"] = bottom
+        map["bounding"] = bounding
+        map["z"] = z
+        map["zlevel"] = zlevel
+        map["silent"] = silent
+        map["invisible"] = invisible
+        map["cursor"] = cursor
+        map["draggable"] = draggable
+        map["progressiv"] = progressiv
+        map["style"] = style
+    }
 }
 
 extension SECImageGraphic.Style : SECEnumable {
-	public enum Enums {
-		case image(String), x(Float), y(Float), width(Float), height(Float), fill(SECColor), stroke(SECColor), lineWidth(Float), shadowBlur(Float), shadowOffsetX(Float), shadowOffsetY(Float), shadowColor(SECColor)
-	}
-
-	public typealias ContentEnum = Enums
-
-	public init(_ elements: Enums...) {
-		for ele in elements {
-			switch ele {
-				case let .image(image):
-					self.image = image
-				case let .x(x):
-					self.x = x
-				case let .y(y):
-					self.y = y
-				case let .width(width):
-					self.width = width
-				case let .height(height):
-					self.height = height
-				case let .fill(fill):
-					self.fill = fill
-				case let .stroke(stroke):
-					self.stroke = stroke
-				case let .lineWidth(lineWidth):
-					self.lineWidth = lineWidth
-				case let .shadowBlur(shadowBlur):
-					self.shadowBlur = shadowBlur
-				case let .shadowOffsetX(shadowOffsetX):
-					self.shadowOffsetX = shadowOffsetX
-				case let .shadowOffsetY(shadowOffsetY):
-					self.shadowOffsetY = shadowOffsetY
-				case let .shadowColor(shadowColor):
-					self.shadowColor = shadowColor
-			}
-		}
-	}
+    public enum Enums {
+        case image(String), x(Float), y(Float), width(Float), height(Float), fill(SECColor), stroke(SECColor), lineWidth(Float), shadowBlur(Float), shadowOffsetX(Float), shadowOffsetY(Float), shadowColor(SECColor)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .image(image):
+                self.image = image
+            case let .x(x):
+                self.x = x
+            case let .y(y):
+                self.y = y
+            case let .width(width):
+                self.width = width
+            case let .height(height):
+                self.height = height
+            case let .fill(fill):
+                self.fill = fill
+            case let .stroke(stroke):
+                self.stroke = stroke
+            case let .lineWidth(lineWidth):
+                self.lineWidth = lineWidth
+            case let .shadowBlur(shadowBlur):
+                self.shadowBlur = shadowBlur
+            case let .shadowOffsetX(shadowOffsetX):
+                self.shadowOffsetX = shadowOffsetX
+            case let .shadowOffsetY(shadowOffsetY):
+                self.shadowOffsetY = shadowOffsetY
+            case let .shadowColor(shadowColor):
+                self.shadowColor = shadowColor
+            }
+        }
+    }
 }
 
 extension SECImageGraphic.Style : SECMappable {
-	public func mapping(map: SECMap) {
-		map["image"] = image
-		map["x"] = x
-		map["y"] = y
-		map["width"] = width
-		map["height"] = height
-		map["fill"] = fill
-		map["stroke"] = stroke
-		map["lineWidth"] = lineWidth
-		map["shadowBlur"] = shadowBlur
-		map["shadowOffsetX"] = shadowOffsetX
-		map["shadowOffsetY"] = shadowOffsetY
-		map["shadowColor"] = shadowColor
-	}
+    public func mapping(map: SECMap) {
+        map["image"] = image
+        map["x"] = x
+        map["y"] = y
+        map["width"] = width
+        map["height"] = height
+        map["fill"] = fill
+        map["stroke"] = stroke
+        map["lineWidth"] = lineWidth
+        map["shadowBlur"] = shadowBlur
+        map["shadowOffsetX"] = shadowOffsetX
+        map["shadowOffsetY"] = shadowOffsetY
+        map["shadowColor"] = shadowColor
+    }
 }

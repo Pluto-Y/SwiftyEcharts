9f53e040588ce2349129e71d4d3f07753af744c8
Longhua Zhang <kuaileainits@163.com>
2017-02-11 19:42:15 +0800

    Make `SECGroupGraphic` conform to `SECEnumable` and `SECMappable`

--- SwiftyEcharts/Models/Graphic/SECGroupGraphic.swift
+++ SwiftyEcharts/Models/Graphic/SECGroupGraphic.swift
@@ -31,8 +31,73 @@ public struct SECGroupGraphic : SECGraphic {
     
 }
 
+extension SECGroupGraphic : SECEnumable {
+    public enum Enums {
+        case id(String), action(SECGraphicAction), left(SECPosition), right(SECPosition), top(SECPosition), bottom(SECPosition), bounding(SECGraphicBounding), z(Float), zlevel(Float), silent(Bool), invisible(Bool), cursor(String), draggable(Bool), progressiv(Bool), width(Float), height(Float), children([SECGraphic])
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .id(id):
+                self.id = id
+            case let .action(action):
+                self.action = action
+            case let .left(left):
+                self.left = left
+            case let .right(right):
+                self.right = right
+            case let .top(top):
+                self.top = top
+            case let .bottom(bottom):
+                self.bottom = bottom
+            case let .bounding(bounding):
+                self.bounding = bounding
+            case let .z(z):
+                self.z = z
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .silent(silent):
+                self.silent = silent
+            case let .invisible(invisible):
+                self.invisible = invisible
+            case let .cursor(cursor):
+                self.cursor = cursor
+            case let .draggable(draggable):
+                self.draggable = draggable
+            case let .progressiv(progressiv):
+                self.progressiv = progressiv
+            case let .width(width):
+                self.width = width
+            case let .height(height):
+                self.height = height
+            case let .children(children):
+                self.children = children
+            }
+        }
+    }
+}
+
 extension SECGroupGraphic : SECMappable {
     public func mapping(map: SECMap) {
-        // TODO: 添加实现代码
+        map["id"] = id
+        map["action"] = action
+        map["left"] = left
+        map["right"] = right
+        map["top"] = top
+        map["bottom"] = bottom
+        map["bounding"] = bounding
+        map["z"] = z
+        map["zlevel"] = zlevel
+        map["silent"] = silent
+        map["invisible"] = invisible
+        map["cursor"] = cursor
+        map["draggable"] = draggable
+        map["progressiv"] = progressiv
+        map["width"] = width
+        map["height"] = height
+        map["children"] = children
     }
 }

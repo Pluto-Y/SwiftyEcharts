488041dc308c48b3503e9da9b112e90c05dca3e0
Longhua Zhang <kuaileainits@163.com>
2017-02-11 22:10:16 +0800

    Make `SECTextGraphic` conform to `SECEnumable` and `SECMappable`

--- SwiftyEcharts/Models/Graphic/SECTextGraphic.swift
+++ SwiftyEcharts/Models/Graphic/SECTextGraphic.swift
@@ -49,7 +49,126 @@ public struct SECTextGraphic : SECGraphic {
     public init() {}
 }
 
+extension SECTextGraphic : SECEnumable {
+    public enum Enums {
+        case id(String), action(SECGraphicAction), left(SECPosition), right(SECPosition), top(SECPosition), bottom(SECPosition), bounding(SECGraphicBounding), z(Float), zlevel(Float), silent(Bool), invisible(Bool), cursor(String), draggable(Bool), progressiv(Bool), style(Style)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .id(id):
+                self.id = id
+            case let .action(action):
+                self.action = action
+            case let .left(left):
+                self.left = left
+            case let .right(right):
+                self.right = right
+            case let .top(top):
+                self.top = top
+            case let .bottom(bottom):
+                self.bottom = bottom
+            case let .bounding(bounding):
+                self.bounding = bounding
+            case let .z(z):
+                self.z = z
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .silent(silent):
+                self.silent = silent
+            case let .invisible(invisible):
+                self.invisible = invisible
+            case let .cursor(cursor):
+                self.cursor = cursor
+            case let .draggable(draggable):
+                self.draggable = draggable
+            case let .progressiv(progressiv):
+                self.progressiv = progressiv
+            case let .style(style):
+                self.style = style
+            }
+        }
+    }
+}
+
 extension SECTextGraphic : SECMappable {
     public func mapping(map: SECMap) {
+        map["id"] = id
+        map["action"] = action
+        map["left"] = left
+        map["right"] = right
+        map["top"] = top
+        map["bottom"] = bottom
+        map["bounding"] = bounding
+        map["z"] = z
+        map["zlevel"] = zlevel
+        map["silent"] = silent
+        map["invisible"] = invisible
+        map["cursor"] = cursor
+        map["draggable"] = draggable
+        map["progressiv"] = progressiv
+        map["style"] = style
+    }
+}
+
+extension SECTextGraphic.Style : SECEnumable {
+    public enum Enums {
+        case text(String), x(Float), y(Float), font(String), textAlign(SECAlign), textVertical(SECVerticalAlign), fill(SECColor), stroke(SECColor), lineWidth(Float), shadowBlur(Float), shadowOffsetX(Float), shadowOffsetY(Float), shadowColor(SECColor)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .text(text):
+                self.text = text
+            case let .x(x):
+                self.x = x
+            case let .y(y):
+                self.y = y
+            case let .font(font):
+                self.font = font
+            case let .textAlign(textAlign):
+                self.textAlign = textAlign
+            case let .textVertical(textVertical):
+                self.textVertical = textVertical
+            case let .fill(fill):
+                self.fill = fill
+            case let .stroke(stroke):
+                self.stroke = stroke
+            case let .lineWidth(lineWidth):
+                self.lineWidth = lineWidth
+            case let .shadowBlur(shadowBlur):
+                self.shadowBlur = shadowBlur
+            case let .shadowOffsetX(shadowOffsetX):
+                self.shadowOffsetX = shadowOffsetX
+            case let .shadowOffsetY(shadowOffsetY):
+                self.shadowOffsetY = shadowOffsetY
+            case let .shadowColor(shadowColor):
+                self.shadowColor = shadowColor
+            }
+        }
+    }
+}
+
+extension SECTextGraphic.Style : SECMappable {
+    public func mapping(map: SECMap) {
+        map["text"] = text
+        map["x"] = x
+        map["y"] = y
+        map["font"] = font
+        map["textAlign"] = textAlign
+        map["textVertical"] = textVertical
+        map["fill"] = fill
+        map["stroke"] = stroke
+        map["lineWidth"] = lineWidth
+        map["shadowBlur"] = shadowBlur
+        map["shadowOffsetX"] = shadowOffsetX
+        map["shadowOffsetY"] = shadowOffsetY
+        map["shadowColor"] = shadowColor
     }
 }

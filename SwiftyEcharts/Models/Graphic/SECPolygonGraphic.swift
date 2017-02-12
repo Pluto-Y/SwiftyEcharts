8c8fad1eef3de3535f8fb7079792700ceab8b881
Longhua Zhang <kuaileainits@163.com>
2017-02-12 23:13:40 +0800

    Make `SECPolygonGraphic` conform to `SECEnumable` and `SECMappable`

--- SwiftyEcharts/Models/Graphic/SECPolygonGraphic.swift
+++ SwiftyEcharts/Models/Graphic/SECPolygonGraphic.swift
@@ -23,6 +23,7 @@ public struct SECPolygonGraphic : SECGraphic {
                 }
             }
         }
+        
         public var point: [[Float]]?
         public var smooth: Smooth?
         public var smoothConstraint: Bool?
@@ -54,7 +55,100 @@ public struct SECPolygonGraphic : SECGraphic {
     public init() {}
 }
 
+extension SECPolygonGraphic.Shape : SECEnumable {
+    public enum Enums {
+        case point([[Float]]), smooth(Smooth), smoothConstraint(Bool)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .point(point):
+                self.point = point
+            case let .smooth(smooth):
+                self.smooth = smooth
+            case let .smoothConstraint(smoothConstraint):
+                self.smoothConstraint = smoothConstraint
+            }
+        }
+    }
+}
+
+extension SECPolygonGraphic.Shape : SECMappable {
+    public func mapping(map: SECMap) {
+        map["point"] = point
+        map["smooth"] = smooth
+        map["smoothConstraint"] = smoothConstraint
+    }
+}
+
+extension SECPolygonGraphic : SECEnumable {
+    public enum Enums {
+        case id(String), action(SECGraphicAction), left(SECPosition), right(SECPosition), top(SECPosition), bottom(SECPosition), bounding(SECGraphicBounding), z(Float), zlevel(Float), silent(Bool), invisible(Bool), cursor(String), draggable(Bool), progressiv(Bool), shape(Shape), style(SECCommonGraphicStyle)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .id(id):
+                self.id = id
+            case let .action(action):
+                self.action = action
+            case let .left(left):
+                self.left = left
+            case let .right(right):
+                self.right = right
+            case let .top(top):
+                self.top = top
+            case let .bottom(bottom):
+                self.bottom = bottom
+            case let .bounding(bounding):
+                self.bounding = bounding
+            case let .z(z):
+                self.z = z
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .silent(silent):
+                self.silent = silent
+            case let .invisible(invisible):
+                self.invisible = invisible
+            case let .cursor(cursor):
+                self.cursor = cursor
+            case let .draggable(draggable):
+                self.draggable = draggable
+            case let .progressiv(progressiv):
+                self.progressiv = progressiv
+            case let .shape(shape):
+                self.shape = shape
+            case let .style(style):
+                self.style = style
+            }
+        }
+    }
+}
+
 extension SECPolygonGraphic : SECMappable {
     public func mapping(map: SECMap) {
+        map["type"] = type
+        map["id"] = id
+        map["action"] = action
+        map["left"] = left
+        map["right"] = right
+        map["top"] = top
+        map["bottom"] = bottom
+        map["bounding"] = bounding
+        map["z"] = z
+        map["zlevel"] = zlevel
+        map["silent"] = silent
+        map["invisible"] = invisible
+        map["cursor"] = cursor
+        map["draggable"] = draggable
+        map["progressiv"] = progressiv
+        map["shape"] = shape
+        map["style"] = style
     }
 }

e0be56aac94ce330e5c2c0e36d69d90e33e88d01
Longhua Zhang <kuaileainits@163.com>
2017-02-13 09:57:05 +0800

    Add detail comments for `SECGraphic`

--- SwiftyEcharts/Models/Graphic/SECGraphic.swift
+++ SwiftyEcharts/Models/Graphic/SECGraphic.swift
@@ -329,18 +329,83 @@ public enum SECGraphicBounding : String, SECJsonable {
 ///     }
 public protocol SECGraphic : SECZable, SECMappable {
     
+    /// `Graphic` 的类型
     var type: SECGraphicType { get }
+    /// id 用于在更新或删除图形元素时指定更新哪个图形元素，如果不需要用可以忽略。
     var id: String? { get set }
+    /// setOption 时指定本次对该图形元素的操作行为
     var action: SECGraphicAction? { get set }
+    /// 描述怎么根据父元素进行定位。
+    ///
+    ///『父元素』是指：如果是顶层元素，父元素是 echarts 图表容器。如果是 group 的子元素，父元素就是 group 元素。
+    ///
+    /// 值的类型可以是：
+    ///
+    /// - number：表示像素值。
+    /// - 百分比值：如 '33%'，用父元素的高和此百分比计算出最终值。
+    /// - 'center'：表示自动居中。
+    ///
+    /// left 和 right 只有一个可以生效。
+    ///
+    /// 如果指定 left 或 right，则 shape 里的 x、cx 等定位属性不再生效。
     var left: SECPosition? { get set }
+    /// 描述怎么根据父元素进行定位。
+    ///
+    ///『父元素』是指：如果是顶层元素，父元素是 echarts 图表容器。如果是 group 的子元素，父元素就是 group 元素。
+    ///
+    /// 值的类型可以是：
+    ///
+    /// - number：表示像素值。
+    /// - 百分比值：如 '33%'，用父元素的高和此百分比计算出最终值。
+    /// - 'center'：表示自动居中。
+    ///
+    /// left 和 right 只有一个可以生效。
+    ///
+    /// 如果指定 left 或 right，则 shape 里的 x、cx 等定位属性不再生效。
     var right: SECPosition? { get set }
+    /// 描述怎么根据父元素进行定位。
+    ///
+    /// 『父元素』是指：如果是顶层元素，父元素是 echarts 图表容器。如果是 group 的子元素，父元素就是 group 元素。
+    ///
+    /// 值的类型可以是：
+    ///
+    /// - number：表示像素值。
+    /// - 百分比值：如 '33%'，用父元素的宽和此百分比计算出最终值。
+    /// - 'middle'：表示自动居中。
+    ///
+    /// top 和 bottom 只有一个可以生效。
+    ///
+    /// 如果指定 top 或 bottom，则 shape 里的 y、cy 等定位属性不再生效。
     var top: SECPosition? { get set }
+    /// /// 描述怎么根据父元素进行定位。
+    ///
+    /// 『父元素』是指：如果是顶层元素，父元素是 echarts 图表容器。如果是 group 的子元素，父元素就是 group 元素。
+    ///
+    /// 值的类型可以是：
+    ///
+    /// - number：表示像素值。
+    /// - 百分比值：如 '33%'，用父元素的宽和此百分比计算出最终值。
+    /// - 'middle'：表示自动居中。
+    ///
+    /// top 和 bottom 只有一个可以生效。
+    ///
+    /// 如果指定 top 或 bottom，则 shape 里的 y、cy 等定位属性不再生效。
     var bottom: SECPosition? { get set }
+    /// 决定此图形元素在定位时，对自身的包围盒计算方式。
+    /// 
+    /// 参见例子：
+    ///
+    /// http://echarts.baidu.com/gallery/editor.html?c=doc-example/graphic-bounding
     var bounding: SECGraphicBounding? { get set }
+    /// 是否不响应鼠标以及触摸事件。
     var silent: Bool? { get set }
+    /// 鼠标悬浮时鼠标的样式是什么。同 CSS 的 cursor。
     var cursor: String? { get set }
+    /// 节点是否可见。
     var invisible: Bool? { get set }
+    /// 图形元素是否可以被拖拽。
     var draggable: Bool? { get set }
+    /// 是否渐进式渲染。当图形元素过多时才使用。
     var progressiv: Bool? { get set }
     // TODO: 添加14个方法， 暂时还没添加
 }

9a13844beed8381ac6ac057d047575ec03227751
Longhua Zhang <kuaileainits@163.com>
2017-02-14 21:48:54 +0800

    Add new module for `SECCandlesticSerie`

---
+++ SwiftyEcharts/Models/Series/SECCandlestickSerie.swift
@@ -0,0 +1,44 @@
+//
+//  SECCandlestickSerie.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 14/02/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+public struct SECCandlestickSerie : SECSeries {
+    
+    public struct Data {
+        public var name: String?
+        public var value: [SECJsonable]?
+        public var itemStyle: SECItemStyle?
+        
+        public init() {}
+    }
+    
+    public var type: SECSerieType {
+        return .candlestick
+    }
+    
+    public var coordinateSystem: SECCoordinateSystem?
+    public var xAxisIndex: UInt8?
+    public var yAxisIndex: UInt8?
+    public var name: String?
+    public var legendHoverLink: Bool?
+    public var hoverAnimation: Bool?
+    public var layout: SECOrient?
+    public var barWidth: Float?
+    public var barMinWidth: Float?
+    public var barMaxWidth: Float?
+    public var itemStyle: SECItemStyle?
+    public var data: [SECJsonable]?
+    public var markPoint: SECMarkPoint?
+    public var markLine: SECMarkLine?
+    public var markArea: SECMarkArea?
+    public var zlevel: Float?
+    public var z: Float?
+    public var silent: Bool?
+    public var animationDuration: Float?
+    public var animationEasing: SECAnimation?
+    public var animationDelay: Float?
+}

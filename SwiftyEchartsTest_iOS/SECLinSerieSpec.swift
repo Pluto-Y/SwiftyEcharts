26ce86184100dc908b5901b1d196e68905bef871
Longhua Zhang <kuaileainits@163.com>
2017-02-14 18:02:18 +0800

    Add new test case for `SECLineSerieSpec`

---
+++ SwiftyEchartsTest_iOS/SECLinSerieSpec.swift
@@ -0,0 +1,32 @@
+//
+//  SECLinSerieSpec.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto-Y on 14/02/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SwiftyEcharts
+
+class SECLineSerieSpec : QuickSpec {
+    
+    override func spec() {
+        describe("For SECLineSerie.AreaStyle") {
+            
+            it("needs to check the SECJsonable for areaStyle") {
+                let areaStyle = SECLineSerie.AreaStyle(
+                    .normal(SECCommonAreaStyleContent())
+                )
+                
+                expect(areaStyle.jsonString).to(equal("{\n"
+                    + "\"normal\":{\n"
+                    + "\n}"
+                    + "\n}"))
+            }
+            
+        }
+    }
+    
+}

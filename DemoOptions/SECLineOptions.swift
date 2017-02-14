f259ea6e8b299b44b9403ec352d85dea54de2a7e
Longhua Zhang <kuaileainits@163.com>
2017-02-14 18:01:37 +0800

    Add new line demo for `Area Stack`

--- DemoOptions/SECLineOptions.swift
+++ DemoOptions/SECLineOptions.swift
@@ -10,6 +10,92 @@ import SwiftyEcharts
 
 public struct SECLineOptions {
     
+    /// 堆叠区域图
+    /// 地址:http://echarts.baidu.com/demo.html#area-stack
+    static func areaStackOption() -> SECOption {
+        return SECOption(
+            .title(SECTitle(
+                .text("堆叠区域图")
+                )),
+            .tooltip(SECTooltip(
+                .trigger(.axis)
+                )),
+            .toolbox(SECToolbox(
+                .feature(SECToolbox.Feature(
+                    .saveAsImage(SECToolbox.Feature.SaveAsImage(
+                        
+                        ))
+                    ))
+                )),
+            .grid(SECGrid(
+                .left(.percent(3)),
+                .right(.percent(4)),
+                .bottom(.percent(3)),
+                .containLabel(true)
+                )),
+            .xAxis([
+                SECAxis(
+                    .type(.category),
+                    .boundaryGap(.category(false)),
+                    .data(["周一","周二","周三","周四","周五","周六","周日"])
+                )
+                ]),
+            .yAxis([
+                SECAxis(
+                    .type(.value)
+                )
+                ]),
+            .series([
+                SECLineSerie(
+                    .name("邮件营销"),
+                    .stack("总量"),
+                    .areaStyle(SECLineSerie.AreaStyle(
+                        .normal(SECCommonAreaStyleContent())
+                        )),
+                    .data([120, 132, 101, 134, 90, 230, 210])
+                ),
+                SECLineSerie(
+                    .name("联盟广告"),
+                    .stack("总量"),
+                    .areaStyle(SECLineSerie.AreaStyle(
+                        .normal(SECCommonAreaStyleContent())
+                        )),
+                    .data([220, 182, 191, 234, 290, 330, 310])
+                ),
+                SECLineSerie(
+                    .name("视频广告"),
+                    .stack("总量"),
+                    .areaStyle(SECLineSerie.AreaStyle(
+                        .normal(SECCommonAreaStyleContent())
+                        )),
+                    .data([150, 232, 201, 154, 190, 330, 410])
+                ),
+                SECLineSerie(
+                    .name("直接访问"),
+                    .stack("总量"),
+                    .areaStyle(SECLineSerie.AreaStyle(
+                        .normal(SECCommonAreaStyleContent())
+                        )),
+                    .data([320, 332, 301, 334, 390, 330, 320])
+                ),
+                SECLineSerie(
+                    .name("搜索引擎"),
+                    .stack("总量"),
+                    .label(SECLabel(
+                        .normal(SECCommonLabelStyle(
+                            .show(true),
+                            .position(.top)
+                            ))
+                        )),
+                    .areaStyle(SECLineSerie.AreaStyle(
+                        .normal(SECCommonAreaStyleContent())
+                        )),
+                    .data([820, 932, 901, 934, 1290, 1330, 1320])
+                )
+                ])
+        )
+    }
+    
     /// 指数折线图
     /// 地址: http://echarts.baidu.com/gallery/editor.html?c=line-log
     static func logAxisOption() -> SECOption {
@@ -23,7 +109,7 @@ public struct SECLineOptions {
                 .formatter(.string("{a} <br/>{b} : {c}"))
                 )),
             .legend(SECLegend(
-//                .left(.left),
+                .left(.left),
                 .datas(["2的指数", "3的指数"])
                 )),
             .xAxis([

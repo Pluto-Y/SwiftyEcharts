835f6254f8d361fc745ee0d252385a84acbd8563
Longhua Zhang <kuaileainits@163.com>
2017-02-14 21:52:30 +0800

    Re-structure codes

--- SwiftyEcharts/Models/Series/SECBoxplotSerie.swift
+++ SwiftyEcharts/Models/Series/SECBoxplotSerie.swift
@@ -43,109 +43,109 @@ public struct SECBoxplotSerie : SECSeries, SECZable {
     public init() {}
 }
 
-extension SECBoxplotSerie : SECEnumable {
-	public enum Enums {
-		case coordinateSystem(SECCoordinateSystem), xAxisIndex(UInt8), yAxisIndex(UInt8), name(String), legendHoverLink(Bool), hoverAnimation(Bool), layout(SECOrient), boxWidth([Float]), itemStyle(SECItemStyle), data([SECJsonable]), markPoint(SECMarkPoint), markLine(SECMarkLine), markArea(SECMarkArea), zlevel(Float), z(Float), silent(Bool), animationDuration(Float), animationEasing(SECAnimation), animationDelay(Float)
-	}
-
-	public typealias ContentEnum = Enums
-
-	public init(_ elements: Enums...) {
-		for ele in elements {
-			switch ele {
-				case let .coordinateSystem(coordinateSystem):
-					self.coordinateSystem = coordinateSystem
-				case let .xAxisIndex(xAxisIndex):
-					self.xAxisIndex = xAxisIndex
-				case let .yAxisIndex(yAxisIndex):
-					self.yAxisIndex = yAxisIndex
-				case let .name(name):
-					self.name = name
-				case let .legendHoverLink(legendHoverLink):
-					self.legendHoverLink = legendHoverLink
-				case let .hoverAnimation(hoverAnimation):
-					self.hoverAnimation = hoverAnimation
-				case let .layout(layout):
-					self.layout = layout
-				case let .boxWidth(boxWidth):
-					self.boxWidth = boxWidth
-				case let .itemStyle(itemStyle):
-					self.itemStyle = itemStyle
-				case let .data(data):
-					self.data = data
-				case let .markPoint(markPoint):
-					self.markPoint = markPoint
-				case let .markLine(markLine):
-					self.markLine = markLine
-				case let .markArea(markArea):
-					self.markArea = markArea
-				case let .zlevel(zlevel):
-					self.zlevel = zlevel
-				case let .z(z):
-					self.z = z
-				case let .silent(silent):
-					self.silent = silent
-				case let .animationDuration(animationDuration):
-					self.animationDuration = animationDuration
-				case let .animationEasing(animationEasing):
-					self.animationEasing = animationEasing
-				case let .animationDelay(animationDelay):
-					self.animationDelay = animationDelay
-			}
-		}
-	}
-}
-
-extension SECBoxplotSerie : SECMappable {
-	public func mapping(map: SECMap) {
-		map["type"] = type
-		map["coordinateSystem"] = coordinateSystem
-		map["xAxisIndex"] = xAxisIndex
-		map["yAxisIndex"] = yAxisIndex
-		map["name"] = name
-		map["legendHoverLink"] = legendHoverLink
-		map["hoverAnimation"] = hoverAnimation
-		map["layout"] = layout
-		map["boxWidth"] = boxWidth
-		map["itemStyle"] = itemStyle
-		map["data"] = data
-		map["markPoint"] = markPoint
-		map["markLine"] = markLine
-		map["markArea"] = markArea
-		map["zlevel"] = zlevel
-		map["z"] = z
-		map["silent"] = silent
-		map["animationDuration"] = animationDuration
-		map["animationEasing"] = animationEasing
-		map["animationDelay"] = animationDelay
-	}
-}
-
 extension SECBoxplotSerie.Data : SECEnumable {
-	public enum Enums {
-		case name(String), value([SECJsonable]), itemStyle(SECItemStyle)
-	}
-
-	public typealias ContentEnum = Enums
-
-	public init(_ elements: Enums...) {
-		for ele in elements {
-			switch ele {
-				case let .name(name):
-					self.name = name
-				case let .value(value):
-					self.value = value
-				case let .itemStyle(itemStyle):
-					self.itemStyle = itemStyle
-			}
-		}
-	}
+    public enum Enums {
+        case name(String), value([SECJsonable]), itemStyle(SECItemStyle)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .name(name):
+                self.name = name
+            case let .value(value):
+                self.value = value
+            case let .itemStyle(itemStyle):
+                self.itemStyle = itemStyle
+            }
+        }
+    }
 }
 
 extension SECBoxplotSerie.Data : SECMappable {
-	public func mapping(map: SECMap) {
-		map["name"] = name
-		map["value"] = value
-		map["itemStyle"] = itemStyle
-	}
+    public func mapping(map: SECMap) {
+        map["name"] = name
+        map["value"] = value
+        map["itemStyle"] = itemStyle
+    }
+}
+
+extension SECBoxplotSerie : SECEnumable {
+    public enum Enums {
+        case coordinateSystem(SECCoordinateSystem), xAxisIndex(UInt8), yAxisIndex(UInt8), name(String), legendHoverLink(Bool), hoverAnimation(Bool), layout(SECOrient), boxWidth([Float]), itemStyle(SECItemStyle), data([SECJsonable]), markPoint(SECMarkPoint), markLine(SECMarkLine), markArea(SECMarkArea), zlevel(Float), z(Float), silent(Bool), animationDuration(Float), animationEasing(SECAnimation), animationDelay(Float)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .coordinateSystem(coordinateSystem):
+                self.coordinateSystem = coordinateSystem
+            case let .xAxisIndex(xAxisIndex):
+                self.xAxisIndex = xAxisIndex
+            case let .yAxisIndex(yAxisIndex):
+                self.yAxisIndex = yAxisIndex
+            case let .name(name):
+                self.name = name
+            case let .legendHoverLink(legendHoverLink):
+                self.legendHoverLink = legendHoverLink
+            case let .hoverAnimation(hoverAnimation):
+                self.hoverAnimation = hoverAnimation
+            case let .layout(layout):
+                self.layout = layout
+            case let .boxWidth(boxWidth):
+                self.boxWidth = boxWidth
+            case let .itemStyle(itemStyle):
+                self.itemStyle = itemStyle
+            case let .data(data):
+                self.data = data
+            case let .markPoint(markPoint):
+                self.markPoint = markPoint
+            case let .markLine(markLine):
+                self.markLine = markLine
+            case let .markArea(markArea):
+                self.markArea = markArea
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .z(z):
+                self.z = z
+            case let .silent(silent):
+                self.silent = silent
+            case let .animationDuration(animationDuration):
+                self.animationDuration = animationDuration
+            case let .animationEasing(animationEasing):
+                self.animationEasing = animationEasing
+            case let .animationDelay(animationDelay):
+                self.animationDelay = animationDelay
+            }
+        }
+    }
+}
+
+extension SECBoxplotSerie : SECMappable {
+    public func mapping(map: SECMap) {
+        map["type"] = type
+        map["coordinateSystem"] = coordinateSystem
+        map["xAxisIndex"] = xAxisIndex
+        map["yAxisIndex"] = yAxisIndex
+        map["name"] = name
+        map["legendHoverLink"] = legendHoverLink
+        map["hoverAnimation"] = hoverAnimation
+        map["layout"] = layout
+        map["boxWidth"] = boxWidth
+        map["itemStyle"] = itemStyle
+        map["data"] = data
+        map["markPoint"] = markPoint
+        map["markLine"] = markLine
+        map["markArea"] = markArea
+        map["zlevel"] = zlevel
+        map["z"] = z
+        map["silent"] = silent
+        map["animationDuration"] = animationDuration
+        map["animationEasing"] = animationEasing
+        map["animationDelay"] = animationDelay
+    }
 }

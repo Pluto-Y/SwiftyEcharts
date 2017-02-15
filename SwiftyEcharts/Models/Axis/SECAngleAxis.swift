ef1e0a5ff86be7c2732734df3cfcdc50c443ce36
Longhua Zhang <kuaileainits@163.com>
2017-02-15 14:19:00 +0800

    Change type of property named 'boundaryGap' in `SECAngleAxi`

--- SwiftyEcharts/Models/Axis/SECAngleAxis.swift
+++ SwiftyEcharts/Models/Axis/SECAngleAxis.swift
@@ -54,7 +54,7 @@ public struct SECAngleAxis : SECZable {
     /// 非类目轴，包括时间，数值，对数轴，boundaryGap是一个两个值的数组，分别表示数据最小值和最大值的延伸范围，可以直接设置数值或者相对的百分比，在设置 min 和 max 后无效。 示例：
     ///
     /// boundaryGap: ['20%', '20%']
-    public var boundaryGap: [Float]? /// FIXME: 暂不支持百分比
+    public var boundaryGap: SECBoundaryGap?
     /// 坐标轴刻度最大值。
     ///
     /// 可以设置成特殊值 'dataMax'，此时取数据在该轴上的最大值作为最大刻度。
@@ -161,7 +161,7 @@ extension SECAngleAxis.Data : SECMappable {
 
 extension SECAngleAxis : SECEnumable {
     public enum Enums {
-        case polarIndex(UInt8), startAngle(Float), clockwise(Bool), type(SECAxisType), boundaryGap([Float]), min(Float), max(Float), scale(Bool), splitNumber(UInt8), minInterval(UInt8), interval(UInt), logBase(Float), silent(Bool), triggerEvent(Bool), axisLine(SECAxisLine), axisTick(SECAxisTick), axisLabel(SECAxisLabel), splitLine(SECSplitLine), splitArea(SECSplitArea), data([Any]), zlevel(Float), z(Float)
+        case polarIndex(UInt8), startAngle(Float), clockwise(Bool), type(SECAxisType), boundaryGap(SECBoundaryGap), min(Float), max(Float), scale(Bool), splitNumber(UInt8), minInterval(UInt8), interval(UInt), logBase(Float), silent(Bool), triggerEvent(Bool), axisLine(SECAxisLine), axisTick(SECAxisTick), axisLabel(SECAxisLabel), splitLine(SECSplitLine), splitArea(SECSplitArea), data([Any]), zlevel(Float), z(Float)
     }
     
     public typealias ContentEnum = Enums

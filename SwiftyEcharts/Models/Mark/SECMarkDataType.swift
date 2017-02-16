a4d57d38831b4ae5b89bb836ee14e243785ca79d
Longhua Zhang <kuaileainits@163.com>
2017-02-16 23:05:37 +0800

    Add new enum for `SECMarkDataType`

---
+++ SwiftyEcharts/Models/Mark/SECMarkDataType.swift
@@ -0,0 +1,22 @@
+//
+//  SECMarkDataType.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 16/02/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+/// 特殊的标注类型，用于标注最大值最小值等。
+///
+/// - min: 最小值
+/// - max: 最大值
+/// - average: 平均值
+public enum SECMarkDataType : String, SECJsonable {
+    case min = "min"
+    case max = "max"
+    case average = "average"
+    
+    public var jsonString: String {
+        return "\"\(self.rawValue)\""
+    }
+}

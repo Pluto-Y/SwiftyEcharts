f5089be6f3e31e21957809f48d71a0c7eec8a314
Longhua Zhang <kuaileainits@163.com>
2017-02-19 22:00:37 +0800

    Add new demo for `line-sections`

--- DemoOptions/SECLineOptions.swift
+++ DemoOptions/SECLineOptions.swift
@@ -672,6 +672,93 @@ public struct SECLineOptions {
         )
     }
     
+    // MARK: 一天用电量分布
+    /// 地址:http://echarts.baidu.com/demo.html#line-sections
+    static func lineSectionsOption() -> SECOption {
+        return SECOption(
+            .title(SECTitle(
+                .text("一天用电量分布"),
+                .subtext("纯属虚构")
+                )),
+            .tooltip(SECTooltip(
+                .trigger(.axis)
+                )),
+            .toolbox(SECToolbox(
+                .show(true),
+                .feature(SECTFeature(
+                    .saveAsImage(SECTFSaveAsImage())
+                    ))
+                )),
+            .xAxis([
+                SECAxis(
+                    .type(.category),
+                    .boundaryGap(.category(false)),
+                    .data(["00:00", "01:15", "02:30", "03:45", "05:00", "06:15", "07:30", "08:45", "10:00", "11:15", "12:30", "13:45", "15:00", "16:15", "17:30", "18:45", "20:00", "21:15", "22:30", "23:45"])
+                )
+                ]),
+            .yAxis([
+                SECAxis(
+                    .type(.value),
+                    .axisLabel(SECAxisLabel(
+                        .formatter(.string("{value} W"))
+                        ))
+                )
+                ]),
+            .visualMap(SECPiecewiseVisualMap(
+                .show(false),
+                .dimension(0),
+                .pieces([
+                    [
+                        "lte": 6,
+                        "color": "green"
+                    ], [
+                        "gt": 6,
+                        "lte": 8,
+                        "color": "red"
+                    ], [
+                        "gt": 8,
+                        "lte": 14,
+                        "color": "green"
+                    ], [
+                        "gt": 14,
+                        "lte": 17,
+                        "color": "red"
+                    ], [
+                        "gt": 17,
+                        "color": "green"
+                    ]
+                    ])
+                )),
+            .series([
+                SECLineSerie(
+                    .name("用电量"),
+                    .smooth(true),
+                    .data([300, 280, 250, 260, 270, 300, 550, 500, 400, 390, 380, 390, 400, 500, 600, 750, 800, 700, 600, 400]),
+                    .markArea(SECMarkArea(
+                        .data([
+                            [
+                                [
+                                    "name": "早高峰",
+                                    "xAxis": "07:30"
+                                ], [
+                                    "xAxis": "10:00"
+                                ]
+                            ],
+                            [
+                                [
+                                    "name": "晚高峰",
+                                    "xAxis": "17:30"
+                                ], [
+                                    "xAxis": "21:15"
+                                ]
+                            ]
+                            ])
+                        ))
+                )
+                ])
+        )
+    }
+    
     // MARK: 折线图堆叠
     /// 地址:http://echarts.baidu.com/demo.html#line-stack
     static func lineStackOption() -> SECOption {

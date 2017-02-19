e7db0f21859650a6c73d108f2fd80e66a8caaa8e
Longhua Zhang <kuaileainits@163.com>
2017-02-19 23:44:09 +0800

    Add new type for save one property which is one or array

---
+++ SwiftyEcharts/Tools/Type/SECOneOrMore.swift
@@ -0,0 +1,30 @@
+//
+//  SECOneOrMore.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 19/02/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+public struct SECOneOrMore<T> {
+    private var one: T? = nil
+    private var more: [T]? = nil
+    internal init(one: T) {
+        self.one = one
+    }
+    
+    internal init(more: [T]) {
+        self.more = more
+    }
+}
+
+extension SECOneOrMore : SECJsonable {
+    public var jsonString: String {
+        if let datas = self.more {
+            return datas.jsonString
+        } else if let data = self.one {
+            return obtainJsonString(from: data)
+        }
+        return "\"null\""
+    }
+}

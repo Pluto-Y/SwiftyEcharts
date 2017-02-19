a128ff4221306a284201a1934efbebcdaea8ba67
Longhua Zhang <kuaileainits@163.com>
2017-02-19 23:46:01 +0800

    Update SECOption for grid which is changed to `SECOneOrMore`

--- SwiftyEcharts/Models/SECOption.swift
+++ SwiftyEcharts/Models/SECOption.swift
@@ -9,7 +9,7 @@
 public struct SECOption : SECTextful, SECAnimatable {
     public var title: SECTitle?
     public var legend: SECLegend?
-    public var grid: [SECGrid]?
+    public var grid: SECOneOrMore<SECGrid>?
     public var xAxis: [SECAxis]?
     public var yAxis: [SECAxis]?
     public var polar: SECPolar?
@@ -76,63 +76,65 @@ public struct SECOption : SECTextful, SECAnimatable {
 }
 
 extension SECOption : SECEnumable {
-	public enum Enums {
-		case title(SECTitle), legend(SECLegend), grid([SECGrid]), xAxis([SECAxis]), yAxis([SECAxis]), polar(SECPolar), radiusAxis([SECRadiusAxis]), angleAxis([SECAngleAxis]), radar(SECRadar), dataZoom([SECDataZoom]), visualMap(SECVisualMap), tooltip(SECTooltip), toolbox(SECToolbox), brush(SECBrush), geo(SECGeo), parallel(SECParallel), parallelAxis([SECParallelAxis]), singleAxis([SECSingleAxis]), timeline(SECTimeline), graphic([SECGraphic]), series([SECSeries]), color([SECColor]), backgroundColor(SECColor), textStyle(SECTextStyle), animation(Bool), animationThreshold(Float), animationDuration(SECTime), animationEasing(SECAnimation), animationDelay(SECTime), animationDurationUpdate(SECTime), animationEasingUpdate(SECAnimation), animationDelayUpdate(SECTime)
-	}
-
-	public typealias ContentEnum = Enums
-
-	public init(_ elements: Enums...) {
-		for ele in elements {
-			switch ele {
-				case let .title(title):
-					self.title = title
-				case let .legend(legend):
-					self.legend = legend
-				case let .grid(grid):
-					self.grid = grid
-				case let .xAxis(xAxis):
-					self.xAxis = xAxis
-				case let .yAxis(yAxis):
-					self.yAxis = yAxis
-				case let .polar(polar):
-					self.polar = polar
-				case let .radiusAxis(radiusAxis):
-					self.radiusAxis = radiusAxis
-				case let .angleAxis(angleAxis):
-					self.angleAxis = angleAxis
-				case let .radar(radar):
-					self.radar = radar
-				case let .dataZoom(dataZoom):
-					self.dataZoom = dataZoom
-				case let .visualMap(visualMap):
-					self.visualMap = visualMap
-				case let .tooltip(tooltip):
-					self.tooltip = tooltip
-				case let .toolbox(toolbox):
-					self.toolbox = toolbox
-				case let .brush(brush):
-					self.brush = brush
-				case let .geo(geo):
-					self.geo = geo
-				case let .parallel(parallel):
-					self.parallel = parallel
-				case let .parallelAxis(parallelAxis):
-					self.parallelAxis = parallelAxis
-				case let .singleAxis(singleAxis):
-					self.singleAxis = singleAxis
-				case let .timeline(timeline):
-					self.timeline = timeline
-				case let .graphic(graphic):
-					self.graphic = graphic
-				case let .series(series):
-					self.series = series
-				case let .color(color):
-					self.color = color
-				case let .backgroundColor(backgroundColor):
-					self.backgroundColor = backgroundColor
-				case let .textStyle(textStyle):
-					self.textStyle = textStyle
+    public enum Enums {
+        case title(SECTitle), legend(SECLegend), grid(SECGrid), grids([SECGrid]), xAxis([SECAxis]), yAxis([SECAxis]), polar(SECPolar), radiusAxis([SECRadiusAxis]), angleAxis([SECAngleAxis]), radar(SECRadar), dataZoom([SECDataZoom]), visualMap(SECVisualMap), tooltip(SECTooltip), toolbox(SECToolbox), brush(SECBrush), geo(SECGeo), parallel(SECParallel), parallelAxis([SECParallelAxis]), singleAxis([SECSingleAxis]), timeline(SECTimeline), graphic([SECGraphic]), series([SECSeries]), color([SECColor]), backgroundColor(SECColor), textStyle(SECTextStyle), animation(Bool), animationThreshold(Float), animationDuration(SECTime), animationEasing(SECAnimation), animationDelay(SECTime), animationDurationUpdate(SECTime), animationEasingUpdate(SECAnimation), animationDelayUpdate(SECTime)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .title(title):
+                self.title = title
+            case let .legend(legend):
+                self.legend = legend
+            case let .grid(grid):
+                self.grid = SECOneOrMore(one: grid)
+            case let .grids(grids):
+                self.grid = SECOneOrMore(more: grids)
+            case let .xAxis(xAxis):
+                self.xAxis = xAxis
+            case let .yAxis(yAxis):
+                self.yAxis = yAxis
+            case let .polar(polar):
+                self.polar = polar
+            case let .radiusAxis(radiusAxis):
+                self.radiusAxis = radiusAxis
+            case let .angleAxis(angleAxis):
+                self.angleAxis = angleAxis
+            case let .radar(radar):
+                self.radar = radar
+            case let .dataZoom(dataZoom):
+                self.dataZoom = dataZoom
+            case let .visualMap(visualMap):
+                self.visualMap = visualMap
+            case let .tooltip(tooltip):
+                self.tooltip = tooltip
+            case let .toolbox(toolbox):
+                self.toolbox = toolbox
+            case let .brush(brush):
+                self.brush = brush
+            case let .geo(geo):
+                self.geo = geo
+            case let .parallel(parallel):
+                self.parallel = parallel
+            case let .parallelAxis(parallelAxis):
+                self.parallelAxis = parallelAxis
+            case let .singleAxis(singleAxis):
+                self.singleAxis = singleAxis
+            case let .timeline(timeline):
+                self.timeline = timeline
+            case let .graphic(graphic):
+                self.graphic = graphic
+            case let .series(series):
+                self.series = series
+            case let .color(color):
+                self.color = color
+            case let .backgroundColor(backgroundColor):
+                self.backgroundColor = backgroundColor
+            case let .textStyle(textStyle):
+                self.textStyle = textStyle
             case let .animation(animation):
                 self.animation = animation
             case let .animationThreshold(animationThreshold):
@@ -149,37 +151,37 @@ extension SECOption : SECEnumable {
                 self.animationEasingUpdate = animationEasingUpdate
             case let .animationDelayUpdate(animationDelayUpdate):
                 self.animationDelayUpdate = animationDelayUpdate
-			}
-		}
-	}
+            }
+        }
+    }
 }
 
 extension SECOption : SECMappable {
-	public func mapping(map: SECMap) {
-		map["title"] = title
-		map["legend"] = legend
-		map["grid"] = grid
-		map["xAxis"] = xAxis
-		map["yAxis"] = yAxis
-		map["polar"] = polar
-		map["radiusAxis"] = radiusAxis
-		map["angleAxis"] = angleAxis
-		map["radar"] = radar
-		map["dataZoom"] = dataZoom
-		map["visualMap"] = visualMap
-		map["tooltip"] = tooltip
-		map["toolbox"] = toolbox
-		map["brush"] = brush
-		map["geo"] = geo
-		map["parallel"] = parallel
-		map["parallelAxis"] = parallelAxis
-		map["singleAxis"] = singleAxis
-		map["timeline"] = timeline
-		map["graphic"] = graphic
-		map["series"] = series
-		map["color"] = color
-		map["backgroundColor"] = backgroundColor
-		map["textStyle"] = textStyle
+    public func mapping(map: SECMap) {
+        map["title"] = title
+        map["legend"] = legend
+        map["grid"] = grid
+        map["xAxis"] = xAxis
+        map["yAxis"] = yAxis
+        map["polar"] = polar
+        map["radiusAxis"] = radiusAxis
+        map["angleAxis"] = angleAxis
+        map["radar"] = radar
+        map["dataZoom"] = dataZoom
+        map["visualMap"] = visualMap
+        map["tooltip"] = tooltip
+        map["toolbox"] = toolbox
+        map["brush"] = brush
+        map["geo"] = geo
+        map["parallel"] = parallel
+        map["parallelAxis"] = parallelAxis
+        map["singleAxis"] = singleAxis
+        map["timeline"] = timeline
+        map["graphic"] = graphic
+        map["series"] = series
+        map["color"] = color
+        map["backgroundColor"] = backgroundColor
+        map["textStyle"] = textStyle
         map["animation"] = animation
         map["animationThreshold"] = animationThreshold
         map["animationDuration"] = animationDuration
@@ -188,5 +190,5 @@ extension SECOption : SECMappable {
         map["animationDurationUpdate"] = animationDurationUpdate
         map["animationEasingUpdate"] = animationEasingUpdate
         map["animationDelayUpdate"] = animationDelayUpdate
-	}
+    }
 }

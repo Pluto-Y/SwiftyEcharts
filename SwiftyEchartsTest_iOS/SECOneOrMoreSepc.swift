9eb8a3be7ea5768af580e1b25c45de937f9207a9
Longhua Zhang <kuaileainits@163.com>
2017-02-21 00:30:13 +0800

    Add test case for custom operator constructor for `SECOneOrMore`

--- SwiftyEchartsTest_iOS/SECOneOrMoreSepc.swift
+++ SwiftyEchartsTest_iOS/SECOneOrMoreSepc.swift
@@ -13,17 +13,25 @@ import SwiftyEcharts
 class SECOneOrMoreSepc : QuickSpec {
     
     override func spec() {
-        describe("Testing for type named 'SECOneOrMore' ") { 
+        describe("Testing for type named 'SECOneOrMore' ") {
+            let oneValue = "Hello"
+            let moreValue: [Any] = ["World", 3.14, 4, false]
             it(" needs to check the construct and jsonString") {
-                let oneValue = "Hello"
+                // 测试默认构造器以及 jsonString 是否正确
                 let one = SECOneOrMore(one: oneValue)
-                
                 expect(one.jsonString).to(equal(oneValue.jsonString))
                 
-                let moreValue: [Any] = ["World", 3.14, 4, false]
+                // 测试默认构造器以及 jsonString 是否正确
                 let more = SECOneOrMore(more: moreValue)
-                
                 expect(more.jsonString).to(equal(moreValue.jsonString))
+            }
+            
+            it(" needs to check the operator contstruct") {
+                let one = |oneValue
+                expect(one.jsonString).to(equal(SECOneOrMore(one: oneValue).jsonString))
+                
+                let more = ||moreValue
+                expect(more.jsonString).to(equal(SECOneOrMore(more: moreValue).jsonString))
             }
         }
     }

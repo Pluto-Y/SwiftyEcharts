883c475aa485b5391922fbf3c0a798241c0f4b70
Longhua Zhang <kuaileainits@163.com>
2017-02-22 09:54:06 +0800

    Add comments for `SECParallelAxis`

--- SwiftyEcharts/Models/Axis/SECParallelAxis.swift
+++ SwiftyEcharts/Models/Axis/SECParallelAxis.swift
@@ -8,11 +8,17 @@
     
 public struct SECParallelAxis {
 
+    /// 在坐标轴上可以进行框选，这里是一些框选的设置。
     public struct AreaSelectStyle : SECColorful, SECOpacitable {
+        /// 框选范围的宽度。
         public var width: Float?
+        /// 选框的边框宽度。
         public var borderWidth: Float?
+        /// 选框的边框颜色。
         public var borderColor: SECColor?
+        /// 选框的填充色。
         public var color: SECColor?
+        /// 选框的透明度。
         public var opacity: Float?
     }
     
@@ -31,31 +37,156 @@ public struct SECParallelAxis {
         }
     }
     
+    /// 坐标轴的维度序号。
+    ///
+    /// 例如 series-parallel.data 中有如下数据：
+    ///
+    ///     [
+    ///         [1,  55,  9,   56,  0.46,  18,  6,  '良'],
+    ///         [2,  25,  11,  21,  0.65,  34,  9,  '优'],
+    ///         [3,  56,  7,   63,  0.3,   14,  5,  '良'],
+    ///         [4,  33,  7,   29,  0.33,  16,  6,  '优'],
+    ///         { // 数据项也可以是 Object，从而里面能含有对线条的特殊设置。
+    ///             value: [5,  42,  24,  44,  0.76,  40,  16, '优']
+    ///             lineStyle: {...},
+    ///         }
+    ///         ...
+    ///     ]
+    ///
+    /// 数据中，每一行是一个『数据项』，每一列属于一个『维度』。（例如上面数据每一列的含义分别是：『日期』,『AQI指数』, 『PM2.5』, 『PM10』, 『一氧化碳值』, 『二氧化氮值』, 『二氧化硫值』）。
+    ///
+    /// dim 定义了数据的哪个维度（即哪个『列』）会对应到此坐标轴上。
+    ///
+    /// 从 0 开始计数。例如，假设坐标轴的 dim 为 1，则表示数据中的第二列会对应到此坐标轴上。
     public var dim: Float?
+    /// 用于定义『坐标轴』对应到哪个『坐标系』中。
+    ///
+    /// 比如有如下配置：
+    ///     myChart.setOption({
+    ///         parallel: [
+    ///             {...},                      // 第一个平行坐标系
+    ///             {...}                       // 第二个平行坐标系
+    ///         ],
+    ///         parallelAxis: [
+    ///             {parallelIndex: 1, ...},    // 第一个坐标轴，对应到第二个平行坐标系
+    ///             {parallelIndex: 0, ...},    // 第二个坐标轴，对应到第一个平行坐标系
+    ///             {parallelIndex: 1, ...},    // 第三个坐标轴，对应到第二个平行坐标系
+    ///             {parallelIndex: 0, ...}     // 第四个坐标轴，对应到第一个平行坐标系
+    ///         ],
+    ///         ...
+    ///     });
+    ///
+    /// 只有一个平行坐标系时可不用设置，自动取默认值 0。
     public var parallelIndex: UInt8?
+    /// 是否坐标轴刷选的时候，实时刷新视图。如果设为 false，则在刷选动作结束时候，才刷新视图。
+    ///
+    /// 大数据量时，建议设置成 false，从而避免卡顿。
     public var realtiem: Bool?
+    /// 在坐标轴上可以进行框选，这里是一些框选的设置。
     public var areaSelectedStyle: AreaSelectStyle?
+    /// 坐标轴类型。
     public var type: SECAxisType?
+    /// 坐标轴名称。
     public var name: String?
+    /// 坐标轴名称的文字样式。
     public var nameTextStyle: SECTextStyle?
+    /// 坐标轴名称显示位置
     public var nameLocation: NameLocation?
+    /// 坐标轴名称与轴线之间的距离。
     public var nameGap: Float?
+    /// 坐标轴名字旋转，角度值。
     public var nameRotate: Float?
+    /// 是否是反向坐标轴。ECharts 3 中新加。
     public var inverse: Bool?
+    /// 坐标轴两边留白策略，类目轴和非类目轴的设置和表现不一样。
+    ///
+    /// 类目轴中 boundaryGap 可以配置为 true 和 false。默认为 true，这时候刻度只是作为分隔线，标签和数据点都会在两个刻度之间的带(band)中间。
+    ///
+    /// 非类目轴，包括时间，数值，对数轴，boundaryGap是一个两个值的数组，分别表示数据最小值和最大值的延伸范围，可以直接设置数值或者相对的百分比，在设置 min 和 max 后无效。 示例：
+    ///
+    /// boundaryGap: ['20%', '20%']
     public var boudaryGap: SECBoundaryGap?
+    /// 坐标轴刻度最小值。
+    ///
+    /// 可以设置成特殊值 'dataMin'，此时取数据在该轴上的最小值作为最小刻度。
+    ///
+    /// 不设置时会自动计算最小值保证坐标轴刻度的均匀分布。
+    ///
+    /// 在类目轴中，也可以设置为类目的序数（如类目轴 data: ['类A', '类B', '类C'] 中，序数 2 表示 '类C'。也可以设置为负数，如 -3）。
     public var min: Float?
+    /// 坐标轴刻度最大值。
+    ///
+    /// 可以设置成特殊值 'dataMax'，此时取数据在该轴上的最大值作为最大刻度。
+    ///
+    /// 不设置时会自动计算最大值保证坐标轴刻度的均匀分布。
+    ///
+    /// 在类目轴中，也可以设置为类目的序数（如类目轴 data: ['类A', '类B', '类C'] 中，序数 2 表示 '类C'。也可以设置为负数，如 -3）。
     public var max: Float?
+    /// 只在数值轴中（type: 'value'）有效。
+    ///
+    /// 是否是脱离 0 值比例。设置成 true 后坐标刻度不会强制包含零刻度。在双数值轴的散点图中比较有用。
+    ///
+    /// 在设置 min 和 max 之后该配置项无效。
     public var scale: Bool?
+    /// 坐标轴的分割段数，需要注意的是这个分割段数只是个预估值，最后实际显示的段数会在这个基础上根据分割后坐标轴刻度显示的易读程度作调整。
+    ///
+    /// 在类目轴中无效。
     public var splitName: UInt8?
+    /// 自动计算的坐标轴最小间隔大小。
+    ///
+    /// 例如可以设置成1保证坐标轴分割刻度显示成整数。
+    /// 
+    ///     {
+    ///         minInterval: 1
+    ///     }
+    ///
+    /// 只在数值轴中（type: 'value'）有效。
     public var minInterval: UInt8?
+    /// 强制设置坐标轴分割间隔。
+    ///
+    /// 因为 splitNumber 是预估的值，实际根据策略计算出来的刻度可能无法达到想要的效果，这时候可以使用 interval 配合 min、max 强制设定刻度划分，一般不建议使用。
+    ///
+    /// 无法在类目轴中使用。在时间轴（type: 'time'）中需要传时间戳，在对数轴（type: 'log'）中需要传指数值。
     public var interval: Int?
+    /// 对数轴的底数，只在对数轴中（type: 'log'）有效。
     public var logBase: Float?
+    /// 坐标轴是否是静态无法交互。
     public var silent: Bool?
+    /// 坐标轴的标签是否响应和触发鼠标事件，默认不响应。
+    ///
+    /// 事件参数如下：
+    ///
+    ///     {
+    ///         // 组件类型，xAxis, yAxis, radiusAxis, angleAxis
+    ///         // 对应组件类型都会有一个属性表示组件的 index，例如 xAxis 就是 xAxisIndex
+    ///         componentType: string,
+    ///         // 未格式化过的刻度值, 点击刻度标签有效
+    ///         value: '',
+    ///         // 坐标轴名称, 点击坐标轴名称有效
+    ///         name: ''
+    ///     }
     public var triggerEvent: Bool?
+    /// 坐标轴轴线相关设置。
     public var axisLine: SECAxisLine?
+    /// 坐标轴刻度相关设置。
     public var axisTick: SECAxisTick?
+    /// 坐标轴刻度标签的相关设置。
     public var axisLabel: SECAxisLabel?
-    public var data: [Any]?
+    /// 类目数据，在类目轴（type: 'category'）中有效。
+    ///
+    /// 示例：
+    ///     // 所有类目名称列表
+    ///     data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    ///     // 每一项也可以是具体的配置项，此时取配置项中的 `value` 为类目名
+    ///     data: [{
+    ///         value: '周一',
+    ///         // 突出周一
+    ///         textStyle: {
+    ///             fontSize: 20,
+    ///             color: 'red'
+    ///         }
+    ///     }, '周二', '周三', '周四', '周五', '周六', '周日']
+    public var data: [SECJsonable]?
 }
 
 extension SECParallelAxis.AreaSelectStyle : SECEnumable {
@@ -95,7 +226,7 @@ extension SECParallelAxis.AreaSelectStyle : SECMappable {
 
 extension SECParallelAxis : SECEnumable {
 	public enum Enums {
-		case dim(Float), parallelIndex(UInt8), realtiem(Bool), areaSelectedStyle(AreaSelectStyle), type(SECAxisType), name(String), nameTextStyle(SECTextStyle), nameLocation(NameLocation), nameGap(Float), nameRotate(Float), inverse(Bool), boudaryGap(SECBoundaryGap), min(Float), max(Float), scale(Bool), splitName(UInt8), minInterval(UInt8), interval(Int), logBase(Float), silent(Bool), triggerEvent(Bool), axisLine(SECAxisLine), axisTick(SECAxisTick), axisLabel(SECAxisLabel), data([Any])
+		case dim(Float), parallelIndex(UInt8), realtiem(Bool), areaSelectedStyle(AreaSelectStyle), type(SECAxisType), name(String), nameTextStyle(SECTextStyle), nameLocation(NameLocation), nameGap(Float), nameRotate(Float), inverse(Bool), boudaryGap(SECBoundaryGap), min(Float), max(Float), scale(Bool), splitName(UInt8), minInterval(UInt8), interval(Int), logBase(Float), silent(Bool), triggerEvent(Bool), axisLine(SECAxisLine), axisTick(SECAxisTick), axisLabel(SECAxisLabel), data([SECJsonable])
 	}
 
 	public typealias ContentEnum = Enums

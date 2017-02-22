65bca863dabd94a4439d233da3a543a50194ac16
Longhua Zhang <kuaileainits@163.com>
2017-02-23 01:55:14 +0800

    Add new demo for `multiple-y-axis`

--- DemoOptions/SECBarOptions.swift
+++ DemoOptions/SECBarOptions.swift
@@ -802,4 +802,107 @@ public struct SECBarOptions {
                 ])
         )
     }
+    
+    // MARK: 多 Y 轴示例
+    /// 地址: http://echarts.baidu.com/demo.html#multiple-y-axis
+    static func multipleYAxisOption() -> SECOption {
+        let colors: [SECColor] = [.hexColor("#5793f3"), .hexColor("#d14a61"), .hexColor("#675bba")]
+        return SECOption(
+            .color(colors),
+            .tooltip(SECTooltip(
+                .trigger(.axis)
+                )),
+            .grid(SECGrid(
+                .right(.value(20%))
+                )),
+            .toolbox(SECToolbox(
+                .feature(SECTFeature(
+                    .dataView(SECTFDataView(
+                        .show(true),
+                        .readOnly(false)
+                        )),
+                    .restore(SECTFRestore(
+                        .show(true)
+                        )),
+                    .saveAsImage(SECTFSaveAsImage(
+                        .show(true)
+                        ))
+                    ))
+                )),
+            .legend(SECLegend(
+                .data(["蒸发量","降水量","平均温度"])
+                )),
+            .xAxis(SECAxis(
+                .type(.category),
+                .axisTick(SECAxisTick(
+                    .alignWithLabel(true)
+                    )),
+                .data(["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"])
+                )),
+            .yAxises([
+                SECAxis(
+                    .type(.value),
+                    .name("蒸发量"),
+                    .min(0),
+                    .max(250),
+                    .position(.right),
+                    .axisLine(SECAxisLine(
+                        .lineStyle(SECLineStyle(
+                            .color(colors[0])
+                            ))
+                        )),
+                    .axisLabel(SECAxisLabel(
+                        .formatter(.string("{value} ml"))
+                        ))
+                ),
+                SECAxis(
+                    .type(.value),
+                    .name("降水量"),
+                    .min(0),
+                    .max(250),
+                    .position(.right),
+                    .offset(40), // 针对屏幕小进行调整
+                    .axisLine(SECAxisLine(
+                        .lineStyle(SECLineStyle(
+                            .color(colors[1])
+                            ))
+                        )),
+                    .axisLabel(SECAxisLabel(
+                        .formatter(.string("{value} ml"))
+                        ))
+                ),
+                SECAxis(
+                    .type(.value),
+                    .name("平均温度"),
+                    .min(0),
+                    .max(25),
+                    .position(.left),
+                    .axisLine(SECAxisLine(
+                        .lineStyle(SECLineStyle(
+                            .color(colors[2])
+                            ))
+                        )),
+                    .axisLabel(SECAxisLabel(
+                        .formatter(.string("{value} °C"))
+                        ))
+                )
+                ]),
+            .series([
+                SECBarSerie(
+                    .name("蒸发量"),
+                    .data([2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3])
+                ),
+                SECBarSerie(
+                    .name("降水量"),
+                    .yAxisIndex(1),
+                    .data([2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3])
+                ),
+                SECLineSerie(
+                    .name("平均温度"),
+                    .yAxisIndex(2),
+                    .data([2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2])
+                )
+                ])
+        )
+    }
 }

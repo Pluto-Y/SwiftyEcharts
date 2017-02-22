086b8de63fab1e72b5ce5fe63563c81f4614b885
Longhua Zhang <kuaileainits@163.com>
2017-02-22 20:09:02 +0800

    Add new demo for `mix-line-bar`

--- DemoOptions/SECBarOptions.swift
+++ DemoOptions/SECBarOptions.swift
@@ -722,4 +722,84 @@ public struct SECBarOptions {
                 ])
         )
     }
+    
+    // MARK: 动态数据
+    /// 地址: http://echarts.baidu.com/demo.html#dynamic-data
+    static func dynamicDataOption() -> SECOption {
+        return SECOption(
+            
+        )
+    }
+    
+    // MARK: 折柱混合
+    /// 地址: http://echarts.baidu.com/demo.html#mix-line-bar
+    static func mixLineBarOption() -> SECOption {
+        return SECOption(
+            .tooltip(SECTooltip(
+                .trigger(.axis)
+                )),
+            .toolbox(SECToolbox(
+                .feature(SECTFeature(
+                    .dataView(SECTFDataView(
+                        .show(true),
+                        .readOnly(false)
+                        )),
+                    .magicType(SECTFMagicType(
+                        .show(true),
+                        .type([.line, .bar])
+                        )),
+                    .restore(SECTFRestore(
+                        .show(true)
+                        )),
+                    .saveAsImage(SECTFSaveAsImage(
+                        .show(true)
+                        ))
+                    ))
+                )),
+            .legend(SECLegend(
+                .data(["蒸发量","降水量","平均温度"])
+                )),
+            .xAxis(SECAxis(
+                .type(.category),
+                .data(["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"])
+                )),
+            .yAxises([
+                SECAxis(
+                    .type(.value),
+                    .name("水量"),
+                    .min(0),
+                    .max(250),
+                    .interval(50),
+                    .axisLabel(SECAxisLabel(
+                        .formatter(.string("{value} ml"))
+                        ))
+                ),
+                SECAxis(
+                    .type(.value),
+                    .name("温度"),
+                    .min(0),
+                    .max(25),
+                    .interval(5),
+                    .axisLabel(SECAxisLabel(
+                        .formatter(.string("{value} °C"))
+                        ))
+                )
+                ]),
+            .series([
+                SECBarSerie(
+                    .name("蒸发量"),
+                    .data([2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3])
+                ),
+                SECBarSerie(
+                    .name("降水量"),
+                    .data([2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3])
+                ),
+                SECLineSerie(
+                    .name("平均温度"),
+                    .yAxisIndex(1),
+                    .data([2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2])
+                )
+                ])
+        )
+    }
 }

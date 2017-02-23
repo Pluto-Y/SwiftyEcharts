1d3699a37a8dace3c055f98dad884881601eca26
Longhua Zhang <kuaileainits@163.com>
2017-02-23 21:09:09 +0800

    Add new demo for `pie-simple`

--- DemoOptions/SECPieOptions.swift
+++ DemoOptions/SECPieOptions.swift
@@ -178,4 +178,46 @@ public struct SECPieOptions {
                 ])
         )
     }
+    
+    // MARK: 某站点用户访问来源
+    /// 地址: http://echarts.baidu.com/demo.html#pie-simple
+    static func pieSimpleOption() -> SECOption {
+        return SECOption(
+            .title(SECTitle(
+                .text("某站点用户访问来源"),
+                .sublink("纯属虚构"),
+                .left(.center)
+                )),
+            .tooltip(SECTooltip(
+                .trigger(.item),
+                .formatter(.string("{a} <br/>{b}: {c} ({d}%)"))
+                )),
+            .legend(SECLegend(
+                .orient(.vertical),
+                .left(.auto),
+                .data(["直接访问","邮件营销","联盟广告","视频广告","搜索引擎"])
+                )),
+            .series([
+                SECPieSerie(
+                    .name("访问来源"),
+                    .radius(55%),
+                    .center([50%, 60%]),
+                    .data([
+                        ["value":335,"name":"直接访问"],
+                        ["value":310,"name":"邮件营销"],
+                        ["value":234,"name":"联盟广告"],
+                        ["value":135,"name":"视频广告"],
+                        ["value":1548,"name":"搜索引擎"]
+                        ]),
+                    .itemStyle(SECItemStyle(
+                        .emphasis(SECCommonItemStyleContent(
+                            .shadowBlur(10),
+                            .shadowOffsetX(0),
+                            .shadowColor(.rgba(0, 0, 0, 0.5))
+                            ))
+                        ))
+                )
+                ])
+        )
+    }
 }

57a9a79c3024d88e94dffad5a9ccb365b15a4c35
Longhua Zhang <kuaileainits@163.com>
2017-02-25 23:44:45 +0800

    Add new demo controller for `Pie`

---
+++ iOS_Example/DemoControllers/PiesController.swift
@@ -0,0 +1,23 @@
+//
+//  PiesControllerViewController.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 25/02/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import UIKit
+
+class PiesController: BaseDemoController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menus = ["Customized Pie", "环形图", "嵌套环形图", "饼图纹理(暂缺)", "某站点用户访问来源", "南丁格尔玫瑰图"]
+        
+        optionClosures = [SECPieOptions.pieCustomOption, SECPieOptions.pieDoughnutOption, SECPieOptions.pieNestOption, SECPieOptions.piePatternOption, SECPieOptions.pieSimpleOption, SECPieOptions.pieRoseTypeOption]
+
+        self.title = "饼图"
+    }
+    
+}
--- iOS_Example/MenuController.swift
+++ iOS_Example/MenuController.swift
@@ -22,12 +22,13 @@ class MenuController : UITableViewController {
     }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        print("选择了\(indexPath.row + 1)行")
         switch indexPath.row {
         case 0: // 折线图的Demo
             self.navigationController?.pushViewController(LinesController(), animated: true)
         case 1: // 柱状图的Demo
             self.navigationController?.pushViewController(BarsController(), animated: true)
+        case 2: // 饼图的Demo
+            self.navigationController?.pushViewController(LinesController(), animated: true)
         default:
             print("敬请期待...")
         }

bb5a1a2591def4fcdb7ab79d38d60f9e345f7735
Longhua Zhang <kuaileainits@163.com>
2017-02-25 19:17:14 +0800

    Delete the default values for `SECAreaStyle`

--- SwiftyEcharts/Models/Type/SECAreaStyle.swift
+++ SwiftyEcharts/Models/Type/SECAreaStyle.swift
@@ -13,12 +13,12 @@ public protocol SECAreaStyleContent : SECColorful, SECShadowable, SECOpacitable 
 /// 分隔区域的样式设置。
 public struct SECAreaStyle : SECAreaStyleContent {
     /// 分隔区域颜色。分隔区域会按数组中颜色的顺序依次循环设置颜色。默认是一个深浅的间隔色。
-    public var color: SECColor? = SECColor.array([SECColor.rgba(250, 250, 250, 0.3), SECColor.rgba(200, 200, 200, 0.3)])
+    public var color: SECColor?
     /// 阴影设置
     public var shadowBlur: Float?
     public var shadowColor: SECColor?
-    public var shadowOffsetX: Float? = 0
-    public var shadowOffsetY: Float? = 0
+    public var shadowOffsetX: Float?
+    public var shadowOffsetY: Float?
     /// 图形透明度。支持从 0 到 1 的数字，为 0 时不绘制该图形。
     public var opacity: Float? {
         didSet {

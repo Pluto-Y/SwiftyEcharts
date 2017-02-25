648c7b1ce8fa7bde59145efc8b6606549170cf7d
Longhua Zhang <kuaileainits@163.com>
2017-02-25 21:01:42 +0800

    Abstruct for the demo controller for the future all the demos will inherit it

---
+++ iOS_Example/DemoControllers/BaseDemoController.swift
@@ -0,0 +1,61 @@
+//
+//  BaseDemoController.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 25/02/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import UIKit
+import SwiftyEcharts
+
+class BaseDemoController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var menuTableView: UITableView!
+    var echartsView: SECEchartsView!
+    var option: SECOption! {
+        didSet {
+            echartsView.option = option
+            echartsView.loadEcharts()
+        }
+    }
+    var menus: [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+        
+        menuTableView = UITableView(frame: CGRect(x: 0, y: 0, width: width, height: height - 300))
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        menuTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "DemoCell")
+        self.view.addSubview(menuTableView)
+        
+        let line = UIView(frame: CGRect(x: 0, y: height - 300, width: width, height: 1))
+        line.backgroundColor = UIColor(red: 239/255.0, green: 239/255.0, blue: 245/255.0, alpha: 1.0)
+        self.view.addSubview(line)
+        
+        echartsView = SECEchartsView(frame: CGRect(x: 0, y: height - 299, width: width, height: 300))
+        self.view.addSubview(echartsView!)
+        
+        SECMap.ignoreNil = true
+
+    }
+
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 18
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("DemoCell", forIndexPath: indexPath)
+        cell.textLabel?.text = menus[indexPath.row]
+        cell.selectionStyle = .None
+        return cell
+    }
+    
+}
--- iOS_Example/DemoControllers/LinesController.swift
+++ iOS_Example/DemoControllers/LinesController.swift
@@ -9,53 +9,18 @@
 import UIKit
 import SwiftyEcharts
 
-class LinesController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class LinesController: BaseDemoController {
     
-    var menuTableView: UITableView!
-    var echartsView: SECEchartsView!
-    var option: SECOption?
-    let menus = ["雨量流量关系图", "大数据量面积图(暂缺)", "堆叠区域图", "Confidence Band(暂缺)", "动态数据 + 时间坐标轴(暂缺)", "雨量流量关系图", "Beijing AQI(暂缺)", "Try Dragging these Points(暂缺)", "Different Easing Functions(暂缺)", "对数轴示例", "未来一周气温变化", "Click to Add Points(暂缺)", "极坐标双数值轴", "极坐标双数值轴", "一天用电量分布", "折线图堆叠", "Step Line", "高度(km)与气温(°C)变化关系"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.whiteColor()
         
-        let width = self.view.frame.width
-        let height = self.view.frame.height
+        option = SECLineOptions.lineLogOption()
         
-        menuTableView = UITableView(frame: CGRect(x: 0, y: 0, width: width, height: height - 300))
-        menuTableView.delegate = self
-        menuTableView.dataSource = self
-        menuTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "LineCell")
-        self.view.addSubview(menuTableView)
+        menus = ["雨量流量关系图", "大数据量面积图(暂缺)", "堆叠区域图", "Confidence Band(暂缺)", "动态数据 + 时间坐标轴(暂缺)", "雨量流量关系图", "Beijing AQI(暂缺)", "Try Dragging these Points(暂缺)", "Different Easing Functions(暂缺)", "对数轴示例", "未来一周气温变化", "Click to Add Points(暂缺)", "极坐标双数值轴", "极坐标双数值轴", "一天用电量分布", "折线图堆叠", "Step Line", "高度(km)与气温(°C)变化关系"]
         
-        let line = UIView(frame: CGRect(x: 0, y: height - 300, width: width, height: 1))
-        line.backgroundColor = UIColor(red: 239/255.0, green: 239/255.0, blue: 245/255.0, alpha: 1.0)
-        self.view.addSubview(line)
-        
-        echartsView = SECEchartsView(frame: CGRect(x: 0, y: height - 299, width: width, height: 300))
-        self.view.addSubview(echartsView!)
-        
-        SECMap.ignoreNil = true
-        
-        echartsView?.option = SECLineOptions.lineLogOption()
-        echartsView?.loadEcharts()
-
         self.title = "折线图"
         
-    }
-    
-    // MARK: - UITableViewDataSource
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 18
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("LineCell", forIndexPath: indexPath)
-        let title = menus[indexPath.row]
-        cell.textLabel?.text = title
-        cell.selectionStyle = .None
-        return cell
     }
     
     // MARK: - UITableViewDelegate
@@ -100,7 +65,5 @@ class LinesController: UIViewController, UITableViewDelegate, UITableViewDataSou
         default:
             option = SECOption()
         }
-        echartsView?.option = option
-        echartsView.loadEcharts()
     }
 }

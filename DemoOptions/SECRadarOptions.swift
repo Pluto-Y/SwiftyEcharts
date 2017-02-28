935e5dfa27c7eaeaaee559ef233c9b157c926c51
Longhua Zhang <kuaileainits@163.com>
2017-02-28 23:00:13 +0800

    Add new demo for `radar`

--- DemoOptions/SECRadarOptions.swift
+++ DemoOptions/SECRadarOptions.swift
@@ -260,8 +260,39 @@ public struct SECRadarOptions {
     // MARK: 基础雷达图
     /// 地址: http://echarts.baidu.com/demo.html#radar
     static func radarOption() -> SECOption {
-        // TODO: 添加实现
         return SECOption(
+            .title(SECTitle(
+                .text("基础雷达图")
+                )),
+            .tooltip(SECTooltip()),
+            .legend(SECLegend(
+                .data(["预算分配（Allocated Budget）", "实际开销（Actual Spending）"])
+                )),
+            .radar(SECRadar(
+                .indicator([
+                    SECIndicator(.name("销售（sales）"), .max(6500)),
+                    SECIndicator(.name("管理（Administration）"), .max(16000)),
+                    SECIndicator(.name("信息技术（Information Techology）"), .max(30000)),
+                    SECIndicator(.name("客服（Customer Support）"), .max(38000)),
+                    SECIndicator(.name("研发（Development）"), .max(52000)),
+                    SECIndicator(.name("市场（Marketing）"), .max(25000))
+                    ])
+                )),
+            .series([
+                SECRadarSerie(
+                    .name("预算 vs 开销（Budget vs spending）"),
+                    .data([
+                        SECRadarSerie.Data(
+                            .value([4300, 10000, 28000, 35000, 50000, 19000]),
+                            .name("预算分配（Allocated Budget）")
+                        ),
+                        SECRadarSerie.Data(
+                            .value([5000, 14000, 28000, 31000, 42000, 21000]),
+                            .name("实际开销（Actual Spending）")
+                        )
+                        ])
+                )
+                ])
         )
     }
 

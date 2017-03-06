2d9c2a7c0b8191cdafcf934f2dba7882e2f00de2
Longhua Zhang <kuaileainits@163.com>
2017-03-06 23:33:07 +0800

    Make `SECLinesSerie` and sub-module conform to `SECEnumable` and `SECMappable`

--- SwiftyEcharts/Models/Series/SECLinesSerie.swift
+++ SwiftyEcharts/Models/Series/SECLinesSerie.swift
@@ -56,3 +56,153 @@ public struct SECLinesSerie : SECSeries, SECZable, SECAnimatable {
     
     public init() { }
 }
+
+extension SECLinesSerie.Effect : SECEnumable {
+    public enum Enums {
+        case show(Bool), period(Float), constantSpeed(Float), symbol(SECSymbol), symbolSize(Float), color(SECColor), trailLength(Float), loop(Bool)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .show(show):
+                self.show = show
+            case let .period(period):
+                self.period = period
+            case let .constantSpeed(constantSpeed):
+                self.constantSpeed = constantSpeed
+            case let .symbol(symbol):
+                self.symbol = symbol
+            case let .symbolSize(symbolSize):
+                self.symbolSize = symbolSize
+            case let .color(color):
+                self.color = color
+            case let .trailLength(trailLength):
+                self.trailLength = trailLength
+            case let .loop(loop):
+                self.loop = loop
+            }
+        }
+    }
+}
+
+extension SECLinesSerie.Effect : SECMappable {
+    public func mapping(map: SECMap) {
+        map["show"] = show
+        map["period"] = period
+        map["constantSpeed"] = constantSpeed
+        map["symbol"] = symbol
+        map["symbolSize"] = symbolSize
+        map["color"] = color
+        map["trailLength"] = trailLength
+        map["loop"] = loop
+    }
+}
+
+extension SECLinesSerie : SECEnumable {
+    public enum Enums {
+        case name(String), coordinateSystem(SECCoordinateSystem), xAxisIndex(UInt8), yAxisIndex(UInt8), geoIndex(UInt8), polyline(Bool), effect(Effect), large(Bool), largeThreshold(Float), symbol(SECSymbol), symbolSize(Float), lineStyle(SECLineStyle), label(SECFormattedLabel), data([SECJsonable]), markPoint(SECMarkPoint), markLine(SECMarkLine), markArea(SECMarkArea), zlevel(Float), z(Float), silent(Bool), animation(Bool), animationThreshold(Float), animationDuration(SECTime), animationEasing(SECAnimation), animationDelay(SECTime), animationDurationUpdate(SECTime), animationEasingUpdate(SECAnimation), animationDelayUpdate(SECTime)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .name(name):
+                self.name = name
+            case let .coordinateSystem(coordinateSystem):
+                self.coordinateSystem = coordinateSystem
+            case let .xAxisIndex(xAxisIndex):
+                self.xAxisIndex = xAxisIndex
+            case let .yAxisIndex(yAxisIndex):
+                self.yAxisIndex = yAxisIndex
+            case let .geoIndex(geoIndex):
+                self.geoIndex = geoIndex
+            case let .polyline(polyline):
+                self.polyline = polyline
+            case let .effect(effect):
+                self.effect = effect
+            case let .large(large):
+                self.large = large
+            case let .largeThreshold(largeThreshold):
+                self.largeThreshold = largeThreshold
+            case let .symbol(symbol):
+                self.symbol = symbol
+            case let .symbolSize(symbolSize):
+                self.symbolSize = symbolSize
+            case let .lineStyle(lineStyle):
+                self.lineStyle = lineStyle
+            case let .label(label):
+                self.label = label
+            case let .data(data):
+                self.data = data
+            case let .markPoint(markPoint):
+                self.markPoint = markPoint
+            case let .markLine(markLine):
+                self.markLine = markLine
+            case let .markArea(markArea):
+                self.markArea = markArea
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .z(z):
+                self.z = z
+            case let .silent(silent):
+                self.silent = silent
+            case let .animation(animation):
+                self.animation = animation
+            case let .animationThreshold(animationThreshold):
+                self.animationThreshold = animationThreshold
+            case let .animationDuration(animationDuration):
+                self.animationDuration = animationDuration
+            case let .animationEasing(animationEasing):
+                self.animationEasing = animationEasing
+            case let .animationDelay(animationDelay):
+                self.animationDelay = animationDelay
+            case let .animationDurationUpdate(animationDurationUpdate):
+                self.animationDurationUpdate = animationDurationUpdate
+            case let .animationEasingUpdate(animationEasingUpdate):
+                self.animationEasingUpdate = animationEasingUpdate
+            case let .animationDelayUpdate(animationDelayUpdate):
+                self.animationDelayUpdate = animationDelayUpdate
+            }
+        }
+    }
+}
+
+extension SECLinesSerie : SECMappable {
+    public func mapping(map: SECMap) {
+        map["type"] = type
+        map["name"] = name
+        map["coordinateSystem"] = coordinateSystem
+        map["xAxisIndex"] = xAxisIndex
+        map["yAxisIndex"] = yAxisIndex
+        map["geoIndex"] = geoIndex
+        map["polyline"] = polyline
+        map["effect"] = effect
+        map["large"] = large
+        map["largeThreshold"] = largeThreshold
+        map["symbol"] = symbol
+        map["symbolSize"] = symbolSize
+        map["lineStyle"] = lineStyle
+        map["label"] = label
+        map["data"] = data
+        map["markPoint"] = markPoint
+        map["markLine"] = markLine
+        map["markArea"] = markArea
+        map["zlevel"] = zlevel
+        map["z"] = z
+        map["silent"] = silent
+        map["animation"] = animation
+        map["animationThreshold"] = animationThreshold
+        map["animationDuration"] = animationDuration
+        map["animationEasing"] = animationEasing
+        map["animationDelay"] = animationDelay
+        map["animationDurationUpdate"] = animationDurationUpdate
+        map["animationEasingUpdate"] = animationEasingUpdate
+        map["animationDelayUpdate"] = animationDelayUpdate
+    }
+}
+

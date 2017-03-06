9633c29473f2392541052368d27f0bdaa7a94df1
Longhua Zhang <kuaileainits@163.com>
2017-03-06 23:34:33 +0800

    Re-structure codes and add 'type' in `mapping:` function

--- SwiftyEcharts/Models/Series/SECMapSerie.swift
+++ SwiftyEcharts/Models/Series/SECMapSerie.swift
@@ -116,102 +116,103 @@ extension SECMapSerie.Data : SECMappable {
 }
 
 extension SECMapSerie : SECEnumable {
-	public enum Enums {
-		case name(String), map(String), roam(SECRoam), center(SECPosition), aspectScale(Float), zoom(Float), scaleLimit(ScaleLimit), nameMap([String: SECJsonable]), selectedMode(SECSelectedMode), label(SECLabel), itemStyle(SECItemStyle), zlevel(Float), z(Float), left(SECPosition), top(SECPosition), right(SECPosition), bottom(SECPosition), layoutCenter(SECPosition), layoutSize(SECLength), geoIndex(UInt8), mapValueCalculation(String), showLegendSymbol(Bool), data([SECJsonable]), markPoint(SECMarkPoint), markLine(SECMarkLine), markArea(SECMarkArea), silent(Bool)
-	}
-
-	public typealias ContentEnum = Enums
-
-	public init(_ elements: Enums...) {
-		for ele in elements {
-			switch ele {
-				case let .name(name):
-					self.name = name
-				case let .map(map):
-					self.map = map
-				case let .roam(roam):
-					self.roam = roam
-				case let .center(center):
-					self.center = center
-				case let .aspectScale(aspectScale):
-					self.aspectScale = aspectScale
-				case let .zoom(zoom):
-					self.zoom = zoom
-				case let .scaleLimit(scaleLimit):
-					self.scaleLimit = scaleLimit
-				case let .nameMap(nameMap):
-					self.nameMap = nameMap
-				case let .selectedMode(selectedMode):
-					self.selectedMode = selectedMode
-				case let .label(label):
-					self.label = label
-				case let .itemStyle(itemStyle):
-					self.itemStyle = itemStyle
-				case let .zlevel(zlevel):
-					self.zlevel = zlevel
-				case let .z(z):
-					self.z = z
-				case let .left(left):
-					self.left = left
-				case let .top(top):
-					self.top = top
-				case let .right(right):
-					self.right = right
-				case let .bottom(bottom):
-					self.bottom = bottom
-				case let .layoutCenter(layoutCenter):
-					self.layoutCenter = layoutCenter
-				case let .layoutSize(layoutSize):
-					self.layoutSize = layoutSize
-				case let .geoIndex(geoIndex):
-					self.geoIndex = geoIndex
-				case let .mapValueCalculation(mapValueCalculation):
-					self.mapValueCalculation = mapValueCalculation
-				case let .showLegendSymbol(showLegendSymbol):
-					self.showLegendSymbol = showLegendSymbol
-				case let .data(data):
-					self.data = data
-				case let .markPoint(markPoint):
-					self.markPoint = markPoint
-				case let .markLine(markLine):
-					self.markLine = markLine
-				case let .markArea(markArea):
-					self.markArea = markArea
-				case let .silent(silent):
-					self.silent = silent
-			}
-		}
-	}
+    public enum Enums {
+        case name(String), map(String), roam(SECRoam), center(SECPosition), aspectScale(Float), zoom(Float), scaleLimit(ScaleLimit), nameMap([String: SECJsonable]), selectedMode(SECSelectedMode), label(SECLabel), itemStyle(SECItemStyle), zlevel(Float), z(Float), left(SECPosition), top(SECPosition), right(SECPosition), bottom(SECPosition), layoutCenter(SECPosition), layoutSize(SECLength), geoIndex(UInt8), mapValueCalculation(String), showLegendSymbol(Bool), data([SECJsonable]), markPoint(SECMarkPoint), markLine(SECMarkLine), markArea(SECMarkArea), silent(Bool)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public init(_ elements: Enums...) {
+        for ele in elements {
+            switch ele {
+            case let .name(name):
+                self.name = name
+            case let .map(map):
+                self.map = map
+            case let .roam(roam):
+                self.roam = roam
+            case let .center(center):
+                self.center = center
+            case let .aspectScale(aspectScale):
+                self.aspectScale = aspectScale
+            case let .zoom(zoom):
+                self.zoom = zoom
+            case let .scaleLimit(scaleLimit):
+                self.scaleLimit = scaleLimit
+            case let .nameMap(nameMap):
+                self.nameMap = nameMap
+            case let .selectedMode(selectedMode):
+                self.selectedMode = selectedMode
+            case let .label(label):
+                self.label = label
+            case let .itemStyle(itemStyle):
+                self.itemStyle = itemStyle
+            case let .zlevel(zlevel):
+                self.zlevel = zlevel
+            case let .z(z):
+                self.z = z
+            case let .left(left):
+                self.left = left
+            case let .top(top):
+                self.top = top
+            case let .right(right):
+                self.right = right
+            case let .bottom(bottom):
+                self.bottom = bottom
+            case let .layoutCenter(layoutCenter):
+                self.layoutCenter = layoutCenter
+            case let .layoutSize(layoutSize):
+                self.layoutSize = layoutSize
+            case let .geoIndex(geoIndex):
+                self.geoIndex = geoIndex
+            case let .mapValueCalculation(mapValueCalculation):
+                self.mapValueCalculation = mapValueCalculation
+            case let .showLegendSymbol(showLegendSymbol):
+                self.showLegendSymbol = showLegendSymbol
+            case let .data(data):
+                self.data = data
+            case let .markPoint(markPoint):
+                self.markPoint = markPoint
+            case let .markLine(markLine):
+                self.markLine = markLine
+            case let .markArea(markArea):
+                self.markArea = markArea
+            case let .silent(silent):
+                self.silent = silent
+            }
+        }
+    }
 }
 
 extension SECMapSerie : SECMappable {
-	public func mapping(map: SECMap) {
-		map["name"] = name
-		map["map"] = map
-		map["roam"] = roam
-		map["center"] = center
-		map["aspectScale"] = aspectScale
-		map["zoom"] = zoom
-		map["scaleLimit"] = scaleLimit
-		map["nameMap"] = nameMap
-		map["selectedMode"] = selectedMode
-		map["label"] = label
-		map["itemStyle"] = itemStyle
-		map["zlevel"] = zlevel
-		map["z"] = z
-		map["left"] = left
-		map["top"] = top
-		map["right"] = right
-		map["bottom"] = bottom
-		map["layoutCenter"] = layoutCenter
-		map["layoutSize"] = layoutSize
-		map["geoIndex"] = geoIndex
-		map["mapValueCalculation"] = mapValueCalculation
-		map["showLegendSymbol"] = showLegendSymbol
-		map["data"] = data
-		map["markPoint"] = markPoint
-		map["markLine"] = markLine
-		map["markArea"] = markArea
-		map["silent"] = silent
-	}
+    public func mapping(map: SECMap) {
+        map["type"] = type
+        map["name"] = name
+        map["map"] = map
+        map["roam"] = roam
+        map["center"] = center
+        map["aspectScale"] = aspectScale
+        map["zoom"] = zoom
+        map["scaleLimit"] = scaleLimit
+        map["nameMap"] = nameMap
+        map["selectedMode"] = selectedMode
+        map["label"] = label
+        map["itemStyle"] = itemStyle
+        map["zlevel"] = zlevel
+        map["z"] = z
+        map["left"] = left
+        map["top"] = top
+        map["right"] = right
+        map["bottom"] = bottom
+        map["layoutCenter"] = layoutCenter
+        map["layoutSize"] = layoutSize
+        map["geoIndex"] = geoIndex
+        map["mapValueCalculation"] = mapValueCalculation
+        map["showLegendSymbol"] = showLegendSymbol
+        map["data"] = data
+        map["markPoint"] = markPoint
+        map["markLine"] = markLine
+        map["markArea"] = markArea
+        map["silent"] = silent
+    }
 }

3a5210a1aca13743b9f6070e64780f16286c573b
Longhua Zhang <kuaileainits@163.com>
2017-03-11 00:37:35 +0800

    Publish `SECJsMap` for Unit Test

--- SwiftyEcharts/Tools/Json/SECJsMap.swift
+++ SwiftyEcharts/Tools/Json/SECJsMap.swift
@@ -6,25 +6,25 @@
 //  Copyright © 2017 com.pluto-y. All rights reserved.
 //
 
-internal struct SECJsMap {
+public struct SECJsMap {
     private static var jsMap: [String] = [String]()
     
-    static func add(function: String) -> Bool {
+    public static func add(function: String) -> Bool {
         jsMap.append(function)
         return true
     }
     
-    static func remove(function: String) {
+    public static func remove(function: String) {
         if let index = jsMap.indexOf(function) {
             jsMap.removeAtIndex(index)
         }
     }
     
-    static func allFunctions() -> [String] {
+    public static func allFunctions() -> [String] {
         return jsMap
     }
     
-    static func contain(function: String) -> Bool {
+    public static func contain(function: String) -> Bool {
         return jsMap.contains(function)
     }
 }

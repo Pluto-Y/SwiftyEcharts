864b80c5e4b3fc7d7608e228b18ec72ea96fdbb2
Longhua Zhang <kuaileainits@163.com>
2017-03-16 01:18:48 +0800

    Add new demo for `map-china-dataRange`

--- DemoOptions/SECMapOptions.swift
+++ DemoOptions/SECMapOptions.swift
@@ -53,8 +53,137 @@ public struct SECMapOptions {
     // MARK: iPhone销量
     /// 地址: http://echarts.baidu.com/demo.html#map-china-dataRange
     static func mapChinaDataRangeOption() -> SECOption {
-        // TODO: 添加实现
+        let serieTemplate = SECMapSerie(
+            .map("china"),
+            .label(SECLabel(
+                .normal(SECCommonLabelStyle(.show(true))),
+                .emphasis(SECCommonLabelStyle(.show(true)))
+                ))
+        )
+        
+        print(serieTemplate.jsonString)
+        
+        var iphone3Data = serieTemplate
+        iphone3Data.name = "iphone3"
+//        iphone3Data.data = [
+//            SECMapSerieData(.name("北京"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("天津"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("上海"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("重庆"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("河北"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("河南"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("云南"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("辽宁"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("黑龙江"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("湖南"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("安徽"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("山东"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("新疆"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("江苏"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("浙江"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("江西"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("湖北"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("广西"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("甘肃"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("山西"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("内蒙古"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("陕西"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("吉林"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("福建"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("贵州"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("广东"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("青海"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("西藏"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("四川"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("宁夏"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("海南"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("台湾"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("香港"), .value(Float(arc4random_uniform(1000)))),
+//            SECMapSerieData(.name("澳门"), .value(Float(arc4random_uniform(1000))))
+//        ]
+        
+        print(iphone3Data.jsonString)
+        
+        var iphone4Data = serieTemplate
+        iphone4Data.name = "iphone4"
+        iphone4Data.data = [
+            SECMapSerieData(.name("北京"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("天津"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("上海"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("重庆"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("河北"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("安徽"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("新疆"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("浙江"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("江西"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("山西"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("内蒙古"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("吉林"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("福建"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("广东"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("西藏"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("四川"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("宁夏"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("香港"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("澳门"), .value(Float(arc4random_uniform(1000))))
+
+        ]
+        
+        print(iphone4Data.jsonString)
+        
+        var iphone5Data = serieTemplate
+        iphone5Data.name = "iphone5"
+        iphone5Data.data = [
+            SECMapSerieData(.name("北京"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("天津"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("上海"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("广东"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("台湾"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("香港"), .value(Float(arc4random_uniform(1000)))),
+            SECMapSerieData(.name("澳门"), .value(Float(arc4random_uniform(1000))))
+
+        ]
+        
+        print(iphone5Data.jsonString)
+
         return SECOption(
+            .title(SECTitle(
+                .text("iphone销量"),
+                .subtext("纯属虚构"),
+                .left(.center)
+                )),
+            .tooltip(SECTooltip(
+                .trigger(.item)
+                )),
+            .legend(SECLegend(
+                .orient(.vertical),
+                .left(.left),
+                .data(["iphone3","iphone4","iphone5"])
+                )),
+            .visualMap(SECContinuousVisualMap(
+                .min(0),
+                .max(2500),
+                .left(.left),
+                .top(.bottom),
+                .text(["高", "低"]),
+                .calculable(true)
+                )),
+            .toolbox(SECToolbox(
+                .show(true),
+                .orient(.vertical),
+                .left(.right),
+                .top(.center),
+                .feature(SECTFeature(
+                    .dataView(SECTFDataView(.readOnly(false))),
+                    .restore(SECTFRestore()),
+                    .saveAsImage(SECTFSaveAsImage())
+                    ))
+                )),
+            .series([
+                iphone3Data,
+                iphone4Data,
+                iphone5Data
+                ])
         )
     }
 

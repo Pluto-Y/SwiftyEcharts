2b9be0340b0def1739b8d969eeb61f871fa50cf8
Longhua Zhang <kuaileainits@163.com>
2017-03-16 17:26:24 +0800

    Update for map menu

--- iOS_Example/DemoControllers/MapsController.swift
+++ iOS_Example/DemoControllers/MapsController.swift
@@ -13,7 +13,7 @@ class MapsController: BaseDemoController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        menus = ["模拟迁徙(暂缺)", "65k+ 飞机航线(暂缺)", "北京公交路线 - 百度地图(暂缺)", "北京公交路线 - 线特效(暂缺)", "杭州热门步行路线 - 百度地图(暂缺)", "iPhone销量(暂缺)", "Map China(暂缺)", "香港18区人口密度 （2011）(暂缺)", "Map Locate(暂缺)", "34 省切换查看(暂缺)", "USA Population Estimates (2012)(暂缺)", "World Population (2010)(暂缺)", "map and scatter share a geo(暂缺)", "Map World(暂缺)"]
+        menus = ["模拟迁徙(暂缺)", "65k+ 飞机航线(暂缺)", "北京公交路线 - 百度地图(暂缺)", "北京公交路线 - 线特效(暂缺)", "杭州热门步行路线 - 百度地图(暂缺)", "iPhone销量", "Map China", "香港18区人口密度 （2011）(暂缺)", "Map Locate(暂缺)", "34 省切换查看(暂缺)", "USA Population Estimates (2012)(暂缺)", "World Population (2010)(暂缺)", "map and scatter share a geo(暂缺)", "Map World"]
         
         optionClosures = [SECMapOptions.geoLinesOption, SECMapOptions.linesAirlineOption, SECMapOptions.linesBmapBusOption, SECMapOptions.linesBmapEffectOption, SECMapOptions.linesBmapOption, SECMapOptions.mapChinaDataRangeOption, SECMapOptions.mapChinaOption, SECMapOptions.mapHKOption, SECMapOptions.mapLocateOption, SECMapOptions.mapProvinceOption, SECMapOptions.mapUsaOption, SECMapOptions.mapWorldDataRangeOption, SECMapOptions.geoMapScatterOption, SECMapOptions.mapWorldOption]
         

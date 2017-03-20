c46ff23b528bf5f7c06e753c3fce39a7cbf0a993
Longhua Zhang <kuaileainits@163.com>
2017-03-20 23:08:18 +0800

    Add comments for Emphasisable

--- SwiftyEcharts/Models/Protocol/Emphasisable.swift
+++ SwiftyEcharts/Models/Protocol/Emphasisable.swift
@@ -6,8 +6,14 @@
 //  Copyright © 2017 com.pluto-y. All rights reserved.
 //
 
+/// 针对那些有 '正常' 和 '高亮' 两种样式的组件的接口， 例如 `ItemStyle`, `AreaStyle` `Label` 等
 public protocol Emphasisable {
+    
+    /// 样式，针对不同的组件有不同的样式
     associatedtype Style
+    
+    /// 默认样式，即非触发 '高亮' 情况下的样式
     var normal: Style? { get  set }
+    /// 高亮样式
     var emphasis: Style? { get set }
 }

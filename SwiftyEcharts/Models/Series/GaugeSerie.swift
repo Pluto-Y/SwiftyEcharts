04cd43a939dded15ec6d862d348225b977eb9507
Longhua Zhang <kuaileainits@163.com>
2017-03-20 23:01:01 +0800

    Add cooments for `GaugeSerie` and re-structure codes

--- SwiftyEcharts/Models/Series/GaugeSerie.swift
+++ SwiftyEcharts/Models/Series/GaugeSerie.swift
@@ -6,98 +6,176 @@
 //  Copyright © 2017 com.pluto-y. All rights reserved.
 //
 
+/// 仪表盘
+///
+/// 示例：http://echarts.baidu.com/demo.html#gauge-car
 public struct GaugeSerie: Serie, Animatable {
     
-    public struct AxisLine {
+    /// 仪表盘轴线相关配置。
+    public struct AxisLine: Displayable, Line {
+        /// 是否显示仪表盘轴线。
         public var show: Bool?
+        /// 仪表盘轴线样式。
         public var lineStyle: LineStyle?
         
         public init() { }
     }
     
-    public struct SplitLine {
+    /// 分隔线。
+    public struct SplitLine: Displayable, Line {
+        /// 是否显示分隔线。
         public var show: Bool?
+        /// 分隔线线长。支持相对半径的百分比。
         public var length: LengthValue?
+        /// 分割线样式
         public var lineStyle: LineStyle?
         
         public init() { }
     }
     
-    public struct AxisTick {
+    /// 刻度样式。
+    public struct AxisTick: Displayable, Line {
+        /// 是否显示刻度。
         public var show: Bool?
+        /// 分隔线之间分割的刻度数。
         public var splitNumber: UInt8?
+        /// 刻度线长。支持相对半径的百分比。
         public var length: LengthValue?
+        /// 刻度线样式。
         public var lineStyle: LineStyle?
         
         public init() { }
     }
     
-    public struct AxisLabel: Formatted {
+    /// 刻度标签。
+    public struct AxisLabel: Displayable, Formatted, Textful, Colorful {
+        /// 是否显示标签。
         public var show: Bool?
+        /// 标签与刻度线的距离。
         public var distance: Float?
+        /// 刻度标签的内容格式器，支持字符串模板和回调函数两种形式。 示例:
+        ///
+        ///     // 使用字符串模板，模板变量为刻度默认标签 {value}
+        ///     formatter: '{value} kg'
+        ///     
+        ///     // 使用函数模板，函数参数分别为刻度数值
+        ///     formatter: function (value) {
+        ///         return value + 'km/h';
+        ///     }
         public var formatter: Formatter?
+        /// 文字样式
         public var textStyle: TextStyle?
+        /// 刻度颜色
         public var color: Color?
         
         public init() { }
     }
     
-    public struct Pointer {
+    /// 仪表盘指针。
+    public struct Pointer: Displayable {
+        /// 是否显示指针。
         public var show: Bool?
+        /// 指针长度，可以是绝对数值，也可以是相对于半径的半分比。
         public var length: LengthValue?
+        /// 指针宽度。
         public var width: Float?
         
         public init() { }
     }
     
-    public struct Title {
+    /// 仪表盘标题。
+    public struct Title: Displayable {
+        /// 是否显示标题。
         public var show: Bool?
+        /// 相对于仪表盘中心的偏移位置，数组第一项是水平方向的偏移，第二项是垂直方向的偏移。可以是绝对的数值，也可以是相对于仪表盘半径的百分比。
         public var offsetCenter: Point?
+        /// 标题文字样式
         public var textStyle: TextStyle?
         
         public init() { }
     }
     
-    public struct Detail: Formatted, Borderable {
+    /// 仪表盘详情，用于显示数据。
+    public struct Detail: Displayable, Formatted, Borderable, Textful {
+        /// 是否显示详情。
         public var show: Bool?
+        /// 详情宽度。
         public var width: Float?
+        /// 详情高度。
         public var height: Float?
+        /// 详情背景色。
         public var backgroundColor: Color?
+        /// 详情边框宽度。
         public var borderWidth: Float?
+        /// 详情边框颜色。
         public var borderColor: Color?
+        /// 相对于仪表盘中心的偏移位置，数组第一项是水平方向的偏移，第二项是垂直方向的偏移。可以是绝对的数值，也可以是相对于仪表盘半径的百分比。
         public var offsetCenter: Point?
+        /// 格式化函数或者字符串
+        ///
+        ///     formatter: function (value) {
+        ///         return value.toFixed(0);
+        ///     }
         public var formatter: Formatter?
+        /// 详情文字样式
         public var textStyle: TextStyle?
         
         public init() { }
     }
     
+    /// 类型
     public var type: SerieType {
         return .gauge
     }
     
+    /// 系列名称，用于tooltip的显示，legend 的图例筛选，在 setOption 更新数据和配置项时用于指定对应的系列。
     public var name: String?
+    /// 仪表盘半径，可以是相对于容器高宽中较小的一项的一半的百分比，也可以是绝对的数值。
+    ///
+    /// {{ use partial-legend-hover-link }}
     public var radius: LengthValue?
+    /// 仪表盘的中心
     public var center: Point?
+    /// 仪表盘起始角度。圆心 正右手侧为0度，正上方为90度，正左手侧为180度。
     public var startAngle: Float?
+    /// 仪表盘结束角度。
     public var endAngle: Float?
+    /// 仪表盘刻度是否是顺时针增长。
     public var clockwise: Bool?
+    /// 最小的数据值，映射到 minAngle。
     public var min: Float?
+    /// 最大的数据值，映射到 maxAngle。
     public var max: Float?
+    /// 仪表盘刻度的分割段数。
     public var splitNumber: Float?
+    /// 仪表盘轴线相关配置。
     public var axisLine: AxisLine?
+    /// 分隔线样式。
     public var splitLine: SplitLine?
+    /// 刻度样式。
     public var axisTick: AxisTick?
+    /// 刻度标签。
     public var axisLabel: AxisLabel?
+    /// 仪表盘指针。
     public var pointer: Pointer?
+    /// 仪表盘指针样式。
     public var itemStyle: ItemStyle?
+    /// 仪表盘标题。
     public var title: Title?
+    /// 仪表盘详情，用于显示数据。
     public var detail: Detail?
+    /// 图表标注。
     public var markPoint: MarkPoint?
+    /// 图表标线。
     public var markLine: MarkLine?
+    /// 图表标域，常用于标记图表中某个范围的数据，例如标出某段时间投放了广告。
     public var markArea: MarkArea?
+    /// 数据
     public var data: [Jsonable]?
+    /// 图形是否不响应和触发鼠标事件，默认为 false，即响应和触发鼠标事件。
     public var silent: Bool?
+    
+    // MARK: Animatable
     public var animation: Bool?
     public var animationThreshold: Float?
     public var animationDuration: Time?
@@ -455,4 +533,3 @@ extension GaugeSerie: Mappable {
         map["animationDelayUpdate"] = animationDelayUpdate
     }
 }
-

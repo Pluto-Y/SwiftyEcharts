d8d53d071fdfcddfc65d9a859cb3245df6f9521b
Longhua Zhang <kuaileainits@163.com>
2017-04-04 02:09:40 +0800

    feature(demo):Add demo menu for `PictorialBar`

---
+++ iOS_Example/DemoControllers/PictorialBarsController.swift
@@ -0,0 +1,22 @@
+//
+//  PictorialBarsController.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 04/04/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+class PictorialBarsController: BaseDemoController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menus = ["圣诞节儿童愿望清单和山峰高度", "驯鹿的速度(暂缺)", "交通工具(暂缺)", "精灵(暂缺)", "人体含水量(暂缺)", "虚线柱状图效果(暂缺)", "森林的增长(暂缺)"]
+        
+        optionClosures = [PictoriaBarOptions.pictorialBarHillOption, PictoriaBarOptions.pictorialBarVelocityOption, PictoriaBarOptions.pictorialBarVehicleOption, PictoriaBarOptions.pictorialBarSpiritOption, PictoriaBarOptions.pictorialBarBodyFillOption, PictoriaBarOptions.pictorialBarDottedOption, PictoriaBarOptions.pictorialBarForestOption]
+        
+        self.title = "折线图"
+        
+    }
+    
+}

059109edb90170b3c8fd52e0c19678fedd7bc936
Longhua Zhang <kuaileainits@163.com>
2017-04-04 22:06:41 +0800

    feature(demo):Add new demo for 'pictorialBar-velocity'

--- iOS_Example/DemoControllers/PictorialBarsController.swift
+++ iOS_Example/DemoControllers/PictorialBarsController.swift
@@ -11,7 +11,7 @@ class PictorialBarsController: BaseDemoController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        menus = ["圣诞节儿童愿望清单和山峰高度", "驯鹿的速度(暂缺)", "交通工具(暂缺)", "精灵(暂缺)", "人体含水量(暂缺)", "虚线柱状图效果(暂缺)", "森林的增长(暂缺)"]
+        menus = ["圣诞节儿童愿望清单和山峰高度", "驯鹿的速度", "交通工具(暂缺)", "精灵(暂缺)", "人体含水量(暂缺)", "虚线柱状图效果(暂缺)", "森林的增长(暂缺)"]
         
         optionClosures = [PictoriaBarOptions.pictorialBarHillOption, PictoriaBarOptions.pictorialBarVelocityOption, PictoriaBarOptions.pictorialBarVehicleOption, PictoriaBarOptions.pictorialBarSpiritOption, PictoriaBarOptions.pictorialBarBodyFillOption, PictoriaBarOptions.pictorialBarDottedOption, PictoriaBarOptions.pictorialBarForestOption]
         

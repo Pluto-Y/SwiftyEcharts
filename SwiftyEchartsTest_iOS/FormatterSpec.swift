83f7750775a959fa5de1b0c0ddc3f000de1d4d6f
Longhua Zhang <kuaileainits@163.com>
2017-04-26 23:52:51 +0800

    test: Add test case for `Formatter`

---
+++ SwiftyEchartsTest_iOS/FormatterSpec.swift
@@ -0,0 +1,27 @@
+//
+//  FormatterSpec.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 26/04/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import SwiftyEcharts
+
+class FormatterSpec: QuickSpec {
+    override func spec() {
+        describe("For Formatter") {
+            it(" needs to check the jsonString for Formatter ") {
+                let strFormatter: Formatter = .string("{c}%")
+                let funcFormatter: Formatter = .function("function (params) {return params[0];}")
+                expect(strFormatter.jsonString).to(equal("{c}%".jsonString))
+                JsCache.removeAll()
+                expect(funcFormatter.jsonString).to(equal("\"\(FormatterFuncPrefix)0\""))
+                expect(JsCache.allJsStrings()[0]).to(equal("var \(FormatterFuncPrefix)0 = function (params) {return params[0];};"))
+                
+            }
+        }
+    }
+}

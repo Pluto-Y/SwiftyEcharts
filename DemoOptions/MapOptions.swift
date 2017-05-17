6263119ff9d4871fd9a7fe1aba041f5a788d272a
Longhua Zhang <kuaileainits@163.com>
2017-05-17 23:24:17 +0800

    feature(demo): Add new demo for `map-HK`

--- DemoOptions/MapOptions.swift
+++ DemoOptions/MapOptions.swift
@@ -567,8 +567,90 @@ public final class MapOptions {
     // MARK: 香港18区人口密度 （2011）
     /// 地址: http://echarts.baidu.com/demo.html#map-HK
     static func mapHKOption() -> Option {
-        // TODO: 添加实现
         return Option(
+            .title(Title(
+                .text("香港18区人口密度 （2011）"),
+                .subtext("人口密度数据来自Wikipedia"),
+                .sublink("http://zh.wikipedia.org/wiki/%E9%A6%99%E6%B8%AF%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83#cite_note-12")
+                )),
+            .tooltip(Tooltip(
+                .trigger(.item),
+                .formatter(.string("{b}<br/>{c} (p / km2)"))
+                )),
+            .toolbox(Toolbox(
+                .show(true),
+                .orient(.vertical),
+                .left(.right),
+                .top(.center),
+                .feature(ToolboxFeature(
+                    .dataView(ToolboxFeatureDataView(
+                        .readOnly(false)
+                        )),
+                    .restore(ToolboxFeatureRestore()),
+                    .saveAsImage(ToolboxFeatureSaveAsImage())
+                    ))
+                )),
+            .visualMap(ContinuousVisualMap(
+                .min(800),
+                .max(50000),
+                .text(["High", "Low"]),
+                .realtime(false),
+                .calculable(true),
+                .inRange([
+                    "color": ["lightskyblue", "yellow", "orangered"]
+                    ])
+                )),
+            .series([
+                MapSerie(
+                    .name("香港18区人口密度"),
+                    .map("HK"), // 自定义扩展图表类型
+                    .label(Label( // 由ItemStyle中直接抽象出来即可用
+                        .normal(CommonLabelStyle(.show(true))),
+                        .emphasis(CommonLabelStyle(.show(true)))
+                        )),
+                    .data([
+                        [ "name": "中西区", "value": 20057.34 ],
+                        [ "name": "湾仔", "value": 15477.48 ],
+                        [ "name": "东区", "value": 31686.1 ],
+                        [ "name": "南区", "value": 6992.6 ],
+                        [ "name": "油尖旺", "value": 44045.49 ],
+                        [ "name": "深水埗", "value": 40689.64 ],
+                        [ "name": "九龙城", "value": 37659.78 ],
+                        [ "name": "黄大仙", "value": 45180.97 ],
+                        [ "name": "观塘", "value": 55204.26 ],
+                        [ "name": "葵青", "value": 21900.9 ],
+                        [ "name": "荃湾", "value": 4918.26 ],
+                        [ "name": "屯门", "value": 5881.84 ],
+                        [ "name": "元朗", "value": 4178.01 ],
+                        [ "name": "北区", "value": 2227.92 ],
+                        [ "name": "大埔", "value": 2180.98 ],
+                        [ "name": "沙田", "value": 9172.94 ],
+                        [ "name": "西贡", "value": 3368 ],
+                        [ "name": "离岛", "value": 806.98 ]
+                        ]),
+                    // 自定义名称映射
+                    .nameMap([
+                        "Central and Western": "中西区",
+                        "Eastern": "东区",
+                        "Islands": "离岛",
+                        "Kowloon City": "九龙城",
+                        "Kwai Tsing": "葵青",
+                        "Kwun Tong": "观塘",
+                        "North": "北区",
+                        "Sai Kung": "西贡",
+                        "Sha Tin": "沙田",
+                        "Sham Shui Po": "深水埗",
+                        "Southern": "南区",
+                        "Tai Po": "大埔",
+                        "Tsuen Wan": "荃湾",
+                        "Tuen Mun": "屯门",
+                        "Wan Chai": "湾仔",
+                        "Wong Tai Sin": "黄大仙",
+                        "Yau Tsim Mong": "油尖旺",
+                        "Yuen Long": "元朗"
+                        ])
+                )
+                ])
         )
     }
     
--- iOS_Example/DemoControllers/MapsController.swift
+++ iOS_Example/DemoControllers/MapsController.swift
@@ -16,7 +16,7 @@ class MapsController: BaseDemoController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        menus = ["模拟迁徙", "65k+ 飞机航线", "北京公交路线 - 百度地图(暂缺)", "北京公交路线 - 线特效(暂缺)", "杭州热门步行路线 - 百度地图(暂缺)", "iPhone销量", "Map China", "香港18区人口密度 （2011）(暂缺)", "Map Locate(暂缺)", "34 省切换查看(暂缺)", "USA Population Estimates (2012)(暂缺)", "World Population (2010)", "map and scatter share a geo", "Map World"]
+        menus = ["模拟迁徙", "65k+ 飞机航线", "北京公交路线 - 百度地图(暂缺)", "北京公交路线 - 线特效(暂缺)", "杭州热门步行路线 - 百度地图(暂缺)", "iPhone销量", "Map China", "香港18区人口密度 （2011）", "Map Locate(暂缺)", "34 省切换查看(暂缺)", "USA Population Estimates (2012)(暂缺)", "World Population (2010)", "map and scatter share a geo", "Map World"]
         
         optionClosures = [MapOptions.geoLinesOption, MapOptions.linesAirlineOption, MapOptions.linesBmapBusOption, MapOptions.linesBmapEffectOption, MapOptions.linesBmapOption, MapOptions.mapChinaDataRangeOption, MapOptions.mapChinaOption, MapOptions.mapHKOption, MapOptions.mapLocateOption, MapOptions.mapProvinceOption, MapOptions.mapUsaOption, MapOptions.mapWorldDataRangeOption, MapOptions.geoMapScatterOption, MapOptions.mapWorldOption]
         
@@ -34,6 +34,24 @@ class MapsController: BaseDemoController {
                     self.option = tmpOption
                     self.echartsView.hideLoading()
                 })
+            })
+        } else if indexPath.row == 7 {
+            echartsView.showLoading()
+            dispatch_async(dispatch_get_global_queue(0, 0), {
+                guard let jsonUrl = NSBundle.mainBundle().URLForResource("HK", withExtension: "json"), let jsonData = NSData(contentsOfURL: jsonUrl), let jsonObj = try? NSJSONSerialization.JSONObjectWithData(jsonData, options: []) else {
+                    self.echartsView.hideLoading()
+                    return
+                }
+                
+                let data = jsonObj as! NSDictionary
+                let tmpOption = self.optionClosures[indexPath.row]()
+                
+                dispatch_sync(dispatch_get_main_queue(), {
+                    self.echartsView.registerMap("HK", data: data)
+                    self.option = tmpOption
+                    self.echartsView.hideLoading()
+                })
+                
             })
         } else {
             option = optionClosures[indexPath.row]()

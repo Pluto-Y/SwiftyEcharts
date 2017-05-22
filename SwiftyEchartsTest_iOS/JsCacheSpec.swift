f3e77451f7b290c4aaa6ebdd3673cf7c87b0bbb1
Longhua Zhang <kuaileainits@163.com>
2017-05-22 22:59:56 +0800

    test: Add file for test `JsCache`

---
+++ SwiftyEchartsTest_iOS/JsCacheSpec.swift
@@ -0,0 +1,21 @@
+//
+//  JsCacheSpec.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 22/05/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SwiftyEcharts
+
+class JsCacheSpec: QuickSpec {
+    override func spec() {
+        describe("For JsCache") {
+            it(" needs to check the methods ") {
+                
+            }
+        }
+    }
+}

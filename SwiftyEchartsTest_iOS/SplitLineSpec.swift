fc77b2d114dae8f088b896ec0c62fd5ff637f0a6
Longhua Zhang <kuaileainits@163.com>
2017-06-22 23:22:28 +0800

    test: Add test case file for `SplitLine`

---
+++ SwiftyEchartsTest_iOS/SplitLineSpec.swift
@@ -0,0 +1,57 @@
+//
+//  SplitLineSpec.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 22/06/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import SwiftyEcharts
+
+class SplitLineSpec: QuickSpec {
+    override func spec() {
+        describe("For SplitLine") {
+            
+            let showValue = false
+            let intervalValue: UInt = 2
+            let lineStyleValue = LineStyle(
+                .color(Color.red),
+                .width(30),
+                .type(.dotted),
+                .opacity(0.88),
+                .shadowBlur(0.65),
+                .shadowColor("#fff"),
+                .shadowOffsetX(16.5),
+                .shadowOffsetY(8.9992),
+                .curveness(0.72)
+            )
+            
+            let dic: [String: Jsonable] = [
+                "show": showValue,
+                "interval": intervalValue,
+                "lineStyle": lineStyleValue
+            ]
+            
+            let splitLine = SplitLine()
+            splitLine.show = showValue
+            splitLine.interval = intervalValue
+            splitLine.lineStyle = lineStyleValue
+            
+            it(" needs to check the jsonString ") {
+                expect(splitLine.jsonString).to(equal(dic.jsonString))
+            }
+            
+            it(" needs to check the Enumable ") {
+                let splitLineByEnums = SplitLine(
+                    .show(showValue),
+                    .interval(intervalValue),
+                    .lineStyle(lineStyleValue)
+                )
+                
+                expect(splitLineByEnums.jsonString).to(equal(splitLine.jsonString))
+            }
+        }
+    }
+}

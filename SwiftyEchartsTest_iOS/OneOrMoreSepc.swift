c758e557ec3d7fd3af89aef8a5fd4f8d4c218e88
Longhua Zhang <kuaileainits@163.com>
2017-06-25 21:15:09 +0800

    test: Add test cases for `OneOrMore`

--- SwiftyEchartsTest_iOS/OneOrMoreSepc.swift
+++ SwiftyEchartsTest_iOS/OneOrMoreSepc.swift
@@ -20,7 +20,7 @@ class OneOrMoreSepc : QuickSpec {
             let finalElement = false
             let oneValue = "Hello"
             let moreValue: [Any] = [firstElement, secondElement, thirdElement, finalElement]
-            it(" needs to check the construct and jsonString") {
+            it("needs to check the construct and jsonString") {
                 // 测试默认构造器以及 jsonString 是否正确
                 let one = OneOrMore(one: oneValue)
                 expect(one.jsonString).to(equal(oneValue.jsonString))
@@ -28,10 +28,20 @@ class OneOrMoreSepc : QuickSpec {
                 // 测试默认构造器以及 jsonString 是否正确
                 let more = OneOrMore(more: moreValue)
                 expect(more.jsonString).to(equal(moreValue.jsonString))
+                
+                // 测试默认数组只有一个元素的情况
+                let onlyOneElements = OneOrMore(more: [firstElement])
+                expect(onlyOneElements.jsonString).to(equal(firstElement.jsonString))
+                expect(onlyOneElements.jsonString).notTo(equal([firstElement].jsonString))
+                
+                // 测试另一个构造器的情况
+                let onlyOneElements2 = OneOrMore(arrayLiteral: firstElement)
+                expect(onlyOneElements2.jsonString).to(equal(firstElement.jsonString))
+                expect(onlyOneElements2.jsonString).notTo(equal([firstElement].jsonString))
             }
             
             // 测试数组常量的构造器
-            it(" needs to check the ArrayLiteralConvertible") {
+            it("needs to check the ArrayLiteralConvertible") {
                 let more: OneOrMore<Any> = [firstElement, secondElement, thirdElement, finalElement]
                 expect(more.jsonString).to(equal(moreValue.jsonString))
                 
@@ -40,12 +50,22 @@ class OneOrMoreSepc : QuickSpec {
                 expect(more1.jsonString).to(equal(emptyArray.jsonString))
             }
             
-            it(" needs to check the subscript ") {
+            it("needs to check the subscript ") {
                 // 测试下标是否能其作用
                 expect(moreValue[0] as? String).to(equal(firstElement))
                 expect(moreValue[1] as? Double).to(equal(secondElement))
                 expect(moreValue[2] as? Int).to(equal(thirdElement))
                 expect(moreValue[3] as? Bool).to(equal(finalElement))
+                
+                let intValue = 10
+                let intValue2 = 18
+                let oneInt = OneOrMore<Int>(one: intValue)
+                expect(oneInt[0]).to(equal(intValue))
+                expect(oneInt[1]).to(beNil())
+                
+                let moreInts = OneOrMore(more: [intValue, intValue2])
+                expect(moreInts[0]).to(equal(intValue))
+                expect(moreInts[1]).to(equal(intValue2))
             }
             
         }

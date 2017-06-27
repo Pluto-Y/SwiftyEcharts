ab500bd2ccf5f5d8f97c73c0a07bc26a8a7c0583
Longhua Zhang <kuaileainits@163.com>
2017-06-27 16:17:13 +0800

    test: Add test cases for `ArcGraphic`

--- SwiftyEchartsTest_iOS/ArcGraphicSpec.swift
+++ SwiftyEchartsTest_iOS/ArcGraphicSpec.swift
@@ -58,5 +58,98 @@ class ArcGraphicSpec: QuickSpec {
                 expect(shapeByEnums.jsonString).to(equal(shape.jsonString))
             }
         }
+        
+        describe("For ArcGraphic") {
+            let idValue = "argraphicId"
+            let actionValue = GraphicAction.merge
+            let leftValue = Position.inside
+            let rightValue = Position.right
+            let topValue = Position.bottom
+            let bottomValue = Position.end
+            let boundingValue = GraphicBounding.raw
+            let zValue: Float = 23.5
+            let zlevelValue: Float = 11.11
+            let silentValue = false
+            let invisibleValue = true
+            let cursorValue = "graphicArcCursor"
+            let draggableValue = false
+            let progressiveValue = true
+            let shapeValue = shape
+            let styleValue = CommonGraphicStyle(
+                .fill(Color.red),
+                .lineWidth(12.5),
+                .shadowColor(Color.green),
+                .shadowOffsetY(5.55)
+            )
+            
+            let arcGraphic = ArcGraphic()
+            arcGraphic.id = idValue
+            arcGraphic.action = actionValue
+            arcGraphic.left = leftValue
+            arcGraphic.right = rightValue
+            arcGraphic.top = topValue
+            arcGraphic.bottom = bottomValue
+            arcGraphic.bounding = boundingValue
+            arcGraphic.z = zValue
+            arcGraphic.zlevel = zlevelValue
+            arcGraphic.silent = silentValue
+            arcGraphic.invisible = invisibleValue
+            arcGraphic.cursor = cursorValue
+            arcGraphic.draggable = draggableValue
+            arcGraphic.progressive = progressiveValue
+            arcGraphic.shape = shapeValue
+            arcGraphic.style = styleValue
+            
+            it("needs to check the type value") {
+                expect(arcGraphic.type).to(equal(GraphicType.arc))
+            }
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "type": GraphicType.arc,
+                    "id": idValue,
+                    "$action": actionValue,
+                    "left": leftValue,
+                    "right": rightValue,
+                    "top": topValue,
+                    "bottom": bottomValue,
+                    "bounding": boundingValue,
+                    "z": zValue,
+                    "zlevel": zlevelValue,
+                    "silent": silentValue,
+                    "invisible": invisibleValue,
+                    "cursor": cursorValue,
+                    "draggable": draggableValue,
+                    "progressive": progressiveValue,
+                    "shape": shapeValue,
+                    "style": styleValue
+                ]
+
+                expect(arcGraphic.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let arcGraphicByEnums = ArcGraphic(
+                    .id(idValue),
+                    .action(actionValue),
+                    .left(leftValue),
+                    .right(rightValue),
+                    .top(topValue),
+                    .bottom(bottomValue),
+                    .bounding(boundingValue),
+                    .z(zValue),
+                    .zlevel(zlevelValue),
+                    .silent(silentValue),
+                    .invisible(invisibleValue),
+                    .cursor(cursorValue),
+                    .draggable(draggableValue),
+                    .progressive(progressiveValue),
+                    .shape(shapeValue),
+                    .style(styleValue)
+                )
+                
+                expect(arcGraphicByEnums.jsonString).to(equal(arcGraphic.jsonString))
+            }
+        }
     }
 }

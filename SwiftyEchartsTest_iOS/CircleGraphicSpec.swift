b1b4fe8619d625c27628934a51be4f92a32e98ef
Longhua Zhang <kuaileainits@163.com>
2017-07-08 18:05:25 +0800

    test: Add test cases for `CircleGraphic`

--- SwiftyEchartsTest_iOS/CircleGraphicSpec.swift
+++ SwiftyEchartsTest_iOS/CircleGraphicSpec.swift
@@ -41,5 +41,98 @@ class CircleGraphicSpec: QuickSpec {
                 expect(shapeByEnums.jsonString).to(equal(shape.jsonString))
             }
         }
+        
+        describe("For CircleGraphic") {
+            let idValue = "circleGraphicIdValue"
+            let actionValue = GraphicAction.remove
+            let leftValue = Position.left
+            let rightValue = Position.right
+            let topValue = Position.top
+            let bottomValue = Position.bottom
+            let boundingValue = GraphicBounding.all
+            let zValue: Float = 5.2234
+            let zlevelValue: Float = 521
+            let silentValue = false
+            let invisibleValue = true
+            let cursorValue = "cicleGraphicCursor"
+            let draggableValue = false
+            let progressiveValue = true
+            let shapeValue = shape
+            let styleValue = CommonGraphicStyle(
+                .shadowOffsetX(123.5),
+                .stroke(Color.hexColor("#582521")),
+                .lineWidth(23.5)
+            )
+            
+            let circleGraphic = CircleGraphic()
+            circleGraphic.id = idValue
+            circleGraphic.action = actionValue
+            circleGraphic.left = leftValue
+            circleGraphic.right = rightValue
+            circleGraphic.top = topValue
+            circleGraphic.bottom = bottomValue
+            circleGraphic.bounding = boundingValue
+            circleGraphic.z = zValue
+            circleGraphic.zlevel = zlevelValue
+            circleGraphic.silent = silentValue
+            circleGraphic.invisible = invisibleValue
+            circleGraphic.cursor = cursorValue
+            circleGraphic.draggable = draggableValue
+            circleGraphic.progressive = progressiveValue
+            circleGraphic.shape = shapeValue
+            circleGraphic.style = styleValue
+            
+            
+            it("needs to check the type value") {
+                expect(circleGraphic.type).to(equal(GraphicType.circle))
+            }
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "type": GraphicType.circle,
+                    "id": idValue,
+                    "$action": actionValue,
+                    "left": leftValue,
+                    "right": rightValue,
+                    "top": topValue,
+                    "bottom": bottomValue,
+                    "bounding": boundingValue,
+                    "z": zValue,
+                    "zlevel": zlevelValue,
+                    "silent": silentValue,
+                    "invisible": invisibleValue,
+                    "cursor": cursorValue,
+                    "draggable": draggableValue,
+                    "progressive": progressiveValue,
+                    "shape": shapeValue,
+                    "style": styleValue
+                ]
+                
+                expect(circleGraphic.jsonString).to(equal(resultDic.jsonString))
+            }
+
+            it("needs to check the Enumable") {
+                let circleGraphicByEnums = CircleGraphic(
+                    .id(idValue),
+                    .action(actionValue),
+                    .left(leftValue),
+                    .right(rightValue),
+                    .top(topValue),
+                    .bottom(bottomValue),
+                    .bounding(boundingValue),
+                    .z(zValue),
+                    .zlevel(zlevelValue),
+                    .silent(silentValue),
+                    .invisible(invisibleValue),
+                    .cursor(cursorValue),
+                    .draggable(draggableValue),
+                    .progressive(progressiveValue),
+                    .shape(shapeValue),
+                    .style(styleValue)
+                )
+                
+                expect(circleGraphicByEnums.jsonString).to(equal(circleGraphic.jsonString))
+            }
+        }
     }
 }

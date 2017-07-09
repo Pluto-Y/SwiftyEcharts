5a3bcdfcede3ef43c1cc67294f7beed2ed24c6a0
Longhua Zhang <kuaileainits@163.com>
2017-07-09 17:24:36 +0800

    test: Add test case for `EffectScatterSerie.RippleEffect`

--- SwiftyEchartsTest_iOS/EffectScatterSerieSpec.swift
+++ SwiftyEchartsTest_iOS/EffectScatterSerieSpec.swift
@@ -33,5 +33,36 @@ class EffectScatterSerieSpec: QuickSpec {
             expect(strokeBrushType.jsonString).to(equal(strokeString.jsonString))
             expect(fillBrushType.jsonString).to(equal(fillString.jsonString))
         }
+        
+        let periodValue: Float = 5.0
+        let scaleValue: Float = 1.5
+        let brushTypeValue = strokeBrushType
+        
+        let rippleEffect = EffectScatterSerie.RippleEffect()
+        rippleEffect.period = periodValue
+        rippleEffect.scale = scaleValue
+        rippleEffect.brushType = brushTypeValue
+        
+        describe("For EffectScatterSerie.RippleEffect") {
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "period": periodValue,
+                    "scale": scaleValue,
+                    "brushType": brushTypeValue
+                ]
+                
+                expect(rippleEffect.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let rippleEffectByEnums = EffectScatterSerie.RippleEffect(
+                    .period(periodValue),
+                    .scale(scaleValue),
+                    .brushType(brushTypeValue)
+                )
+                
+                expect(rippleEffectByEnums.jsonString).to(equal(rippleEffect.jsonString))
+            }
+        }
     }
 }

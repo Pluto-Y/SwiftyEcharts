b8e506e37d137416bcea3c060e733cfa9dc300eb
Longhua Zhang <kuaileainits@163.com>
2017-07-15 19:47:18 +0800

    test: Add test cases for `Geo`

--- SwiftyEchartsTest_iOS/GeoSepc.swift
+++ SwiftyEchartsTest_iOS/GeoSepc.swift
@@ -12,7 +12,6 @@ import Nimble
 
 class GeoSepc: QuickSpec {
     override func spec() {
-        
         let minScaleLimitValue: Float = 2.552
         let maxScaleLimitValue: Float = 100.100
         
@@ -83,6 +82,126 @@ class GeoSepc: QuickSpec {
                 )
                 
                 expect(regionByEnums.jsonString).to(equal(region.jsonString))
+            }
+        }
+        
+        describe("For Geo") {
+            let showValue = true
+            let mapValue = "China"
+            let roamValue = false
+            let centerValue: Point = [1.28521, 4832.273]
+            let aspectScaleValue: Float = 4.245
+            let zoomValue: Float = 3.245
+            let scaleLimitValue = scaleLimit
+            let nameMapValue: String = "{'China': '中国'}"
+            let selectedModeValue = SelectedMode.multiple
+            let labelValue = FormattedLabel(
+                .normal(FormattedLabelStyle(
+                    .show(true),
+                    .position(.outside),
+                    .offset([10, 0]),
+                    .textStyle(TextStyle(
+                        .fontSize(6)
+                        ))
+                    ))
+            )
+            let itemStyleValue = ItemStyle(
+                .normal(CommonItemStyleContent(
+                    .opacity(0.8),
+                    .shadowBlur(10),
+                    .shadowOffsetX(0),
+                    .shadowOffsetY(0),
+                    .shadowColor(.rgba(0, 0, 0, 0.5))
+                    ))
+            )
+            let zlevelValue: Float = 5.22
+            let zValue: Float = 2.1023
+            let leftValue = Position.insideLeft
+            let rightValue = Position.insideRight
+            let bottomValue = Position.insideBottom
+            let topValue = Position.insideTop
+            let layoutCenterValue = "bottom"
+            let layoutSizeValue = "['30%', '30%']"
+            let regionsValue: [Geo.Region] = [region]
+            let silentValue = true
+            
+            let geo = Geo()
+            geo.show = showValue
+            geo.map = mapValue
+            geo.roam = roamValue
+            geo.center = centerValue
+            geo.aspectScale = aspectScaleValue
+            geo.zoom = zoomValue
+            geo.scaleLimit = scaleLimitValue
+            geo.nameMap = nameMapValue
+            geo.selectedMode = selectedModeValue
+            geo.label = labelValue
+            geo.itemStyle = itemStyleValue
+            geo.zlevel = zlevelValue
+            geo.z = zValue
+            geo.left = leftValue
+            geo.top = topValue
+            geo.right = rightValue
+            geo.bottom = bottomValue
+            geo.layoutCenter = layoutCenterValue
+            geo.layoutSize = layoutSizeValue
+            geo.regions = regionsValue
+            geo.silent = silentValue
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "show": showValue,
+                    "map": mapValue,
+                    "roam": roamValue,
+                    "center": centerValue,
+                    "aspectScale": aspectScaleValue,
+                    "zoom": zoomValue,
+                    "scaleLimit": scaleLimitValue,
+                    "nameMap": nameMapValue,
+                    "selectedMode": selectedModeValue,
+                    "label": labelValue,
+                    "itemStyle": itemStyleValue,
+                    "zlevel": zlevelValue,
+                    "z": zValue,
+                    "left": leftValue,
+                    "top": topValue,
+                    "right": rightValue,
+                    "bottom": bottomValue,
+                    "layoutCenter": layoutCenterValue,
+                    "layoutSize": layoutSizeValue,
+                    "regions": regionsValue,
+                    "silent": silentValue
+                ]
+                
+                expect(geo.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let geoByEnums = Geo(
+                    .show(showValue),
+                    .map(mapValue),
+                    .roam(roamValue),
+                    .center(centerValue),
+                    .aspectScale(aspectScaleValue),
+                    .zoom(zoomValue),
+                    .scaleLimit(scaleLimitValue),
+                    .nameMap(nameMapValue),
+                    .selectedMode(selectedModeValue),
+                    .label(labelValue),
+                    .itemStyle(itemStyleValue),
+                    .zlevel(zlevelValue),
+                    .z(zValue),
+                    .left(leftValue),
+                    .top(topValue),
+                    .right(rightValue),
+                    .bottom(bottomValue),
+                    .layoutCenter(layoutCenterValue),
+                    .layoutSize(layoutSizeValue),
+                    .regions(regionsValue),
+                    .silent(silentValue)
+                )
+                
+                expect(geoByEnums.jsonString).to(equal(geo.jsonString))
             }
         }
     }

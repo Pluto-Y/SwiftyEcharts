a9f9adf2183f4aaf0c512c2c7edeed0b5e02da00
Longhua Zhang <kuaileainits@163.com>
2017-07-17 22:58:24 +0800

    test: Add test cases for `ImageGraphic`

--- SwiftyEchartsTest_iOS/ImageGraphicSpec.swift
+++ SwiftyEchartsTest_iOS/ImageGraphicSpec.swift
@@ -79,5 +79,89 @@ class ImageGraphicSpec: QuickSpec {
                 expect(styleByEnums.jsonString).to(equal(style.jsonString))
             }
         }
+        
+        describe("For ImageGraphic") {
+            let idValue = "imageGraphicIdValue"
+            let actionValue = GraphicAction.remove
+            let leftValue = Position.middle
+            let rightValue = Position.start
+            let topValue = Position.point([20, 300])
+            let bottomValue = Position.outside
+            let boundingValue = GraphicBounding.all
+            let zValue: Float = 2.852
+            let zlevelValue: Float = 0.0001
+            let silentValue = true
+            let invisibleValue = false
+            let cursorValue = "imageGraphicCursorValue"
+            let draggableValue = false
+            let progressiveValue = true
+            let styleValue = style
+            
+            let imageGraphic = ImageGraphic()
+            imageGraphic.id = idValue
+            imageGraphic.action = actionValue
+            imageGraphic.left = leftValue
+            imageGraphic.right = rightValue
+            imageGraphic.top = topValue
+            imageGraphic.bottom = bottomValue
+            imageGraphic.bounding = boundingValue
+            imageGraphic.z = zValue
+            imageGraphic.zlevel = zlevelValue
+            imageGraphic.silent = silentValue
+            imageGraphic.invisible = invisibleValue
+            imageGraphic.cursor = cursorValue
+            imageGraphic.draggable = draggableValue
+            imageGraphic.progressive = progressiveValue
+            imageGraphic.style = styleValue
+            
+            it("needs to check the type value") {
+                expect(imageGraphic.type.jsonString).to(equal(GraphicType.image.jsonString))
+            }
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "type": GraphicType.image,
+                    "id": idValue,
+                    "$action": actionValue,
+                    "left": leftValue,
+                    "right": rightValue,
+                    "top": topValue,
+                    "bottom": bottomValue,
+                    "bounding": boundingValue,
+                    "z": zValue,
+                    "zlevel": zlevelValue,
+                    "silent": silentValue,
+                    "invisible": invisibleValue,
+                    "cursor": cursorValue,
+                    "draggable": draggableValue,
+                    "progressive": progressiveValue,
+                    "style": styleValue
+                ]
+                
+                expect(imageGraphic.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let imageGraphicByEnunms = ImageGraphic(
+                    .id(idValue),
+                    .action(actionValue),
+                    .left(leftValue),
+                    .right(rightValue),
+                    .top(topValue),
+                    .bottom(bottomValue),
+                    .bounding(boundingValue),
+                    .z(zValue),
+                    .zlevel(zlevelValue),
+                    .silent(silentValue),
+                    .invisible(invisibleValue),
+                    .cursor(cursorValue),
+                    .draggable(draggableValue),
+                    .progressive(progressiveValue),
+                    .style(styleValue)
+                )
+                
+                expect(imageGraphicByEnunms.jsonString).to(equal(imageGraphic.jsonString))
+            }
+        }
     }
 }

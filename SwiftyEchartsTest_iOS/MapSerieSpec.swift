d921734b40ae04490f41514430822177185abf79
Longhua Zhang <kuaileainits@163.com>
2017-07-25 00:09:49 +0800

    test: Add test cases for `MapSerie.Data`

--- SwiftyEchartsTest_iOS/MapSerieSpec.swift
+++ SwiftyEchartsTest_iOS/MapSerieSpec.swift
@@ -38,5 +38,55 @@ class MapSerieSpec: QuickSpec {
                 expect(scaleLimitByEnums.jsonString).to(equal(scaleLimit.jsonString))
             }
         }
+        
+        let nameDataValue = "dataNameValue"
+        let valueDataValue: Float = 29284.02383
+        let selectedDataValue = false
+        let itemStyleDataValue = ItemStyle(
+            .normal(CommonItemStyleContent(
+                .barBorderRadius(284.2),
+                .opacity(182.09)
+                )),
+            .emphasis(CommonItemStyleContent(
+                .color(rgba(20, 238, 82, 0.284)),
+                .shadowBlur(204.28)
+                ))
+        )
+        let labelDataValue = Label(
+            .emphasis(CommonLabelStyle(.show(false)))
+        )
+        
+        let data = MapSerie.Data()
+        data.name = nameDataValue
+        data.value = valueDataValue
+        data.selected = selectedDataValue
+        data.itemStyle = itemStyleDataValue
+        data.label = labelDataValue
+        
+        describe("For MapSerie.Data") { 
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "name": nameDataValue,
+                    "value": valueDataValue,
+                    "selected": selectedDataValue,
+                    "itemStyle": itemStyleDataValue,
+                    "label": labelDataValue
+                ]
+                
+                expect(data.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let dataByEnums = MapSerie.Data(
+                    .name(nameDataValue),
+                    .value(valueDataValue),
+                    .selected(selectedDataValue),
+                    .itemStyle(itemStyleDataValue),
+                    .label(labelDataValue)
+                )
+                
+                expect(dataByEnums.jsonString).to(equal(data.jsonString))
+            }
+        }
     }
 }

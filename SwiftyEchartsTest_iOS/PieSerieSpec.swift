e815ce48c55a2384b6425831d89e18ddf805108b
Longhua Zhang <kuaileainits@163.com>
2017-07-29 16:46:16 +0800

    test: Add test cases for `PieSerie.LabelLineContent`

---
+++ SwiftyEchartsTest_iOS/PieSerieSpec.swift
@@ -0,0 +1,57 @@
+//
+//  PieSerieSpec.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 29/07/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import SwiftyEcharts
+
+class PieSerieSpec: QuickSpec {
+    override func spec() {
+        let showLabelLineContentValue = false
+        let lengthLabelLineContentValue: Float = 7.77777
+        let length2LabelLineContentValue: Float = 65.374
+        let smoothLabelLineContentValue = true
+        let lineStyleLabelLineContentValue = LineStyle(
+            .curveness(6.485),
+            .type(LineType.dashed)
+        )
+        
+        let labelLineContent = PieSerie.LabelLineContent()
+        labelLineContent.show = showLabelLineContentValue
+        labelLineContent.length = lengthLabelLineContentValue
+        labelLineContent.length2 = length2LabelLineContentValue
+        labelLineContent.smooth = smoothLabelLineContentValue
+        labelLineContent.lineStyle = lineStyleLabelLineContentValue
+        
+        describe("For PieSerie.LabelLineContent") { 
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "show": showLabelLineContentValue,
+                    "length": lengthLabelLineContentValue,
+                    "length2": length2LabelLineContentValue,
+                    "smooth": smoothLabelLineContentValue,
+                    "lineStyle": lineStyleLabelLineContentValue
+                ]
+                
+                expect(labelLineContent.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let labelLineContentByEnums = PieSerie.LabelLineContent(
+                    .show(showLabelLineContentValue),
+                    .length(lengthLabelLineContentValue),
+                    .length2(length2LabelLineContentValue),
+                    .smooth(smoothLabelLineContentValue),
+                    .lineStyle(lineStyleLabelLineContentValue)
+                )
+                
+                expect(labelLineContentByEnums.jsonString).to(equal(labelLineContent.jsonString))
+            }
+        }
+    }
+}

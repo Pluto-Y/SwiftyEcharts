81680ef465f29518eb3426451707b9ba310b18e3
Longhua Zhang <kuaileainits@163.com>
2017-08-03 11:47:23 +0800

    feature(model): Add new model for `ShadowStyle`

--- SwiftyEcharts/Models/Protocol/Shadowable.swift
+++ SwiftyEcharts/Models/Protocol/Shadowable.swift
@@ -24,3 +24,55 @@ public protocol Shadowable {
     /// 阴影垂直方向上的偏移距离。
     var shadowOffsetY: Float? { get set }
 }
+
+public final class ShadowStyle: Shadowable, Opacitable {
+    
+    public var color: Color?
+    public var shadowBlur: Float?
+    public var shadowColor: Color?
+    public var shadowOffsetX: Float?
+    public var shadowOffsetY: Float?
+    public var opacity: Float?
+    
+    public init() { }
+}
+
+extension ShadowStyle: Enumable {
+    public enum Enums {
+        case color(Color), shadowBlur(Float), shadowColor(Color), shadowOffsetX(Float), shadowOffsetY(Float), opacity(Float)
+    }
+    
+    public typealias ContentEnum = Enums
+    
+    public convenience init(_ elements: Enums...) {
+        self.init()
+        for ele in elements {
+            switch ele {
+                case let .color(color):
+                    self.color = color
+                case let .shadowBlur(shadowBlur):
+                    self.shadowBlur = shadowBlur
+                case let .shadowColor(shadowColor):
+                    self.shadowColor = shadowColor
+                case let .shadowOffsetX(shadowOffsetX):
+                    self.shadowOffsetX = shadowOffsetX
+                case let .shadowOffsetY(shadowOffsetY):
+                    self.shadowOffsetY = shadowOffsetY
+                case let .opacity(opacity):
+                    self.opacity = opacity
+            
+            }
+        }
+    }
+}
+
+extension ShadowStyle: Mappable {
+    public func mapping(map: Mapper) {
+        map["color"] = color
+        map["shadowBlur"] = shadowBlur
+        map["shadowColor"] = shadowColor
+        map["shadowOffsetX"] = shadowOffsetX
+        map["shadowOffsetY"] = shadowOffsetY
+        map["opacity"] = opacity
+    }
+}

133a2c9385b5b109367806b27c8367c5e75287e2
Longhua Zhang <kuaileainits@163.com>
2017-08-05 22:38:33 +0800

    test: Add test cases for `RadiusAxis.AxisPointer.Label`

--- SwiftyEchartsTest_iOS/RadiusAxisSpec.swift
+++ SwiftyEchartsTest_iOS/RadiusAxisSpec.swift
@@ -24,5 +24,76 @@ class RadiusAxisSpec: QuickSpec {
                 expect(shadowType.jsonString).to(equal(shadowString.jsonString))
             }
         }
+        
+        let showLabelValue = false
+        let precisionLabelValue: Float = 4.8723
+        let formatterLabelValue = Formatter.string("labelFormatterValue")
+        let marginLabelValue: [Float] = [0.0, 0.284, 237, 573.237]
+        let textStyleLabelValue = TextStyle()
+        let paddingLabelValue = Padding.verticalAndHorizontal(5, 10)
+        let backgroundColorLabelValue = Color.hexColor("#029573")
+        let borderColorLabelValue = Color.red
+        let borderWidthLabelValue: Float = 0.8572
+        let shadowBlurLabelValue: Float = 10
+        let shadowColorLabelValue = rgb(100, 0, 38)
+        let shadowOffsetXLabelValue: Float = 0.8572
+        let shadowOffsetYLabelValue: Float = 8937462.7623467
+        
+        let label = RadiusAxis.AxisPointer.Label()
+        label.show = showLabelValue
+        label.precision = precisionLabelValue
+        label.formatter = formatterLabelValue
+        label.margin = marginLabelValue
+        label.textStyle = textStyleLabelValue
+        label.padding = paddingLabelValue
+        label.backgroundColor = backgroundColorLabelValue
+        label.borderColor = borderColorLabelValue
+        label.borderWidth = borderWidthLabelValue
+        label.shadowBlur = shadowBlurLabelValue
+        label.shadowColor = shadowColorLabelValue
+        label.shadowOffsetX = shadowOffsetXLabelValue
+        label.shadowOffsetY = shadowOffsetYLabelValue
+        
+        describe("For RadiusAxis.AxisPointer.Label") { 
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "show": showLabelValue,
+                    "precision": precisionLabelValue,
+                    "formatter": formatterLabelValue,
+                    "margin": marginLabelValue,
+                    "textStyle": textStyleLabelValue,
+                    "padding": paddingLabelValue,
+                    "backgroundColor": backgroundColorLabelValue,
+                    "borderColor": borderColorLabelValue,
+                    "borderWidth": borderWidthLabelValue,
+                    "shadowBlur": shadowBlurLabelValue,
+                    "shadowColor": shadowColorLabelValue,
+                    "shadowOffsetX": shadowOffsetXLabelValue,
+                    "shadowOffsetY": shadowOffsetYLabelValue
+                ]
+                
+                expect(label.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let labelByEnums = RadiusAxis.AxisPointer.Label(
+                    .show(showLabelValue),
+                    .precision(precisionLabelValue),
+                    .formatter(formatterLabelValue),
+                    .margin(marginLabelValue),
+                    .textStyle(textStyleLabelValue),
+                    .padding(paddingLabelValue),
+                    .backgroundColor(backgroundColorLabelValue),
+                    .borderColor(borderColorLabelValue),
+                    .borderWidth(borderWidthLabelValue),
+                    .shadowBlur(shadowBlurLabelValue),
+                    .shadowColor(shadowColorLabelValue),
+                    .shadowOffsetX(shadowOffsetXLabelValue),
+                    .shadowOffsetY(shadowOffsetYLabelValue)
+                )
+                
+                expect(labelByEnums.jsonString).to(equal(label.jsonString))
+            }
+        }
     }
 }

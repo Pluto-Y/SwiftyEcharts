dcd35ef7d94c6dbda19436212cf558d17dd0890f
Longhua Zhang <kuaileainits@163.com>
2017-08-06 13:23:02 +0800

    test: Add test cases for `RingGraphic`

--- SwiftyEchartsTest_iOS/RingGraphicSpec.swift
+++ SwiftyEchartsTest_iOS/RingGraphicSpec.swift
@@ -46,5 +46,97 @@ class RingGraphicSpec: QuickSpec {
                 expect(shapeByEnums.jsonString).to(equal(shape.jsonString))
             }
         }
+        
+        describe("For RingGraphic") {
+            let typeValue = GraphicType.ring
+            let idValue = "ringGraphicIdValue"
+            let actionValue = GraphicAction.merge
+            let leftValue = Position.insideRight
+            let rightValue = Position.insideLeft
+            let topValue = Position.insideBottom
+            let bottomValue = Position.insideTop
+            let boundingValue = GraphicBounding.raw
+            let zValue: Float = 474.237
+            let zlevelValue: Float = 95.347
+            let silentValue = false
+            let invisibleValue = true
+            let cursorValue = "ringGraphicCursorValue"
+            let draggableValue = false
+            let progressiveValue = true
+            let shapeValue = shape
+            let styleValue = CommonGraphicStyle(
+                .shadowBlur(99.2836),
+                .stroke(Color.array([Color.red, Color.transparent]))
+            )
+            
+            let ringGraphic = RingGraphic()
+            ringGraphic.id = idValue
+            ringGraphic.action = actionValue
+            ringGraphic.left = leftValue
+            ringGraphic.right = rightValue
+            ringGraphic.top = topValue
+            ringGraphic.bottom = bottomValue
+            ringGraphic.bounding = boundingValue
+            ringGraphic.z = zValue
+            ringGraphic.zlevel = zlevelValue
+            ringGraphic.silent = silentValue
+            ringGraphic.invisible = invisibleValue
+            ringGraphic.cursor = cursorValue
+            ringGraphic.draggable = draggableValue
+            ringGraphic.progressive = progressiveValue
+            ringGraphic.shape = shapeValue
+            ringGraphic.style = styleValue
+            
+            it("needs to check the typeValue") {
+                expect(ringGraphic.type.jsonString).to(equal(typeValue.jsonString))
+            }
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "type": typeValue,
+                    "id": idValue,
+                    "$action": actionValue,
+                    "left": leftValue,
+                    "right": rightValue,
+                    "top": topValue,
+                    "bottom": bottomValue,
+                    "bounding": boundingValue,
+                    "z": zValue,
+                    "zlevel": zlevelValue,
+                    "silent": silentValue,
+                    "invisible": invisibleValue,
+                    "cursor": cursorValue,
+                    "draggable": draggableValue,
+                    "progressive": progressiveValue,
+                    "shape": shapeValue,
+                    "style": styleValue
+                ]
+                
+                expect(ringGraphic.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let ringGraphicByEnums = RingGraphic(
+                    .id(idValue),
+                    .action(actionValue),
+                    .left(leftValue),
+                    .right(rightValue),
+                    .top(topValue),
+                    .bottom(bottomValue),
+                    .bounding(boundingValue),
+                    .z(zValue),
+                    .zlevel(zlevelValue),
+                    .silent(silentValue),
+                    .invisible(invisibleValue),
+                    .cursor(cursorValue),
+                    .draggable(draggableValue),
+                    .progressive(progressiveValue),
+                    .shape(shapeValue),
+                    .style(styleValue)
+                )
+                
+                expect(ringGraphicByEnums.jsonString).to(equal(ringGraphic.jsonString))
+            }
+        }
     }
 }

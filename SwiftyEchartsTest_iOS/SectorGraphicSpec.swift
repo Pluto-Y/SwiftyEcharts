5242d89c6e0b8aa5cc2f3fd80bbc586a748aada7
Longhua Zhang <kuaileainits@163.com>
2017-08-08 17:48:44 +0800

    test: Add test cases for `SectorGraphic`

--- SwiftyEchartsTest_iOS/SectorGraphicSpec.swift
+++ SwiftyEchartsTest_iOS/SectorGraphicSpec.swift
@@ -58,5 +58,102 @@ class SectorGraphicSpec: QuickSpec {
                 expect(shapeByEnums.jsonString).to(equal(shape.jsonString))
             }
         }
+        
+        describe("For SectorGraphic") {
+            let typeValue = GraphicType.sector
+            let idValue = "sectorGraphicIdValue"
+            let actionValue = GraphicAction.remove
+            let leftValue = Position.insideTop
+            let topValue = Position.insideLeft
+            let rightValue = Position.insideBottom
+            let bottomValue = Position.insideRight
+            let boundingValue = GraphicBounding.all
+            let zValue: Float = 75.2764
+            let zlevelValue: Float = 0.02746
+            let silentValue = false
+            let invisibleValue = false
+            let cursorValue = "sectorGraphicCursorValue"
+            let draggableValue = true
+            let progressiveValue = true
+            let shapeValue = shape
+            let styleValue = CommonGraphicStyle(
+                .fill(Color.transparent),
+                .stroke(Color.red),
+                .lineWidth(0.000001),
+                .shadowBlur(7.7777777),
+                .shadowOffsetX(8),
+                .shadowOffsetY(0),
+                .shadowColor(Color.green)
+            )
+            
+            let sectorGraphic = SectorGraphic()
+            sectorGraphic.id = idValue
+            sectorGraphic.action = actionValue
+            sectorGraphic.left = leftValue
+            sectorGraphic.right = rightValue
+            sectorGraphic.top = topValue
+            sectorGraphic.bottom = bottomValue
+            sectorGraphic.bounding = boundingValue
+            sectorGraphic.z = zValue
+            sectorGraphic.zlevel = zlevelValue
+            sectorGraphic.silent = silentValue
+            sectorGraphic.invisible = invisibleValue
+            sectorGraphic.cursor = cursorValue
+            sectorGraphic.draggable = draggableValue
+            sectorGraphic.progressive = progressiveValue
+            sectorGraphic.shape = shapeValue
+            sectorGraphic.style = styleValue
+            
+            it("needs to check the type value") {
+                expect(sectorGraphic.type.jsonString).to(equal(typeValue.jsonString))
+            }
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "type": typeValue,
+                    "id": idValue,
+                    "$action": actionValue,
+                    "left": leftValue,
+                    "right": rightValue,
+                    "top": topValue,
+                    "bottom": bottomValue,
+                    "bounding": boundingValue,
+                    "z": zValue,
+                    "zlevel": zlevelValue,
+                    "silent": silentValue,
+                    "invisible": invisibleValue,
+                    "cursor": cursorValue,
+                    "draggable": draggableValue,
+                    "progressive": progressiveValue,
+                    "shape": shapeValue,
+                    "style": styleValue
+                ]
+                
+                expect(sectorGraphic.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let sectorGraphicByEnums = SectorGraphic(
+                    .id(idValue),
+                    .action(actionValue),
+                    .left(leftValue),
+                    .right(rightValue),
+                    .top(topValue),
+                    .bottom(bottomValue),
+                    .bounding(boundingValue),
+                    .z(zValue),
+                    .zlevel(zlevelValue),
+                    .silent(silentValue),
+                    .invisible(invisibleValue),
+                    .cursor(cursorValue),
+                    .draggable(draggableValue),
+                    .progressive(progressiveValue),
+                    .shape(shapeValue),
+                    .style(styleValue)
+                )
+                
+                expect(sectorGraphicByEnums.jsonString).to(equal(sectorGraphic.jsonString))
+            }
+        }
     }
 }

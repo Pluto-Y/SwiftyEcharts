1efb91972d7a2a6a0d377e1341778fc2305c82e4
Longhua Zhang <kuaileainits@163.com>
2017-08-11 21:05:53 +0800

    test: Add test cases for `Timeline.CheckpointStyle`

---
+++ SwiftyEchartsTest_iOS/TimelineSpec.swift
@@ -0,0 +1,75 @@
+//
+//  TimelineSpec.swift
+//  SwiftyEcharts
+//
+//  Created by Pluto Y on 11/08/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import SwiftyEcharts
+
+class TimelineSpec: QuickSpec {
+    override func spec() {
+        let symbolCheckpointStyleOriginValue = Symbol.arrow
+        let symbolCheckpointStyleValue = OneOrMore(one: symbolCheckpointStyleOriginValue)
+        let symbolSizeCheckpointStyleValue = FunctionOrFloatOrPair.point([20, 20%])
+        let symbolRotateCheckpointStyleValue: Float = 56.7
+        let symbolOffsetCheckpointStyleValue: Point = [0, 100%]
+        let colorCheckpointStyleValue = rgb(67, 89 , 10)
+        let borderWidthCheckpointStyleValue: Float = 0.5827
+        let borderColorCheckpointStyleValue = rgba(75, 24, 27, 0.577)
+        let animationCheckpointStyleValue = false
+        let animationDurationCheckpointStyleValue: Float = 95.3874
+        let animationEasingCheckpointStyleValue = EasingFunction.backIn
+        
+        let checkpointStyle = Timeline.CheckpointStyle()
+        checkpointStyle.symbol = symbolCheckpointStyleValue
+        checkpointStyle.symbolSize = symbolSizeCheckpointStyleValue
+        checkpointStyle.symbolRotate = symbolRotateCheckpointStyleValue
+        checkpointStyle.symbolOffset = symbolOffsetCheckpointStyleValue
+        checkpointStyle.color = colorCheckpointStyleValue
+        checkpointStyle.borderColor = borderColorCheckpointStyleValue
+        checkpointStyle.borderWidth = borderWidthCheckpointStyleValue
+        checkpointStyle.animation = animationCheckpointStyleValue
+        checkpointStyle.animationDuration = animationDurationCheckpointStyleValue
+        checkpointStyle.animationEasing = animationEasingCheckpointStyleValue
+        
+        describe("For Timeline.CheckpointStyle") {
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "symbol": symbolCheckpointStyleValue,
+                    "symbolSize": symbolSizeCheckpointStyleValue,
+                    "symbolRotate": symbolRotateCheckpointStyleValue,
+                    "symbolOffset": symbolOffsetCheckpointStyleValue,
+                    "color": colorCheckpointStyleValue,
+                    "borderColor": borderColorCheckpointStyleValue,
+                    "borderWidth": borderWidthCheckpointStyleValue,
+                    "animation": animationCheckpointStyleValue,
+                    "animationDuration": animationDurationCheckpointStyleValue,
+                    "animationEasing": animationEasingCheckpointStyleValue
+                ]
+                
+                expect(checkpointStyle.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let checkpointStyleByEnums = Timeline.CheckpointStyle(
+                    .symbol(symbolCheckpointStyleOriginValue),
+                    .symbolSize(symbolSizeCheckpointStyleValue),
+                    .symbolRotate(symbolRotateCheckpointStyleValue),
+                    .symbolOffset(symbolOffsetCheckpointStyleValue),
+                    .color(colorCheckpointStyleValue),
+                    .borderColor(borderColorCheckpointStyleValue),
+                    .borderWidth(borderWidthCheckpointStyleValue),
+                    .animation(animationCheckpointStyleValue),
+                    .animationDuration(animationDurationCheckpointStyleValue),
+                    .animationEasing(animationEasingCheckpointStyleValue)
+                )
+                
+                expect(checkpointStyleByEnums.jsonString).to(equal(checkpointStyle.jsonString))
+            }
+        }
+    }
+}

b618d7e86b99341caaeefbe7994309d409442ff8
Longhua Zhang <kuaileainits@163.com>
2017-08-13 02:30:01 +0800

    test: Add test cases for `Toolbox.IconStyleContent`

--- SwiftyEchartsTest_iOS/ToolboxSpec.swift
+++ SwiftyEchartsTest_iOS/ToolboxSpec.swift
@@ -24,5 +24,68 @@ class ToolboxSpec: QuickSpec {
                 expect(jpegType.jsonString).to(equal(jpegString.jsonString))
             }
         }
+        
+        let colorIconStyleContentValue = Color.rgba(128, 128, 128, 0.3)
+        let borderColorIconStyleContentValue = Color.hexColor("#abcdef")
+        let borderWidthIconStyleContentValue: Float = 8.427
+        let borderTypeIconStyleContentValue = LineType.solid
+        let shadowBlurIconStyleContentValue: Float = 87234.23872
+        let shadowColorIconStyleContentValue = Color.array([Color.red, Color.hexColor("#2746ff"), Color.rgba(0, 0, 128, 0.77)])
+        let shadowOffsetXIconStyleContentValue: Float = 85.34874
+        let shadowOffsetYIconStyleContentValue: Float = 0.88873
+        let opacityIconStyleContentValue: Float = 0.74623
+        let textPositionIconStyleContentValue = Position.top
+        let textAlignIconStyleContentValue = Align.right
+        
+        let iconStyleContent = Toolbox.IconStyleContent()
+        iconStyleContent.color = colorIconStyleContentValue
+        iconStyleContent.borderColor = borderColorIconStyleContentValue
+        iconStyleContent.borderWidth = borderWidthIconStyleContentValue
+        iconStyleContent.borderType = borderTypeIconStyleContentValue
+        iconStyleContent.shadowBlur = shadowBlurIconStyleContentValue
+        iconStyleContent.shadowColor = shadowColorIconStyleContentValue
+        iconStyleContent.shadowOffsetX = shadowOffsetXIconStyleContentValue
+        iconStyleContent.shadowOffsetY = shadowOffsetYIconStyleContentValue
+        iconStyleContent.opacity = opacityIconStyleContentValue
+        iconStyleContent.textPosition = textPositionIconStyleContentValue
+        iconStyleContent.textAlign = textAlignIconStyleContentValue
+        
+        describe("For Toolbox.IconStyleContent") {
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "color": colorIconStyleContentValue,
+                    "borderColor": borderColorIconStyleContentValue,
+                    "borderWidth": borderWidthIconStyleContentValue,
+                    "borderType": borderTypeIconStyleContentValue,
+                    "shadowBlur": shadowBlurIconStyleContentValue,
+                    "shadowColor": shadowColorIconStyleContentValue,
+                    "shadowOffsetX": shadowOffsetXIconStyleContentValue,
+                    "shadowOffsetY": shadowOffsetYIconStyleContentValue,
+                    "opacity": opacityIconStyleContentValue,
+                    "textPosition": textPositionIconStyleContentValue,
+                    "textAlign": textAlignIconStyleContentValue
+                ]
+                
+                expect(iconStyleContent.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let iconStyleContentByEnums = Toolbox.IconStyleContent(
+                    .color(colorIconStyleContentValue),
+                    .borderColor(borderColorIconStyleContentValue),
+                    .borderWidth(borderWidthIconStyleContentValue),
+                    .borderType(borderTypeIconStyleContentValue),
+                    .shadowBlur(shadowBlurIconStyleContentValue),
+                    .shadowColor(shadowColorIconStyleContentValue),
+                    .shadowOffsetX(shadowOffsetXIconStyleContentValue),
+                    .shadowOffsetY(shadowOffsetYIconStyleContentValue),
+                    .opacity(opacityIconStyleContentValue),
+                    .textPosition(textPositionIconStyleContentValue),
+                    .textAlign(textAlignIconStyleContentValue)
+                )
+                
+                expect(iconStyleContentByEnums.jsonString).to(equal(iconStyleContentByEnums.jsonString))
+            }
+        }
     }
 }

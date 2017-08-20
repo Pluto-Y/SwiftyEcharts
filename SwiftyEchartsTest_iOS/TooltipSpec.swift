19f109fcde3f14be8b81b1668ef3d15e66557cb3
Longhua Zhang <kuaileainits@163.com>
2017-08-20 19:30:33 +0800

    test: Add test cases for `Tooltip`

--- SwiftyEchartsTest_iOS/TooltipSpec.swift
+++ SwiftyEchartsTest_iOS/TooltipSpec.swift
@@ -215,5 +215,100 @@ class TooltipSpec: QuickSpec {
                 expect(axisPointerByEnums.jsonString).to(equal(axisPointer.jsonString))
             }
         }
+        
+        describe("For Tooltip") { 
+            let showValue = true
+            let showContentValue = true
+            let triggerValue = Tooltip.Trigger.axis
+            let triggerOnValue = Tooltip.Trigger.TriggerOn.none
+            let alwaysShowContentValue = false
+            let showDelayValue: Float = 85.7346
+            let hideDelayValue: Float = 0.75723
+            let positionValue = Position.inside
+            let confineValue = false
+            let transitionDurationValue: Float = 5555
+            let formatterValue = Formatter.string("tooltipFormatterValue")
+            let backgroundColorValue = Color.hexColor("#fff000")
+            let borderColorValue = Color.rgb(0, 0, 255)
+            let borderWidthValue: Float = 8573.273
+            let paddingValue = Padding.all(88)
+            let textStyleValue = TextStyle(
+                .color(Color.rgb(255, 255, 255)),
+                .fontWeight(FontWeight.bold),
+                .fontSize(16)
+            )
+            let extraCssTextValue = "box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);"
+            let axisPointerValue = axisPointer
+            
+            let tooltip = Tooltip()
+            tooltip.show = showValue
+            tooltip.showContent = showContentValue
+            tooltip.trigger = triggerValue
+            tooltip.triggerOn = triggerOnValue
+            tooltip.alwaysShowContent = alwaysShowContentValue
+            tooltip.showDelay = showDelayValue
+            tooltip.hideDelay = hideDelayValue
+            tooltip.position = positionValue
+            tooltip.confine = confineValue
+            tooltip.transitionDuration = transitionDurationValue
+            tooltip.formatter = formatterValue
+            tooltip.backgroundColor = backgroundColorValue
+            tooltip.borderColor = borderColorValue
+            tooltip.borderWidth = borderWidthValue
+            tooltip.padding = paddingValue
+            tooltip.textStyle = textStyleValue
+            tooltip.extraCssText = extraCssTextValue
+            tooltip.axisPointer = axisPointerValue
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "show": showValue,
+                    "showContent": showContentValue,
+                    "trigger": triggerValue,
+                    "triggerOn": triggerOnValue,
+                    "alwaysShowContent": alwaysShowContentValue,
+                    "showDelay": showDelayValue,
+                    "hideDelay": hideDelayValue,
+                    "position": positionValue,
+                    "confine": confineValue,
+                    "transitionDuration": transitionDurationValue,
+                    "formatter": formatterValue,
+                    "backgroundColor": backgroundColorValue,
+                    "borderColor": borderColorValue,
+                    "borderWidth": borderWidthValue,
+                    "padding": paddingValue,
+                    "textStyle": textStyleValue,
+                    "extraCssText": extraCssTextValue,
+                    "axisPointer": axisPointerValue
+                ]
+                
+                expect(tooltip.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let tooltipByEnums = Tooltip(
+                    .show(showValue),
+                    .showContent(showContentValue),
+                    .trigger(triggerValue),
+                    .triggerOn(triggerOnValue),
+                    .alwaysShowContent(alwaysShowContentValue),
+                    .showDelay(showDelayValue),
+                    .hideDelay(hideDelayValue),
+                    .position(positionValue),
+                    .confine(confineValue),
+                    .transitionDuration(transitionDurationValue),
+                    .formatter(formatterValue),
+                    .backgroundColor(backgroundColorValue),
+                    .borderColor(borderColorValue),
+                    .borderWidth(borderWidthValue),
+                    .padding(paddingValue),
+                    .textStyle(textStyleValue),
+                    .extraCssText(extraCssTextValue),
+                    .axisPointer(axisPointerValue)
+                )
+                
+                expect(tooltipByEnums.jsonString).to(equal(tooltip.jsonString))
+            }
+        }
     }
 }

3138abc6d167f54506a3425a2a807390f67bb940
Longhua Zhang <kuaileainits@163.com>
2017-08-20 19:44:22 +0800

    test: Add test cases for `TreemapSerie.ColorMappingBy`

--- SwiftyEchartsTest_iOS/TreemapSerieSpec.swift
+++ SwiftyEchartsTest_iOS/TreemapSerieSpec.swift
@@ -27,5 +27,21 @@ class TreemapSerieSpec: QuickSpec {
                 expect(linkNodeClick.jsonString).to(equal(linkString.jsonString))
             }
         }
+        
+        describe("For TreemapSerie.ColorMappingBy") {
+            let valueString = "value"
+            let indexString = "index"
+            let idString = "id"
+            
+            let valueColorMappingBy = TreemapSerie.ColorMappingBy.value
+            let indexColorMappingBy = TreemapSerie.ColorMappingBy.index
+            let idColorMappingBy = TreemapSerie.ColorMappingBy.id
+            
+            it("needs to check the enum case jsonString") {
+                expect(valueColorMappingBy.jsonString).to(equal(valueString.jsonString))
+                expect(indexColorMappingBy.jsonString).to(equal(indexString.jsonString))
+                expect(idColorMappingBy.jsonString).to(equal(idString.jsonString))
+            }
+        }
     }
 }

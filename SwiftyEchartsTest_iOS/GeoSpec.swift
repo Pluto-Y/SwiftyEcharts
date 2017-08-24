828b25c0c365c5cded09fb8aad23e2009193ded9
Longhua Zhang <kuaileainits@163.com>
2017-08-24 23:54:16 +0800

    test: Update test cases for `Geo.ScaleLimit`

--- SwiftyEchartsTest_iOS/GeoSpec.swift
+++ SwiftyEchartsTest_iOS/GeoSpec.swift
@@ -8,38 +8,35 @@
 
 import Quick
 import Nimble
-import SwiftyEcharts
+@testable import SwiftyEcharts
 
 class GeoSpec: QuickSpec {
     override func spec() {
+        let minValue = Float(arc4random_uniform(100)) / 100.0
+        let maxValue = Float(arc4random_uniform(100)) / 100.0 + 20.0
+        let scaleLimitNormal = Geo.ScaleLimit()
+        scaleLimitNormal.min = minValue
+        scaleLimitNormal.max = maxValue
+        
         describe("For Geo.ScaleLimit") {
-            it(" needs to check the Enumable and Mappable") {
-                let minValue = Float(arc4random_uniform(100)) / 100.0
-                let maxValue = Float(arc4random_uniform(100)) / 100.0 + 20.0
-                let min = Geo.ScaleLimit.Enums.min(minValue)
-                let max = Geo.ScaleLimit.Enums.max(maxValue)
+            it("needs to chekc the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "min": minValue,
+                    "max": maxValue
+                ]
                 
-                let scaleLimitByEnums = Geo.ScaleLimit(min, max)
-                let scaleLimitNormal = Geo.ScaleLimit()
-                scaleLimitNormal.min = minValue
-                scaleLimitNormal.max = maxValue
-                
-                expect(scaleLimitByEnums.min).to(equal(scaleLimitNormal.min))
-                expect(scaleLimitByEnums.max).to(equal(scaleLimitNormal.max))
+                expect(scaleLimitNormal.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            
+            it("needs to chekc the Enumable") {
+                let scaleLimitByEnums = Geo.ScaleLimit(
+                    .min(minValue),
+                    .max(maxValue)
+                )
                 
                 expect(scaleLimitByEnums.jsonString).to(equal(scaleLimitNormal.jsonString))
-                // 检查是否正确的 jsonString
-                expect(scaleLimitByEnums.jsonString).to(equal(["max": maxValue, "min":  minValue].jsonString))
             }
         }
-
-        // FIXME: 先写一些小的类
-//        describe("For Geo.Region") {
-//            it(" needs to check the Enumable and Mappable") {
-//                let nameValue = "浙江"
-//                let selectedValue = arc4random_uniform(100) % 2 == 0
-//                let 
-//            }
-//        }
     }
 }

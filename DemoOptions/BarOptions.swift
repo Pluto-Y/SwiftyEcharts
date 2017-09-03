6ae2e1479082d830f215ba2b064155f143838b92
Longhua Zhang <kuaileainits@163.com>
2017-09-03 12:23:11 +0800

    feature(demo): Add new demo for `bar-polar-real-estate`

--- DemoOptions/BarOptions.swift
+++ DemoOptions/BarOptions.swift
@@ -1005,8 +1005,97 @@ public final class BarOptions {
     // MARK: 在中国租个房子有多贵
     /// 地址: http://echarts.baidu.com/demo.html#bar-polar-real-estate
     static func barPolarRealEstateOption() -> Option {
-        // TODO: 添加实现
+        let data: [[Float]] = [
+            [5000, 10000, 6785.71],
+            [4000, 10000, 6825],
+            [3000, 6500, 4463.33],
+            [2500, 5600, 3793.83],
+            [2000, 4000, 3060],
+            [2000, 4000, 3222.33],
+            [2500, 4000, 3133.33],
+            [1800, 4000, 3100],
+            [2000, 3500, 2750],
+            [2000, 3000, 2500],
+            [1800, 3000, 2433.33],
+            [2000, 2700, 2375],
+            [1500, 2800, 2150],
+            [1500, 2300, 2100],
+            [1600, 3500, 2057.14],
+            [1500, 2600, 2037.5],
+            [1500, 2417.54, 1905.85],
+            [1500, 2000, 1775],
+            [1500, 1800, 1650]
+        ]
+        
+        let cities: [Jsonable] = ["北京", "上海", "深圳", "广州", "苏州", "杭州", "南京", "福州", "青岛", "济南", "长春", "大连", "温州", "郑州", "武汉", "成都", "东莞", "沈阳", "烟台"]
+        
+        let barHeight: Float = 50
+        
         return Option(
+            .title(Title(
+                .text("在中国租个房子有多贵?"),
+                .subtext("市中心一室月租费（数据来源：https://www.numbeo.com）")
+                )),
+            .legend(Legend(
+                .show(true),
+                .data(["价格范围", "均值"])
+                )),
+            .grid(Grid(
+                .top(Position.value(100))
+                )),
+            .angleAxis(AngleAxis(
+                .type(.category),
+                .data(cities)
+                )),
+            .tooltip(Tooltip(
+                .show(true),
+                .formatter(.string("function (params) { var id = params.dataIndex; return cities[id] + '<br>最低：' + data[id][0] + '<br>最高：' + data[id][1] + '<br>平均：' + data[id][2]; }"))
+                )),
+            .radiusAxis(RadiusAxis()),
+            .polar(Polar()),
+            .series([
+                BarSerie(
+                    .itemStyle(ItemStyle(
+                        .normal(CommonItemStyleContent(
+                            .color(Color.transparent)
+                            ))
+                        )),
+                    .data(data.map{ $0[0] }),
+                    .coordinateSystem(.polar),
+                    .stack("最大最小值"),
+                    .silent(true)
+                ),
+                BarSerie(
+                    .data(data.map { $0[1] - $0[0] }),
+                    .coordinateSystem(.polar),
+                    .name("价格范围"),
+                    .stack("最大最小值")
+                ),
+                BarSerie(
+                    .itemStyle(ItemStyle(
+                        .normal(CommonItemStyleContent(
+                            .color(Color.transparent)
+                            ))
+                        )),
+                    .data(data.map { $0[2] - barHeight }),
+                    .coordinateSystem(.polar),
+                    .stack("均值"),
+                    .silent(true),
+                    .z(10)
+                ),
+                BarSerie(
+                    .data(data.map { _ in barHeight * 2 }),
+                    .coordinateSystem(.polar),
+                    .name("均值"),
+                    .stack("均值"),
+                    .barGap((-100)%),
+                    .z(10)
+                )
+                ]),
+            .legend(Legend(
+                .show(true),
+                .data(["A", "B", "C"])
+                ))
         )
     }
     

bf1cde538802035704550e3bc07adbf1a2805402
Longhua Zhang <kuaileainits@163.com>
2017-09-10 20:45:56 +0800

    feature(util):Update implements for `Echarts` and add new internal class `DataTool`

--- SwiftyEcharts/Tools/Echarts.swift
+++ SwiftyEcharts/Tools/Echarts.swift
@@ -9,10 +9,20 @@
 import JavaScriptCore
 
 public final class Echarts {
-    public static let frameworkBundle = NSBundle(path: NSBundle.mainBundle().pathForResource("SwiftyEcharts", ofType: "framework", inDirectory: "Frameworks")!)!
+    public static let frameworkBundle: NSBundle? = {
+        if let bundlePath = NSBundle.mainBundle().pathForResource("SwiftyEcharts", ofType: "framework", inDirectory: "Frameworks") {
+            return NSBundle(path: bundlePath)!
+        }
+        for bundle in NSBundle.allBundles() {
+            if let bundlePath = bundle.pathForResource("SwiftyEcharts", ofType: "framework") {
+                return NSBundle(path: bundlePath)!
+            }
+        }
+        return nil
+    }() 
     
     public static func loadEcharts(context: JSContext) {
-        guard let echartScriptPath = frameworkBundle.pathForResource("echarts.min", ofType: "js", inDirectory: "js"), let echartsScripts = try? String(contentsOfFile: echartScriptPath) else {
+        guard let echartScriptPath = frameworkBundle?.pathForResource("echarts.min", ofType: "js", inDirectory: "js"), let echartsScripts = try? String(contentsOfFile: echartScriptPath) else {
             return
         }
         
@@ -22,44 +32,46 @@ public final class Echarts {
 
 // MARK: DataTool
 extension Echarts {
-    public static func prepareBoxplotData(datas: [[Float]]) -> [String: [Jsonable]] {
-        guard let scriptPath = frameworkBundle.pathForResource("dataTool", ofType: "js", inDirectory: "js"), let scripts = try? String(contentsOfFile: scriptPath) else {
-            return [:]
-        }
-        
-        guard let context = JSContext(virtualMachine: JSVirtualMachine()) else {
-            return [:]
-        }
-
-        self.loadEcharts(context)
-        context.evaluateScript(scripts)
-        
-        let scriptResult = context.evaluateScript("echarts.dataTool.prepareBoxplotData(\(datas))")
-        let scriptResultDic = scriptResult?.toDictionary() as! [String: NSArray]
-        var result: [String: [Jsonable]] = [:]
-        var outliers: [[Jsonable]] = []
-        for outer in scriptResultDic["outliers"]! {
-            var innerArr: [Jsonable] = []
-            for inner in outer as! NSArray {
-                innerArr.append(inner as! Jsonable)
+    public class DataTool {
+        public static func prepareBoxplotData(datas: [[Float]]) -> [String: [Jsonable]] {
+            guard let scriptPath = frameworkBundle?.pathForResource("dataTool", ofType: "js", inDirectory: "js"), let scripts = try? String(contentsOfFile: scriptPath) else {
+                return [:]
             }
-            outliers.append(innerArr)
-        }
-        result["outliers"] = outliers.map { $0 as Jsonable }
-        var axisDatas: [String] = []
-        for axisData in scriptResultDic["axisData"]! {
-            axisDatas.append(axisData as! String)
-        }
-        result["axisData"]  = axisDatas.map { $0 as Jsonable }
-        var boxDatas: [[Jsonable]] = []
-        for outer in scriptResultDic["boxData"]! {
-            var innerArr: [Jsonable] = []
-            for inner in outer as! NSArray {
-                innerArr.append(inner as! Jsonable)
+            
+            guard let context = JSContext(virtualMachine: JSVirtualMachine()) else {
+                return [:]
             }
-            boxDatas.append(innerArr)
+            
+            Echarts.loadEcharts(context)
+            context.evaluateScript(scripts)
+            
+            let scriptResult = context.evaluateScript("echarts.dataTool.prepareBoxplotData(\(datas))")
+            let scriptResultDic = scriptResult?.toDictionary() as! [String: NSArray]
+            var result: [String: [Jsonable]] = [:]
+            var outliers: [[Jsonable]] = []
+            for outer in scriptResultDic["outliers"]! {
+                var innerArr: [Jsonable] = []
+                for inner in outer as! NSArray {
+                    innerArr.append(inner as! Jsonable)
+                }
+                outliers.append(innerArr)
+            }
+            result["outliers"] = outliers.map { $0 as Jsonable }
+            var axisDatas: [String] = []
+            for axisData in scriptResultDic["axisData"]! {
+                axisDatas.append(axisData as! String)
+            }
+            result["axisData"]  = axisDatas.map { $0 as Jsonable }
+            var boxDatas: [[Jsonable]] = []
+            for outer in scriptResultDic["boxData"]! {
+                var innerArr: [Jsonable] = []
+                for inner in outer as! NSArray {
+                    innerArr.append(inner as! Jsonable)
+                }
+                boxDatas.append(innerArr)
+            }
+            result["boxData"]  = boxDatas.map { $0 as Jsonable }
+            return result
         }
-        result["boxData"]  = boxDatas.map { $0 as Jsonable }
-        return result
     }
 }

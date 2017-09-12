7cbc284da6abfc8d6a363f8a02e9fcde234d50f7
Longhua Zhang <kuaileainits@163.com>
2017-09-12 20:00:51 +0800

    fix(demo): Update implements for `BoxplotOptions`

--- DemoOptions/BoxplotOptions.swift
+++ DemoOptions/BoxplotOptions.swift
@@ -13,7 +13,7 @@ public final class BoxplotOptions {
     // MARK: Boxplot Light Velocity
     /// 地址: http://echarts.baidu.com/demo.html#boxplot-light-velocity
     static func boxplotLightVelocityOption() -> Option {
-        let data: [String: [Jsonable]] = Echarts.prepareBoxplotData([
+        let data: [String: [Jsonable]] = Echarts.DataTool.prepareBoxplotData([
             [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
             [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
             [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
@@ -90,11 +90,17 @@ public final class BoxplotOptions {
     // MARK: Boxplot Light Velocity2
     /// 地址: http://echarts.baidu.com/demo.html#boxplot-light-velocity2
     static func boxplotLightVelocity2Option() -> Option {
-        let data: [String: [Jsonable]] = [
-            "boxData": [[655, 850, 940, 980, 1175], [672.5, 800, 845, 885, 1012.5], [780, 840, 855, 880, 940], [621.25, 767.5, 815, 865, 1011.25], [713.75, 807.5, 810, 870, 963.75]],
-            "outliers": [[650, 0], [620, 2], [720, 2], [720, 2], [950, 2], [970, 2]],
-            "axisData": ["0", "1", "2", "3", "4"]
+        let originData: [[Float]] = [
+            [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
+            [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
+            [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
+            [890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850, 780],
+            [890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810, 870]
         ]
+        let option: [String: String] = [
+            "layout": "vertical"
+        ]
+        let data: [String: [Jsonable]] = Echarts.DataTool.prepareBoxplotData(originData, option)
         return Option(
             .titles([
                 Title(

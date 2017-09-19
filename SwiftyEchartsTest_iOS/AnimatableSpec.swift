fbe026453f3b52dfd9043ccc8303ed5525df23a4
Longhua Zhang <kuaileainits@163.com>
2017-09-20 00:44:14 +0800

    fix: Update the implements and test cases for `Animatable`

--- SwiftyEchartsTest_iOS/AnimatableSpec.swift
+++ SwiftyEchartsTest_iOS/AnimatableSpec.swift
@@ -14,27 +14,28 @@ import Nimble
 class AnimatableSpec: QuickSpec {
     override func spec() {
         describe("For type enum Time") {
+            beforeEach {
+                JsCache.removeAll()
+            }
             
             it(" needs to check the jsonString for Time ") {
+                let function: Function = "function (idx) {return idx * 0.65;}"
                 let numberTime = Time.number(3.14)
-                let functionTime = Time.function("function (idx) {return idx * 0.65;}")
+                let functionTime = Time.function(function)
                 
                 expect(numberTime.jsonString).to(equal("3.14"))
-                JsCache.removeAll()
-                expect(functionTime.jsonString).to(equal("\"\(TimeFuncPrefix)0\""))
-                expect(JsCache.allJsStrings()[0]).to(equal("var \(TimeFuncPrefix)0 = function (idx) {return idx * 0.65;};"))
+                expect(functionTime.jsonString).to(equal(function.jsonString))
             }
             
             it(" needs to check the convertiable for Time ") {
+                let function: Function = "function (idx) {return idx * 100;}"
                 let floatTime: Time = 100.0
                 let intTime: Time = 85
-                let functionTime: Time = "function (idx) {return idx * 100;}"
+                let functionTime: Time = Time.function(function)
                 
                 expect(floatTime.jsonString).to(equal(Time.number(100.0).jsonString))
                 expect(intTime.jsonString).to(equal(Time.number(85).jsonString))
-                JsCache.removeAll()
-                expect(functionTime.jsonString).to(equal("\"\(TimeFuncPrefix)0\""))
-                expect(JsCache.allJsStrings()[0]).to(equal("var \(TimeFuncPrefix)0 = function (idx) {return idx * 100;};"))
+                expect(functionTime.jsonString).to(equal(function.jsonString))
             }
         }
         

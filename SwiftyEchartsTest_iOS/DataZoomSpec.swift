fde859cd6bc09c3f74ffe86be4d870bb013b4fa2
Longhua Zhang <kuaileainits@163.com>
2017-10-08 19:44:08 +0800

    test: Add test cases for `DataZoomAction`

--- SwiftyEchartsTest_iOS/DataZoomSpec.swift
+++ SwiftyEchartsTest_iOS/DataZoomSpec.swift
@@ -25,5 +25,50 @@ class DataZoomSpec: QuickSpec {
                 expect(emptyFilterMode.jsonString).to(equal(emptyString.jsonString))
             }
         }
+        
+        describe("For DataZoomAction") {
+            let typeValue = EchartsActionType.dataZoom
+            let dataZoomIndexValue = 23487348972398
+            let startValue: Float = 0.000000
+            let endValue: Float = 99.999999
+            let startValueValue: Float = 92372.23894723894
+            let endValueValue: Float = 2343.02394823
+            
+            let dataZoomAction = DataZoomAction()
+            dataZoomAction.dataZoomIndex = dataZoomIndexValue
+            dataZoomAction.start = startValue
+            dataZoomAction.end = endValue
+            dataZoomAction.startValue = startValueValue
+            dataZoomAction.endValue = endValueValue
+            
+            it("needs to check the type value") {
+                expect(dataZoomAction.type.jsonString).to(equal(typeValue.jsonString))
+            }
+            
+            it("needs to check the jsonString") {
+                let resultDic: [String: Jsonable] = [
+                    "type": typeValue,
+                    "dataZoomIndex": dataZoomIndexValue,
+                    "start": startValue,
+                    "end": endValue,
+                    "startValue": startValueValue,
+                    "endValue": endValueValue
+                ]
+                
+                expect(dataZoomAction.jsonString).to(equal(resultDic.jsonString))
+            }
+            
+            it("needs to check the Enumable") {
+                let dataZoomActionByEnums = DataZoomAction(
+                    .dataZoomIndex(dataZoomIndexValue),
+                    .start(startValue),
+                    .end(endValue),
+                    .startValue(startValueValue),
+                    .endValue(endValueValue)
+                )
+                
+                expect(dataZoomActionByEnums.jsonString).to(equal(dataZoomAction.jsonString))
+            }
+        }
     }
 }

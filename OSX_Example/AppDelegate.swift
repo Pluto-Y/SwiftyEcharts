8e28effef510ec2abac541623d4554327d9d6a2b
Longhua Zhang <kuaileainits@163.com>
2017-11-02 14:54:53 +0800

    feature(osx): Add scheme for the supporting of `OSX`

---
+++ OSX_Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSX_Example
+//
+//  Created by Pluto Y on 02/11/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ OSX_Example/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  OSX_Example
+//
+//  Created by Pluto Y on 02/11/2017.
+//  Copyright © 2017 com.pluto-y. All rights reserved.
+//
+
+import Cocoa
+import SwiftyEcharts
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

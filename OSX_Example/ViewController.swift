dee6db86a63f852047e39404a9d80fb2449c9cf1
Longhua Zhang <kuaileainits@163.com>
2017-11-04 01:38:59 +0800

    Fix: Fix the bug in the `OS X` and `iOS 8`

--- OSX_Example/ViewController.swift
+++ OSX_Example/ViewController.swift
@@ -9,12 +9,49 @@
 import Cocoa
 import SwiftyEcharts
 
-class ViewController: NSViewController {
+class EchartsViewController: NSViewController {
 
+    private var echartsView: EchartsView?
     override func viewDidLoad() {
+        print("viewDidLoad")
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        echartsView = EchartsView(frame: CGRect(x: 0, y: 30, width: 300, height: 300))
+        self.view.addSubview(echartsView!)
+        let leadingConstraint = NSLayoutConstraint(item: echartsView!,
+                                                   attribute: .leading,
+                                                   relatedBy: .equal,
+                                                   toItem: self.view,
+                                                   attribute: .leading,
+                                                   multiplier: 1.0,
+                                                   constant: 0)
+        let trailingConstraint = NSLayoutConstraint(item: echartsView!,
+                                                    attribute: .trailing,
+                                                    relatedBy: .equal,
+                                                    toItem: self.view,
+                                                    attribute: .trailing,
+                                                    multiplier: 1.0,
+                                                    constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: echartsView!,
+                                                  attribute: .bottom,
+                                                  relatedBy: .equal,
+                                                  toItem: self.view,
+                                                  attribute: .bottom,
+                                                  multiplier: 1.0,
+                                                  constant: 0)
+        let topConstraint = NSLayoutConstraint(item: echartsView!,
+                                               attribute: .top,
+                                               relatedBy: .equal,
+                                               toItem: self.view,
+                                               attribute: .top,
+                                               multiplier: 1.0,
+                                               constant: 30)
+//        self.view.addConstraints([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
+        print("loadEcharts")
+        let option = LineOptions.areaStackOption()
+        echartsView?.option = option
+        echartsView?.loadEcharts()
+        print("loadEcharts finished")
     }
 
     override var representedObject: Any? {
--- SwiftyEcharts/Views/EchartsView.swift
+++ SwiftyEcharts/Views/EchartsView.swift
@@ -70,17 +70,20 @@ open class EchartsView: WKWebView, WKNavigationDelegate, WKUIDelegate, WKScriptM
     /// 重置图表
     public func reset() {
         loadFinsih = false
-        loadHTMLString(htmlContents, baseURL: URL(fileURLWithPath: bundlePath))
+        loadHTMLString(htmlContents, baseURL: URL(fileURLWithPath: (bundlePath as NSString).deletingLastPathComponent))
     }
     
     open func loadEcharts() {
-//        if !loadFinsih && !self.resizeDivFinish { // 如果还没页面加载完则等待 0.05 秒后刷新
-//            dispatch_after(UInt64(Double(NSEC_PER_SEC) * 0.05), dispatch_get_main_queue(), { [unowned self] in 
-//                self.loadEcharts()
-//            })
-//            return
-//        }
-        loadHTMLString(htmlContents, baseURL: URL(fileURLWithPath: bundlePath))
+        #if os(iOS)
+            if #available(iOS 9.0, *) {
+                loadHTMLString(htmlContents, baseURL: URL(fileURLWithPath: bundlePath))
+            } else if #available(iOS 8.0, *) {
+                loadEchartsForBugllyOfWKEchartsView()
+            }
+        #elseif os(OSX)
+            loadEchartsForBugllyOfWKEchartsView()
+        #endif
+        
     }
     
     open func loadEcharts(with option: Option) {
@@ -150,6 +153,8 @@ open class EchartsView: WKWebView, WKNavigationDelegate, WKUIDelegate, WKScriptM
             return
         }
         
+        
+        
         guard let htmlStr = try? String(contentsOfFile: bundlePath) else {
             return
         }
@@ -172,6 +177,11 @@ open class EchartsView: WKWebView, WKNavigationDelegate, WKUIDelegate, WKScriptM
             userContentController.addUserScript(newScript)
         }
         
+//        if let jsPath = bundle.path(forResource: "echarts.min", ofType: "js", inDirectory: "js"), let js = try? String(contentsOfFile: jsPath) {
+//            let newScript = WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+//            userContentController.addUserScript(newScript)
+//        }
+        
         userContentController.add(self, name: "wkechartview")
 #if os(OSX)
 #elseif os(iOS)
@@ -182,6 +192,59 @@ open class EchartsView: WKWebView, WKNavigationDelegate, WKUIDelegate, WKScriptM
         navigationDelegate = self
         reset()
         
+    }
+    
+    /// 在 iOS8 以及在 OS X 的加载 Echarts 的方法
+    private func loadEchartsForBugllyOfWKEchartsView() {
+        do {
+            let url = URL(fileURLWithPath: bundlePath)
+            let fileURL = try fileURLForBuggyWKWebView8(fileURL: url)
+            load(URLRequest(url: fileURL))
+        } catch let error as NSError {
+            print("Error: " + error.debugDescription)
+        }
+    }
+    
+    /// 解决 iOS8 以及在 OS X 上没办法加载本地内容以及资源文件的情况
+    private func fileURLForBuggyWKWebView8(fileURL: URL) throws -> URL {
+        // Some safety checks
+        if !fileURL.isFileURL {
+            throw NSError(
+                domain: "BuggyWKWebViewDomain",
+                code: 1001,
+                userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("URL must be a file URL.", comment:"")])
+        }
+        let _ = try! fileURL.checkResourceIsReachable()
+        
+        let absolutePath = fileURL.absoluteString
+        let dirPath = (absolutePath as NSString).deletingLastPathComponent
+        let dirUrl = URL(fileURLWithPath: dirPath)
+        let jsUrl = dirUrl.appendingPathComponent("js")
+        
+        // Create "/temp/www" directory
+        let fm = FileManager.default
+        let tmpDirURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("www")
+        try! fm.createDirectory(at: tmpDirURL, withIntermediateDirectories: true, attributes: nil)
+        let tmpJsDirUrl = tmpDirURL.appendingPathComponent("js")
+        try! fm.createDirectory(at: tmpJsDirUrl, withIntermediateDirectories: true, attributes: nil)
+        
+        let jsPath = jsUrl.absoluteString.replacingOccurrences(of: "file:", with: "")
+        if let files = try? fm.contentsOfDirectory(atPath: jsPath) {
+            for file in files {
+                let jsFileUrl = URL(fileURLWithPath: jsPath + file)
+                let dstURL = tmpJsDirUrl.appendingPathComponent(jsFileUrl.lastPathComponent)
+                let _ = try? fm.removeItem(at: dstURL)
+                try! fm.copyItem(at: jsFileUrl, to: dstURL)
+            }
+        }
+        
+        // Now copy given file to the temp directory
+        let dstURL = tmpDirURL.appendingPathComponent(fileURL.lastPathComponent)
+        let _ = try? fm.removeItem(at: dstURL)
+        try! fm.copyItem(at: fileURL, to: dstURL)
+        
+        // Files in "/temp/www" load flawlesly :)
+        return dstURL
     }
     
     fileprivate func callJsMethod(_ jsString: String, completionHandler: ((Any?, Error?) -> Swift.Void)? = nil) {
@@ -208,6 +271,9 @@ open class EchartsView: WKWebView, WKNavigationDelegate, WKUIDelegate, WKScriptM
     // MARK: - Delegate
     // MARK: UIWebViewDelegate
     open func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.evaluateJavaScript("echarts") { [weak self] (result, error) in
+            Swift.print(result)
+        }
         webView.evaluateJavaScript("document.readyState") { [weak self] (result, error) in
             guard let strongSelf = self else { return }
             guard let result = result as? String, result == "complete" else {
